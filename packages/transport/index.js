58c70d45ddf1dca59930c40ae6c6cc3ed6a378bb
Durran Jordan <durran@gmail.com>
2019-10-23 23:31:20 +0200

    Adding in StitchServerTransport

--- packages/transport/index.js
+++ packages/transport/index.js
@@ -1,7 +1,9 @@
 const NodeTransport = require('./lib/node-transport');
 const StitchBrowserTransport = require('./lib/stitch-browser-transport');
+const StitchServerTransport = require('./lib/stitch-server-transport');
 
 module.exports = {
   NodeTransport,
-  StitchBrowserTransport
+  StitchBrowserTransport,
+  StitchServerTransport
 };
---
+++ packages/transport/lib/stitch-server-transport.js
@@ -0,0 +1,32 @@
+const NOT_IMPLEMENTED = 'is not implemented in the Stitch server SDK';
+
+/**
+ * Encapsulates logic for communicating with a MongoDB instance via
+ * Stitch in the server.
+ */
+class StitchServerTransport {
+  /**
+   * Instantiate a new Stitch server transport with a connected stitch
+   * client instance.
+   *
+   * @param {Client} stitchClient - The Stitch client instance.
+   */
+  constructor(stitchClient) {
+    this.stichClient = stitchClient;
+  }
+
+  /**
+   * Run a command against the database.
+   *
+   * @param {String} database - The database name.
+   * @param {Object} spec - The command specification.
+   * @param {Object} options - The database options.
+   *
+   * @returns {Promise} The promise of command results.
+   */
+  runCommand(database, spec, options = {}) {
+    return Promise.reject(`StitchServerTransport#runCommand ${NOT_IMPLEMENTED}`);
+  }
+};
+
+module.exports = StitchServerTransport;
