7b5c7e17f1309c0930c323ee4cafed79c4d4ab5d
Durran Jordan <durran@gmail.com>
2019-11-05 14:23:02 +0100

    Adding runCommand unit test with stubs

--- packages/transport/src/node-transport.spec.js
+++ packages/transport/src/node-transport.spec.js
@@ -1,4 +1,5 @@
 const NodeTransport = require('./node-transport');
+const { MongoClient, Db } = require('mongodb');
 const { expect } = require('chai');
 const sinon = require('sinon');
 
@@ -9,6 +10,34 @@ describe('NodeTransport', () => {
 
     it('sets the mongo client on the instance', () => {
       expect(nodeTransport.mongoClient).to.equal(mongoClient);
+    });
+  });
+
+  describe('#runCommand', () => {
+    let clientStub;
+    let dbStub;
+    let nodeTransport;
+
+    beforeEach(() => {
+      dbStub = sinon.createStubInstance(Db, {
+        command: sinon.stub().withArgs({ ismaster: 1 }).resolves({ primary: true })
+      });
+      clientStub = sinon.createStubInstance(MongoClient, {
+        db: sinon.stub().returns(dbStub)
+      });
+      nodeTransport = new NodeTransport(clientStub);
+    });
+
+    afterEach(() => {
+      dbStub = null;
+      clientStub = null;
+      nodeTransport = null;
+    });
+
+    it('executes the command against the database', () => {
+      return nodeTransport.runCommand({ ismaster: 1 }).then((result) => {
+        expect(result).to.deep.equal({ primary: true });
+      });
     });
   });
 })
