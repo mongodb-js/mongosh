02d7cabd62246ef550e3b087416b49059af10fef
aherlihy <anna.herlihy@10gen.com>
2019-11-08 11:38:44 +0300

    More shell api

--- packages/mapper/lib/mapper.js
+++ packages/mapper/lib/mapper.js
@@ -18,7 +18,7 @@ class Mapper {
     this.runCommand = (database, cmd) => {
       return this._serviceProvider.runCommand(database.database, cmd);
     };
-    
+
     this.use = (db) => {
       this._ctx.db = new Database(this, db);
       return `switched to db ${db}`;
@@ -29,6 +29,7 @@ class Mapper {
      * @param collection {Collection} - reference to the collection object this method is called from.
      * @param query {Object} - query spec
      * @param projection {Object} - projection spec
+     *
      * @return {ShellApi.Cursor} - newly instantiated cursor object
      */
     this.find = (collection, query, projection) => {
@@ -45,7 +46,47 @@ class Mapper {
           options
         )
       );
+    };
+
+    /**
+     * Aggregate command
+     *
+     * @param collection {Collection} - reference to the collection object this method is called from.
+     * @param pipeline {Array} - pipeline array
+     * @param options {Object} - options
+     *
+     * @return {ShellApi.Cursor} - newly instantiated cursor object
+     */
+    this.aggregate = (collection, pipeline, options) => {
+      return new Cursor(
+        this,
+        this._serviceProvider.aggregate(
+          collection.database,
+          collection.collection,
+          pipeline,
+          options
+        )
+      );
     }
+
+    /**
+     * Bulk write command
+     *
+     * @param collection {Collection} - reference to the collection object this method is called from.
+     * @param {Array} pipeline - The aggregation pipeline.
+     * @param {Object} options - The pipeline options.
+     *
+     * @return {boolean} - A boolean acknowledged as true if the operation ran with write concern or false if write concern was disabled.
+     */
+    this.bulkWrite = (collection, operations, writeConcern, ordered) => {
+      // TODO: options for bulk write
+      this._serviceProvider.bulkWrite(
+        collection.database,
+        collection.collection,
+        operations,
+        options
+      )
+    };
   }
 
 }
