bc63f2c6dfb43cd77c54019a8b9ee88657406b9f
aherlihy <anna.herlihy@10gen.com>
2019-11-08 18:28:19 +0300

    Add rest of crud to mapper (roughly)

--- packages/service-provider/lib/cli-service-provider.js
+++ packages/service-provider/lib/cli-service-provider.js
@@ -32,9 +32,51 @@ class CliServiceProvider {
   find(database, collection, query, options = {}) {
     return this.nodeTransport.find(database, collection, query, options);
   }
-
-
-
+  aggregate(database, collection, pipeline, options) {
+    return this.nodeTransport.aggregate(database, collection, pipeline, options);
+  }
+  bulkWrite(database, collection, requests = {}, options = {}) {
+    return this.nodeTransport.bulkWrite(database, collection, requests, options);
+  }
+  countDocuments(database, collection, filter, options) {
+    return this.nodeTransport.countDocuments(database, collection, filter, options);
+  }
+  deleteMany(database, collection, filter, options) {
+    return this.nodeTransport.deleteMany(database, collection, filter, options);
+  }
+  deleteOne(database, collection, filter, options) {
+    return this.nodeTransport.deleteOne(database, collection, filter, options);
+  }
+  distinct(database, collection, filter, options) {
+    return this.nodeTransport.distinct(database, collection, filter, options);
+  }
+  estimatedDocumentCount(database, collection, filter, options) {
+    return this.nodeTransport.estimatedDocumentCount(database, collection, filter, options);
+  }
+  findOneAndDelete(database, collection, filter, options) {
+    return this.nodeTransport.findOneAndDelete(database, collection, filter, options);
+  }
+  findOneAndReplace(database, collection, filter, options) {
+    return this.nodeTransport.findOneAndReplace(database, collection, filter, options);
+  }
+  findOneAndUpdate(database, collection, filter, options) {
+    return this.nodeTransport.findOneAndUpdate(database, collection, filter, options);
+  }
+  insertMany(database, collection, filter, options) {
+    return this.nodeTransport.insertMany(database, collection, filter, options);
+  }
+  insertOne(database, collection, filter, options) {
+    return this.nodeTransport.insertOne(database, collection, filter, options);
+  }
+  replaceOne(database, collection, filter, options) {
+    return this.nodeTransport.replaceOne(database, collection, filter, options);
+  }
+  updateMany(database, collection, filter, options) {
+    return this.nodeTransport.updateMany(database, collection, filter, options);
+  }
+  updateOne(database, collection, filter, options) {
+    return this.nodeTransport.updateOne(database, collection, filter, options);
+  }
 }
 
 module.exports = CliServiceProvider;
