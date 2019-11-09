179782478fa2a74150a2da807af63015f1311921
aherlihy <anna.herlihy@10gen.com>
2019-11-09 18:58:30 +0300

    Update node service provider to match shell api

--- packages/transport/src/node-transport.ts
+++ packages/transport/src/node-transport.ts
@@ -29,10 +29,25 @@ class NodeTransport {
   }
 
   /**
+   * Instantiate a new Node transport with the Node driver's connected
+   * MongoClient instance.
+   *
+   * @param {MongoClient} mongoClient - The Node drivers' MongoClient instance.
+   */
+  constructor(mongoClient) {
+    this.mongoClient = mongoClient;
+  }
+
+  /**
    * Run an aggregation pipeline.
    *
    * @note: Passing a null collection will cause the
    *   aggregation to run on the DB.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
+   * @note: Shell API sets readConcern via options in object,
+   * node driver flat.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -42,15 +57,25 @@ class NodeTransport {
    * @returns {Promise} The promise of the aggregation cursor.
    */
   aggregate(database, collection, pipeline = [], options = {}) {
-    if (collection === null) {
-      return this._db(database).aggregate(pipeline, options);
+    const dbOptions = {};
+    if ('readConcern' in options) {
+      dbOptions.readConcern = options.readConcern;
     }
-    return this._db(database).collection(collection).
+    if ('writeConcern' in options) {
+      dbOptions.writeConcern = options.writeConcern;
+    }
+    if (collection === null) {
+      return this._db(database, dbOptions).aggregate(pipeline, options);
+    }
+    return this._db(database, dbOptions).collection(collection).
       aggregate(pipeline, options);
   }
 
   /**
    * Execute a mix of write operations.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -60,8 +85,40 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   bulkWrite(database, collection, requests = {}, options = {}) {
+    const bulkOptions = {};
+    if ('writeConcern' in options) {
+      Object.assign(bulkOptions, options.writeConcern);
+    }
+    if ('ordered' in options) {
+      bulkOptions.ordered = options.ordered;
+    }
     return this._db(database).collection(collection).
       bulkWrite(requests, options);
+  }
+
+  /**
+   * Deprecated count command.
+   *
+   * @note: Shell API passes readConcern via options, node via collection
+   * @note: Shell API passes collation as option, node driver via cursor.
+   *
+   * @param {String} database - The database name.
+   * @param {String} collection - The collection name.
+   * @param {Object} query - The filter.
+   * @param {Object} options - The count options.
+   *
+   * @returns {Promise} The promise of the count.
+   */
+  count(database, collection, query = {}, options = {}) {
+    const collOpts = {};
+    if ('readConcern' in options) {
+      collOpts.readConcern = options.readConcern;
+    }
+    const cursor = this._db(database).collection(collection, collOpts).count(query)
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
@@ -80,17 +137,11 @@ class NodeTransport {
   }
 
   /**
-   * Instantiate a new Node transport with the Node driver's connected
-   * MongoClient instance.
-   *
-   * @param {MongoClient} mongoClient - The Node drivers' MongoClient instance.
-   */
-  constructor(mongoClient) {
-    this.mongoClient = mongoClient;
-  }
-
-  /**
    * Delete multiple documents from the collection.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
+   * @note: Shell API passes collation as option, node driver via cursor.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -100,12 +151,25 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   deleteMany(database, collection, filter = {}, options = {}) {
-    return this._db(database).collection(collection).
-      deleteMany(filter, options);
+    const cmdOpts = {};
+    if ('writeConcern' in options) {
+      Object.assign(cmdOpts, options.writeConcern);
+    }
+    const cursor = this._db(database).collection(collection).
+      deleteMany(filter, cmdOpts);
+
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
    * Delete one document from the collection.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
+   * @note: Shell API passes collation as option, node driver via cursor.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -115,12 +179,22 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   deleteOne(database, collection, filter = {}, options = {}) {
-    return this._db(database).collection(collection).
-      deleteOne(filter, options);
+    const cmdOpts = {};
+    if ('writeConcern' in options) {
+      Object.assign(cmdOpts, options.writeConcern);
+    }
+    const cursor = this._db(database).collection(collection).
+      deleteOne(filter, cmdOpts);
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
    * Get distinct values for the field.
+   *
+   * @note: Shell API passes collation as option, node driver via cursor.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -131,8 +205,13 @@ class NodeTransport {
    * @returns {Promise} The promise of the cursor.
    */
   distinct(database, collection, fieldName, filter = {}, options = {}) {
-    return this._db(database).collection(collection).
+    const cursor = this._db(database).collection(collection).
       distinct(fieldName, filter, options);
+
+    if ('collation' in cursor) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
@@ -152,20 +231,29 @@ class NodeTransport {
   /**
    * Find documents in the collection.
    *
+   * @note: Shell API passes filter and projection to find,
+   * node driver uses filter and options.
+   *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
    * @param {Object} filter - The filter.
-   * @param {Object} options - The find options.
+   * @param {Object} projection - The projection.
    *
    * @returns {Promise} The promise of the cursor.
    */
-  find(database, collection, filter = {}, options = {}) {
+  find(database, collection, filter = {}, projection = {}) {
+    const options = {};
+    if (projection) {
+      options.projection = projection;
+    }
     return this._db(database).collection(collection).
       find(filter, options);
   }
 
   /**
    * Find one document and delete it.
+   *
+   * @note: Shell API passes collation as option, node driver via cursor.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -175,12 +263,18 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   findOneAndDelete(database, collection, filter = {}, options = {}) {
-    return this._db(database).collection(collection).
+    const cursor = this._db(database).collection(collection).
       findOneAndDelete(filter, options);
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
    * Find one document and replace it.
+   *
+   * @note: Shell API passes collation as option, node driver via cursor.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -191,12 +285,19 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   findOneAndReplace(database, collection, filter = {}, replacement = {}, options = {}) {
-    return this._db(database).collection(collection).
+    const cursor = this._db(database).collection(collection).
       findOneAndReplace(filter, replacement, options);
+
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
    * Find one document and update it.
+   *
+   * @note: Shell API passes collation as option, node driver via cursor.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -207,12 +308,20 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   findOneAndUpdate(database, collection, filter = {}, update = {}, options = {}) {
-    return this._db(database).collection(collection).
+    const cursor = this._db(database).collection(collection).
       findOneAndUpdate(filter, update, options);
+
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
-   * Insert many documents into the colleciton.
+   * Insert many documents into the collection.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -222,12 +331,24 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   insertMany(database, collection, docs = [], options = {}) {
+    const cmdOpts = {};
+
+    if ('writeConcern' in options) {
+      Object.assign(cmdOpts, options.writeConcern);
+    }
+    if ('ordered' in options) {
+      cmdOpts.ordered = options.ordered;
+    }
+
     return this._db(database).collection(collection).
-      insertMany(docs, options);
+      insertMany(docs, cmdOpts);
   }
 
   /**
    * Insert one document into the collection.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -237,24 +358,81 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   insertOne(database, collection, doc = {}, options = {}) {
+    const cmdOpts = {};
+
+    if ('writeConcern' in options) {
+      Object.assign(cmdOpts, options.writeConcern);
+    }
     return this._db(database).collection(collection).
       insertOne(doc, options);
   }
 
+  /**
+   * Is the collection capped?
+   *
+   * @param database
+   * @param collection
+   * @return {Promise}
+   */
   isCapped(database, collection) {
     return this._db(database).collection(collection).isCapped();
   }
 
-  remove(database, collection, selector, options) {
-    return this._db(database).collection(collection).remove(selector, options);
+  /**
+   * Deprecated remove command.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
+   * @note: Shell API passes collation as option, node driver via cursor.
+   *
+   * @param database
+   * @param collection
+   * @param query
+   * @param options
+   * @return {Promise}
+   */
+  remove(database, collection, query, options) {
+    let removeOptions = {};
+    if (typeof options === 'boolean') {
+      removeOptions = { single: options };
+    }
+    if ('writeConcern' in options) {
+      Object.assign(removeOptions, options.writeConcern);
+    }
+
+    const cursor = this._db(database).collection(collection).remove(query, options);
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
+  /**
+   * Deprecated save command.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
+   *
+   * @param database
+   * @param collection
+   * @param doc
+   * @param options
+   * @return {Promise}
+   */
   save(database, collection, doc, options) {
-    return this._db(database).collection(collection).save(doc, options);
+    const saveOptions = {};
+    if ('writeConcern' in options) {
+      Object.assign(saveOptions, options.writeConcern);
+    }
+    return this._db(database).collection(collection).save(doc, saveOptions);
   }
 
   /**
    * Replace a document with another.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
+   * @note: Shell API sets collation via options, node driver via cursor.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -265,8 +443,16 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   replaceOne(database, collection, filter = {}, replacement = {}, options = {}) {
-    return this._db(database).collection(collection).
-      replaceOne(filter, replacement, options);
+    const cmdOpts = {};
+    if ('writeConcern' in options) {
+      Object.assign(cmdOpts, options.writeConcern);
+    }
+    const cursor = this._db(database).collection(collection).
+      replaceOne(filter, replacement, cmdOpts);
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
@@ -283,7 +469,11 @@ class NodeTransport {
   }
 
   /**
-   * Update many document.
+   * Update many documents.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
+   * @note: Shell API sets collation via options, node driver via cursor.
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -294,12 +484,25 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   updateMany(database, collection, filter = {}, update = {}, options = {}) {
-    return this._db(database).collection(collection).
-      updateMany(filter, update, options);
+    const cmdOpts = {};
+    if ('writeConcern' in options) {
+      Object.assign(cmdOpts, options.writeConcern);
+    }
+    const cursor = this._db(database).collection(collection).
+    updateMany(filter, update, cmdOpts);
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
    * Update a document.
+   *
+   * @note: Shell API sets writeConcern via options in object,
+   * node driver flat.
+   * @note: Shell API sets collation via options, node driver via cursor.
+   * TODO: Shell API provides 'hint' but node driver does not
    *
    * @param {String} database - The database name.
    * @param {String} collection - The collection name.
@@ -310,8 +513,16 @@ class NodeTransport {
    * @returns {Promise} The promise of the result.
    */
   updateOne(database, collection, filter = {}, update = {}, options = {}) {
-    return this._db(database).collection(collection).
-      updateOne(filter, update, options);
+    const cmdOpts = {};
+    if ('writeConcern' in options) {
+      Object.assign(cmdOpts, options.writeConcern);
+    }
+    const cursor = this._db(database).collection(collection).
+      updateOne(filter, update, cmdOpts);
+    if ('collation' in options) {
+      return cursor.collation(options.collation);
+    }
+    return cursor;
   }
 
   /**
