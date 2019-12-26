379cbbf9689cfb54ea0a742ea475a86d3c7df6bb
Durran Jordan <durran@gmail.com>
2019-12-26 15:49:07 +0100

    MONGOSH-46: Get TS running with Karma in browser

--- packages/transport-browser/lib/index.js
+++ packages/transport-browser/lib/index.js
@@ -1,7 +1,4 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var stitch_browser_transport_1 = __importDefault(require("./stitch-browser-transport"));
+const stitch_browser_transport_1 = require("./stitch-browser-transport");
 exports.StitchBrowserTransport = stitch_browser_transport_1.default;
--- packages/transport-browser/lib/stitch-browser-transport.js
+++ packages/transport-browser/lib/stitch-browser-transport.js
@@ -8,49 +8,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var mongosh_transport_core_1 = require("mongosh-transport-core");
-var mongodb_stitch_browser_sdk_1 = require("mongodb-stitch-browser-sdk");
+const mongosh_transport_core_1 = require("mongosh-transport-core");
+const mongodb_stitch_browser_sdk_1 = require("mongodb-stitch-browser-sdk");
 /**
  * Init error.
  */
-var INIT_ERROR = 'Error authenticating with Stitch.';
+const INIT_ERROR = 'Error authenticating with Stitch.';
 /**
  * Atlas id.
  */
-var ATLAS = 'mongodb-atlas';
+const ATLAS = 'mongodb-atlas';
 /**
  * Encapsulates logic for communicating with a MongoDB instance via
  * Stitch in the browser.
  */
-var StitchBrowserTransport = /** @class */ (function () {
+class StitchBrowserTransport {
     /**
      * Instantiate a new Stitch server transport with a connected stitch
      * client instance.
@@ -58,9 +31,8 @@ var StitchBrowserTransport = /** @class */ (function () {
      * @param {Client} stitchClient - The Stitch client instance.
      * @param {String} serviceName - The Mongo service name.
      */
-    function StitchBrowserTransport(stitchClient, serviceName) {
-        if (serviceName === void 0) { serviceName = ATLAS; }
-        var mongoClient = stitchClient.
+    constructor(stitchClient, serviceName = ATLAS) {
+        const mongoClient = stitchClient.
             getServiceClient(mongodb_stitch_browser_sdk_1.RemoteMongoClient.factory, serviceName);
         this.stitchTransport =
             new mongosh_transport_core_1.StitchTransport(stitchClient, mongoClient);
@@ -73,30 +45,19 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the Stitch server transport.
      */
-    StitchBrowserTransport.fromAppId = function (stitchAppId, serviceName) {
-        return __awaiter(this, void 0, void 0, function () {
-            var client, err_1;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        client = mongodb_stitch_browser_sdk_1.Stitch.initializeDefaultAppClient(stitchAppId);
-                        _a.label = 1;
-                    case 1:
-                        _a.trys.push([1, 3, , 4]);
-                        return [4 /*yield*/, client.auth.loginWithCredential(new mongodb_stitch_browser_sdk_1.AnonymousCredential())];
-                    case 2:
-                        _a.sent();
-                        return [3 /*break*/, 4];
-                    case 3:
-                        err_1 = _a.sent();
-                        /* eslint no-console:0 */
-                        console.log(INIT_ERROR, err_1);
-                        return [3 /*break*/, 4];
-                    case 4: return [2 /*return*/, new StitchBrowserTransport(client, serviceName)];
-                }
-            });
+    static fromAppId(stitchAppId, serviceName) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const client = mongodb_stitch_browser_sdk_1.Stitch.initializeDefaultAppClient(stitchAppId);
+            try {
+                yield client.auth.loginWithCredential(new mongodb_stitch_browser_sdk_1.AnonymousCredential());
+            }
+            catch (err) {
+                /* eslint no-console:0 */
+                console.log(INIT_ERROR, err);
+            }
+            return new StitchBrowserTransport(client, serviceName);
         });
-    };
+    }
     /**
      * Run an aggregation pipeline.
      *
@@ -110,18 +71,17 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Cursor} The aggregation cursor.
      */
-    StitchBrowserTransport.prototype.aggregate = function (database, collection, pipeline) {
-        if (pipeline === void 0) { pipeline = []; }
+    aggregate(database, collection, pipeline = []) {
         return this.stitchTransport.aggregate(database, collection, pipeline);
-    };
+    }
     /**
      * Not implemented in Stitch.
      *
      * @returns {Promise} The rejected promise.
      */
-    StitchBrowserTransport.prototype.bulkWrite = function () {
+    bulkWrite() {
         return this.stitchTransport.bulkWrite();
-    };
+    }
     /**
      * Get an exact document count from the collection.
      *
@@ -132,11 +92,9 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the count.
      */
-    StitchBrowserTransport.prototype.countDocuments = function (database, collection, filter, options) {
-        if (filter === void 0) { filter = {}; }
-        if (options === void 0) { options = {}; }
+    countDocuments(database, collection, filter = {}, options = {}) {
         return this.stitchTransport.countDocuments(database, collection, filter, options);
-    };
+    }
     /**
      * Delete multiple documents from the collection.
      *
@@ -147,10 +105,9 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the result.
      */
-    StitchBrowserTransport.prototype.deleteMany = function (database, collection, filter) {
-        if (filter === void 0) { filter = {}; }
+    deleteMany(database, collection, filter = {}) {
         return this.stitchTransport.deleteMany(database, collection, filter);
-    };
+    }
     /**
      * Delete one document from the collection.
      *
@@ -161,26 +118,25 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the result.
      */
-    StitchBrowserTransport.prototype.deleteOne = function (database, collection, filter) {
-        if (filter === void 0) { filter = {}; }
+    deleteOne(database, collection, filter = {}) {
         return this.stitchTransport.deleteOne(database, collection, filter);
-    };
+    }
     /**
      * Not implemented in Stitch.
      *
      * @returns {Promise} The rejected promise.
      */
-    StitchBrowserTransport.prototype.distinct = function () {
+    distinct() {
         return this.stitchTransport.distinct();
-    };
+    }
     /**
      * Not implemented in Stitch.
      *
      * @returns {Promise} The rejected promise.
      */
-    StitchBrowserTransport.prototype.estimatedDocumentCount = function () {
+    estimatedDocumentCount() {
         return this.stitchTransport.estimatedDocumentCount();
-    };
+    }
     /**
      * Find documents in the collection.
      *
@@ -191,11 +147,9 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Cursor} The cursor.
      */
-    StitchBrowserTransport.prototype.find = function (database, collection, filter, options) {
-        if (filter === void 0) { filter = {}; }
-        if (options === void 0) { options = {}; }
+    find(database, collection, filter = {}, options = {}) {
         return this.stitchTransport.find(database, collection, filter, options);
-    };
+    }
     /**
      * Find one document and delete it.
      *
@@ -206,11 +160,9 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the result.
      */
-    StitchBrowserTransport.prototype.findOneAndDelete = function (database, collection, filter, options) {
-        if (filter === void 0) { filter = {}; }
-        if (options === void 0) { options = {}; }
+    findOneAndDelete(database, collection, filter = {}, options = {}) {
         return this.stitchTransport.findOneAndDelete(database, collection, filter, options);
-    };
+    }
     /**
      * Find one document and replace it.
      *
@@ -222,12 +174,9 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the result.
      */
-    StitchBrowserTransport.prototype.findOneAndReplace = function (database, collection, filter, replacement, options) {
-        if (filter === void 0) { filter = {}; }
-        if (replacement === void 0) { replacement = {}; }
-        if (options === void 0) { options = {}; }
+    findOneAndReplace(database, collection, filter = {}, replacement = {}, options = {}) {
         return this.stitchTransport.findOneAndReplace(database, collection, filter, replacement, options);
-    };
+    }
     /**
      * Find one document and update it.
      *
@@ -239,12 +188,9 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the result.
      */
-    StitchBrowserTransport.prototype.findOneAndUpdate = function (database, collection, filter, update, options) {
-        if (filter === void 0) { filter = {}; }
-        if (update === void 0) { update = {}; }
-        if (options === void 0) { options = {}; }
+    findOneAndUpdate(database, collection, filter = {}, update = {}, options = {}) {
         return this.stitchTransport.findOneAndUpdate(database, collection, filter, update, options);
-    };
+    }
     /**
      * Insert many documents into the colleciton.
      *
@@ -255,11 +201,9 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the result.
      */
-    StitchBrowserTransport.prototype.insertMany = function (database, collection, docs, options) {
-        if (docs === void 0) { docs = []; }
-        if (options === void 0) { options = {}; }
+    insertMany(database, collection, docs = [], options = {}) {
         return this.stitchTransport.insertMany(database, collection, docs);
-    };
+    }
     /**
      * Insert one document into the collection.
      *
@@ -270,27 +214,25 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the result.
      */
-    StitchBrowserTransport.prototype.insertOne = function (database, collection, doc, options) {
-        if (doc === void 0) { doc = {}; }
-        if (options === void 0) { options = {}; }
+    insertOne(database, collection, doc = {}, options = {}) {
         return this.stitchTransport.insertOne(database, collection, doc);
-    };
+    }
     /**
      * Not implemented in Stitch.
      *
      * @returns {Promise} The rejected promise.
      */
-    StitchBrowserTransport.prototype.replaceOne = function () {
+    replaceOne() {
         return this.stitchTransport.replaceOne();
-    };
+    }
     /**
      * Not implemented in Stitch.
      *
      * @returns {Promise} The rejected promise.
      */
-    StitchBrowserTransport.prototype.runCommand = function () {
+    runCommand() {
         return this.stitchTransport.runCommand();
-    };
+    }
     /**
      * Update many document.
      *
@@ -302,12 +244,9 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the result.
      */
-    StitchBrowserTransport.prototype.updateMany = function (database, collection, filter, update, options) {
-        if (filter === void 0) { filter = {}; }
-        if (update === void 0) { update = {}; }
-        if (options === void 0) { options = {}; }
+    updateMany(database, collection, filter = {}, update = {}, options = {}) {
         return this.stitchTransport.updateMany(database, collection, filter, update, options);
-    };
+    }
     /**
      * Update a document.
      *
@@ -319,24 +258,16 @@ var StitchBrowserTransport = /** @class */ (function () {
      *
      * @returns {Promise} The promise of the result.
      */
-    StitchBrowserTransport.prototype.updateOne = function (database, collection, filter, update, options) {
-        if (filter === void 0) { filter = {}; }
-        if (update === void 0) { update = {}; }
-        if (options === void 0) { options = {}; }
+    updateOne(database, collection, filter = {}, update = {}, options = {}) {
         return this.stitchTransport.updateOne(database, collection, filter, update, options);
-    };
-    Object.defineProperty(StitchBrowserTransport.prototype, "userId", {
-        /**
-         * Get the current user id.
-         *
-         * @returns {String} The user id.
-         */
-        get: function () {
-            return this.stitchTransport.userId;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return StitchBrowserTransport;
-}());
+    }
+    /**
+     * Get the current user id.
+     *
+     * @returns {String} The user id.
+     */
+    get userId() {
+        return this.stitchTransport.userId;
+    }
+}
 exports.default = StitchBrowserTransport;
