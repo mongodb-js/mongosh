9624bc58663231918125f1402703b71aece37c25
Durran Jordan <durran@gmail.com>
2020-01-07 14:32:06 +0700

    Unifying typescript configurations

--- packages/transport-browser/lib/index.js
+++ packages/transport-browser/lib/index.js
@@ -1,4 +1,8 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const stitch_browser_transport_1 = require("./stitch-browser-transport");
+var stitch_browser_transport_1 = __importDefault(require("./stitch-browser-transport"));
 exports.StitchBrowserTransport = stitch_browser_transport_1.default;
+//# sourceMappingURL=index.js.map
