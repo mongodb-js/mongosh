d1bb18e1eb0c88983b2269b307abf5939a4cc321
Durran Jordan <durran@gmail.com>
2020-01-10 20:22:27 +0700

    MONGOSH-44: Convert CLI REPL to TS

--- packages/cli-repl/src/index.ts
+++ packages/cli-repl/src/index.ts
@@ -1,8 +1,8 @@
-const CliRepl = require('./lib/cli-repl.js');
+import CliRepl from './cli-repl';
 
 // TODO: Durran: Refactor to handle both executable and
 // dev mode with npm start.
 if (process.argv[2] === 'start') new CliRepl();
 if (process.argv[2] === 'start-antlr') new CliRepl(true);
 
-module.exports = CliRepl;
+export default CliRepl;
