0ec728b32fbc0d99f811067122f3e6d9321d2e5d
Durran Jordan <durran@gmail.com>
2020-01-14 22:34:00 +0700

    MONGOSH-55: Adding in general options

--- packages/cli-repl/src/arg-parser.spec.ts
+++ packages/cli-repl/src/arg-parser.spec.ts
@@ -20,6 +20,152 @@ describe('arg-parser.parse', () => {
     context('when providing a URI + options', () => {
       const uri = 'mongodb://domain.com:20000';
 
+      context('when providing general options', () => {
+        context('when providing --ipv6', () => {
+          const argv = [ ...baseArgv, uri, '--ipv6' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the ipv6 value in the object', () => {
+            expect(parse(argv).ipv6).to.equal(true);
+          });
+        });
+
+        context('when providing -h', () => {
+          const argv = [ ...baseArgv, uri, '-h' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the help value in the object', () => {
+            expect(parse(argv).help).to.equal(true);
+          });
+        });
+
+        context('when providing --help', () => {
+          const argv = [ ...baseArgv, uri, '--help' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the help value in the object', () => {
+            expect(parse(argv).help).to.equal(true);
+          });
+        });
+
+        context('when providing --version', () => {
+          const argv = [ ...baseArgv, uri, '--version' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the version value in the object', () => {
+            expect(parse(argv).version).to.equal(true);
+          });
+        });
+
+        context('when providing --verbose', () => {
+          const argv = [ ...baseArgv, uri, '--verbose' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the verbose value in the object', () => {
+            expect(parse(argv).verbose).to.equal(true);
+          });
+        });
+
+        context('when providing --shell', () => {
+          const argv = [ ...baseArgv, uri, '--shell' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the shell value in the object', () => {
+            expect(parse(argv).shell).to.equal(true);
+          });
+        });
+
+        context('when providing --nodb', () => {
+          const argv = [ ...baseArgv, uri, '--nodb' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the nodb value in the object', () => {
+            expect(parse(argv).nodb).to.equal(true);
+          });
+        });
+
+        context('when providing --norc', () => {
+          const argv = [ ...baseArgv, uri, '--norc' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the norc value in the object', () => {
+            expect(parse(argv).norc).to.equal(true);
+          });
+        });
+
+        context('when providing --quiet', () => {
+          const argv = [ ...baseArgv, uri, '--quiet' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the quiet value in the object', () => {
+            expect(parse(argv).quiet).to.equal(true);
+          });
+        });
+
+        context('when providing --eval', () => {
+          const argv = [ ...baseArgv, uri, '--eval', '1+1' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the eval value in the object', () => {
+            expect(parse(argv).eval).to.equal('1+1');
+          });
+        });
+
+        context('when providing --retryWrites', () => {
+          const argv = [ ...baseArgv, uri, '--retryWrites' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the retryWrites value in the object', () => {
+            expect(parse(argv).retryWrites).to.equal(true);
+          });
+        });
+
+        context('when providing --disableImplicitSessions', () => {
+          const argv = [ ...baseArgv, uri, '--disableImplicitSessions' ];
+
+          it('returns the URI in the object', () => {
+            expect(parse(argv)._[0]).to.equal(uri);
+          });
+
+          it('sets the disableImplicitSessions value in the object', () => {
+            expect(parse(argv).disableImplicitSessions).to.equal(true);
+          });
+        });
+      });
+
       context('when providing authentication options', () => {
         context('when providing -u', () => {
           const argv = [ ...baseArgv, uri, '-u', 'richard' ];
