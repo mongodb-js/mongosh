b2af77c986117e5f2f76d88034371d9040f32a77
Durran Jordan <durran@gmail.com>
2020-01-14 23:22:31 +0700

    MONGOSH-55: Adding in tests for npm start

--- packages/cli-repl/bin/mongosh.js
+++ packages/cli-repl/bin/mongosh.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
 const { CliRepl } = require('../lib');
-
 process.title = 'mongosh';
-
-if (process.argv[2] === 'start-antlr') {
-  return new CliRepl(true);
-}
-
 new CliRepl();
