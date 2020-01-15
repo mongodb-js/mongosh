db8c9c97e885ac65cbf28e1ba7cb6e979d5f4e42
Durran Jordan <durran@gmail.com>
2020-01-15 15:19:41 +0700

    Switch docs to CliOptions

--- packages/cli-repl/src/arg-mapper.ts
+++ packages/cli-repl/src/arg-mapper.ts
@@ -3,6 +3,10 @@ import CliOptions from './cli-options';
 /**
  * Map the arguments provided on the command line to
  * driver friendly options.
+ *
+ * @param {CliOptions} options - The CLI options.
+ *
+ * @returns {} The driver options.
  */
 function mapCliToDriver(options: CliOptions) {
 };
