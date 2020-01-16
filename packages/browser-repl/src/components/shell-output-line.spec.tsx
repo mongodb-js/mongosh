90165e79ec72bfdaa35d7a7aaa69a81191e5306c
mcasimir <maurizio.cas@gmail.com>
2020-01-16 15:23:50 +0100

    feat(browser-ui): render stringified output

--- packages/browser-repl/src/components/shell-output-line.spec.tsx
+++ packages/browser-repl/src/components/shell-output-line.spec.tsx
@@ -7,7 +7,24 @@ import { ShellOutputLine } from './shell-output-line';
 describe('<ShellOutputLine />', () => {
   it('renders a string value', () => {
     const wrapper = shallow(<ShellOutputLine entry={{type: 'output', value: 'some text'}} />);
-    expect(wrapper.text()).to.contain('some text');
+    expect(wrapper.text()).to.contain('"some text"');
+  });
+
+  it('renders an integer value', () => {
+    const wrapper = shallow(<ShellOutputLine entry={{type: 'output', value: 1}} />);
+    expect(wrapper.text()).to.contain(1);
+  });
+
+  it('renders an object', () => {
+    const object = {x: 1};
+    const wrapper = shallow(<ShellOutputLine entry={{type: 'output', value: object}} />);
+    expect(wrapper.text()).to.contain(JSON.stringify(object, null, 2));
+  });
+
+  it('renders an error as stack trace', () => {
+    const err = new Error('x');
+    const wrapper = shallow(<ShellOutputLine entry={{type: 'output', value: err}} />);
+    expect(wrapper.text()).to.contain(err.stack);
   });
 });
 
