813086fdc514fa33f09c326101a85a78fa7e9477
Durran Jordan <durran@gmail.com>
2020-01-17 08:27:21 +0700

    MONGOSH-55: Merge auth and log level options

--- packages/cli-repl/src/arg-mapper.ts
+++ packages/cli-repl/src/arg-mapper.ts
@@ -1,23 +1,24 @@
 import CliOptions from './cli-options';
 import { NodeOptions } from 'mongosh-transport-server';
 
-// auth: NodeAuthOptions; //username -> user, password -> password
-// loggerLevel: string // --quiet -> error, --verbose -> debug
-
 /**
  * Mapping fields from the CLI args to Node options.
  */
 const MAPPINGS = {
-  authenticationDatabase: 'authSource',
-  authenticationMechanism: 'authMechanism',
-  disableImplicitSessions: 'explicitlyIgnoreSession',
-  retryWrites: 'retryWrites',
-  tls: 'tls',
-  tlsAllowInvalidCertificates: 'tlsAllowInvalidCertificates',
-  tlsAllowInvalidHostnames: 'tlsAllowInvalidHostnames',
-  tlsCAFile: 'tlsCAFile',
-  tlsCertificateKeyFile: 'tlsCertificateKeyFile',
-  tlsCertificateKeyFilePassword: 'tlsCertificateKeyFilePassword'
+  authenticationDatabase: (value) => ([ 'authSource', value ]),
+  authenticationMechanism: (value) => ([ 'authMechanism', value ]),
+  disableImplicitSessions: (value) => ([ 'explicitlyIgnoreSession', value ]),
+  password: (value) => ([ 'auth', { password: value }]),
+  quiet: () => ([ 'loggerLevel', 'error' ]),
+  retryWrites: (value) => ([ 'retryWrites', value ]),
+  tls: (value) => ([ 'tls', value ]),
+  tlsAllowInvalidCertificates: (value) => ([ 'tlsAllowInvalidCertificates', value ]),
+  tlsAllowInvalidHostnames: (value) => ([ 'tlsAllowInvalidHostnames', value ]),
+  tlsCAFile: (value) => ([ 'tlsCAFile', value ]),
+  tlsCertificateKeyFile: (value) => ([ 'tlsCertificateKeyFile', value ]),
+  tlsCertificateKeyFilePassword: (value) => ([ 'tlsCertificateKeyFilePassword', value ]),
+  username: (value) => ([ 'auth', { user: value }]),
+  verbose: () => ([ 'loggerLevel', 'debug' ])
 };
 
 /**
@@ -34,7 +35,15 @@ function mapCliToDriver(options: CliOptions): NodeOptions {
   const nodeOptions = {};
   Object.keys(MAPPINGS).forEach((cliOption) => {
     if (options.hasOwnProperty(cliOption)) {
-      nodeOptions[MAPPINGS[cliOption]] = options[cliOption];
+      const mapped = MAPPINGS[cliOption](options[cliOption]);
+      // In the case of auth, and potentially others, we need to
+      // merge the objects if the key already exists in the new
+      // options.
+      if (nodeOptions.hasOwnProperty(mapped[0])) {
+        nodeOptions[mapped[0]] = { ...nodeOptions[mapped[0]], ...mapped[1] };
+      } else {
+        nodeOptions[mapped[0]] = mapped[1];
+      }
     }
   });
   return nodeOptions;
