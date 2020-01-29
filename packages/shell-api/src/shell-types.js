689b36d56d8e868b9d5a713c8db24e73d78c31e6
Durran Jordan <durran@gmail.com>
2020-01-29 13:03:47 +0100

    Compile all shell api sources

---
+++ packages/shell-api/src/shell-types.js
@@ -0,0 +1,162 @@
+const AggregationCursor = {
+  type: 'AggregationCursor',
+  attributes: {
+    bsonsize: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    close: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    forEach: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    hasNext: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    isClosed: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    isExhausted: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    itcount: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    map: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    next: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    objsLeftInBatch: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    toArray: { type: 'function', returnsPromise: false, returnType: 'Unknown' }
+  }
+};
+const BulkWriteResult = {
+  type: 'BulkWriteResult',
+  attributes: {
+
+  }
+};
+const Collection = {
+  type: 'Collection',
+  attributes: {
+    aggregate: { type: 'function', returnsPromise: false, returnType: 'AggregationCursor' },
+    bulkWrite: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    countDocuments: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    count: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    deleteMany: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    deleteOne: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    distinct: { type: 'function', returnsPromise: false, returnType: 'Cursor' },
+    estimatedDocumentCount: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    find: { type: 'function', returnsPromise: false, returnType: 'Cursor' },
+    findAndModify: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    findOne: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    findOneAndDelete: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    findOneAndReplace: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    findOneAndUpdate: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    insert: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    insertMany: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    insertOne: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    isCapped: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    remove: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    save: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    replaceOne: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    update: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    updateMany: { type: 'function', returnsPromise: true, returnType: 'Unknown' },
+    updateOne: { type: 'function', returnsPromise: true, returnType: 'Unknown' }
+  }
+};
+const Cursor = {
+  type: 'Cursor',
+  attributes: {
+    addOption: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    allowPartialResults: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    arrayAccess: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    batchSize: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    clone: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    close: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    collation: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    comment: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    count: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    explain: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    forEach: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    getQueryPlan: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    hasNext: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    hint: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    isClosed: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    isExhausted: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    itcount: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    length: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    limit: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    map: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    max: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    maxScan: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    maxTimeMS: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    min: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    modifiers: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    next: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    noCursorTimeout: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    objsLeftInBatch: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    oplogReplay: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    projection: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    pretty: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    readConcern: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    readOnly: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    readPref: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    returnKey: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    showDiskLoc: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    showRecordId: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    size: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    skip: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    snapshot: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    sort: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    tailable: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    toArray: { type: 'function', returnsPromise: false, returnType: 'Unknown' }
+  }
+};
+const Database = {
+  type: 'Database',
+  attributes: {
+    runCommand: { type: 'function', returnsPromise: false, returnType: 'Unknown' }
+  }
+};
+const DeleteResult = {
+  type: 'DeleteResult',
+  attributes: {
+
+  }
+};
+const InsertManyResult = {
+  type: 'InsertManyResult',
+  attributes: {
+
+  }
+};
+const InsertOneResult = {
+  type: 'InsertOneResult',
+  attributes: {
+
+  }
+};
+const ReplicaSet = {
+  type: 'ReplicaSet',
+  attributes: {
+
+  }
+};
+const Shard = {
+  type: 'Shard',
+  attributes: {
+
+  }
+};
+const ShellApi = {
+  type: 'ShellApi',
+  attributes: {
+    use: { type: 'function', returnsPromise: false, returnType: 'Unknown' },
+    it: { type: 'function', returnsPromise: false, returnType: 'Unknown' }
+  }
+};
+const UpdateResult = {
+  type: 'UpdateResult',
+  attributes: {
+
+  }
+};
+export {
+  AggregationCursor,
+  BulkWriteResult,
+  Collection,
+  Cursor,
+  Database,
+  DeleteResult,
+  InsertManyResult,
+  InsertOneResult,
+  ReplicaSet,
+  Shard,
+  ShellApi,
+  UpdateResult
+};
