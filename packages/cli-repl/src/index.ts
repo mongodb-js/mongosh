ac4015ca16da02dd638a7e284dcd59312e762bc7
Irina Shestak <shestak.irina@gmail.com>
2020-01-29 12:15:33 +0100

    add usage command on --help

--- packages/cli-repl/src/index.ts
+++ packages/cli-repl/src/index.ts
@@ -2,8 +2,10 @@ import CliRepl from './cli-repl';
 import parseCliArgs from './arg-parser';
 import mapCliToDriver from './arg-mapper';
 import generateUri from './uri-generator';
-import completer from './completer';
 import changeHistory from './history';
+import completer from './completer'
+import { USAGE } from './constants';
 
 export default CliRepl;
-export { CliRepl, parseCliArgs, mapCliToDriver, generateUri, completer, changeHistory };
+export { CliRepl, parseCliArgs, mapCliToDriver, generateUri, USAGE, completer,
+  changeHistory };
