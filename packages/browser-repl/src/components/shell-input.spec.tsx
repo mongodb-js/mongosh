3ad21ce2b8eb17ac6edc98ee11efafeca88a224c
mcasimir <maurizio.cas@gmail.com>
2020-01-29 18:31:09 +0100

    feat(browser-repl): history with single line input

--- packages/browser-repl/src/components/shell-input.spec.tsx
+++ packages/browser-repl/src/components/shell-input.spec.tsx
@@ -5,7 +5,7 @@ import { shallow } from '../../testing/enzyme';
 
 import { ShellInput } from './shell-input';
 
-describe('<ShellInput />', () => {
+describe.only('<ShellInput />', () => {
   it('renders an input', () => {
     const wrapper = shallow(<ShellInput />);
     expect(wrapper.find('textarea')).to.have.lengthOf(1);
@@ -42,5 +42,109 @@ describe('<ShellInput />', () => {
 
   it.skip('allows newline when shift+enter is pressed', () => {
     // TODO: hard/impossible to test with enzyme
+  });
+
+  describe('history', () => {
+    it('navigates history backward on ArrowUp', () => {
+      const onInput = sinon.spy();
+      const wrapper = shallow(<ShellInput onInput={onInput} initialHistory={['value1', 'value2']} />);
+
+      const textarea = wrapper.find('textarea');
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      expect(wrapper.state('currentValue')).to.equal('value2');
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      expect(wrapper.state('currentValue')).to.equal('value1');
+    });
+
+    it('navigates history backward up to first element', () => {
+      const onInput = sinon.spy();
+      const wrapper = shallow(<ShellInput onInput={onInput} initialHistory={['value1']} />);
+
+      const textarea = wrapper.find('textarea');
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      wrapper.update();
+      expect(wrapper.state('currentValue')).to.equal('value1');
+    });
+
+    it('navigates history forward', () => {
+      const onInput = sinon.spy();
+      const wrapper = shallow(<ShellInput onInput={onInput} initialHistory={['value1', 'value2']} />);
+
+      const textarea = wrapper.find('textarea');
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      textarea.simulate('keydown', { key: 'ArrowDown' });
+      expect(wrapper.state('currentValue')).to.equal('value2');
+    });
+
+    it('does not move the history index past the last element', () => {
+      const onInput = sinon.spy();
+      const wrapper = shallow(<ShellInput onInput={onInput} initialHistory={['value1', 'value2']} />);
+
+      const textarea = wrapper.find('textarea');
+
+      textarea.simulate('keydown', { key: 'ArrowDown' });
+      expect(wrapper.state('currentValue')).to.equal('');
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      expect(wrapper.state('currentValue')).to.equal('value2');
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      expect(wrapper.state('currentValue')).to.equal('value1');
+    });
+
+    it('navigates forward back to currentValue', () => {
+      const onInput = sinon.spy();
+      const wrapper = shallow(<ShellInput onInput={onInput} initialHistory={['value1', 'value2']} />);
+
+      const textarea = wrapper.find('textarea');
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      expect(wrapper.state('currentValue')).to.equal('value2');
+
+      textarea.simulate('keydown', { key: 'ArrowDown' });
+      expect(wrapper.state('currentValue')).to.equal('');
+    });
+
+    it('does not commit "dirty" last value', () => {
+      // This may happen if i change the input, navigate up and then submit that entry.
+      // We do not want to submit the initially changed input.
+
+      const onInput = sinon.spy();
+      const wrapper = shallow(<ShellInput onInput={onInput} initialHistory={['value1', 'value2']} />);
+
+      const textarea = wrapper.find('textarea');
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      textarea.simulate('change', { target: { value: 'value3' } });
+      textarea.simulate('keyup', { key: 'Enter' });
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      expect(wrapper.state('currentValue')).to.equal('value3');
+
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      expect(wrapper.state('currentValue')).to.equal('value2');
+    });
+
+    it('does commit last value if navigated back', () => {
+      const onInput = sinon.spy();
+      const wrapper = shallow(<ShellInput onInput={onInput} initialHistory={['value1', 'value2']} />);
+
+      const textarea = wrapper.find('textarea');
+
+      textarea.simulate('change', { target: { value: 'value3' } });
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      textarea.simulate('keydown', { key: 'ArrowDown' });
+      textarea.simulate('keydown', { key: 'ArrowDown' });
+      textarea.simulate('keydown', { key: 'ArrowUp' });
+      textarea.simulate('keydown', { key: 'ArrowDown' });
+      textarea.simulate('keydown', { key: 'ArrowDown' });
+      expect(wrapper.state('currentValue')).to.equal('value3');
+    });
   });
 });
--- packages/browser-repl/src/components/shell-input.tsx
+++ packages/browser-repl/src/components/shell-input.tsx
@@ -1,45 +1,121 @@
+/* eslint-disable react/sort-comp */
 import React, { Component, ChangeEvent, KeyboardEvent } from 'react';
 import PropTypes from 'prop-types';
 
 interface ShellInputProps {
   onInput?(code: string): void | Promise<void>;
+  initialHistory?: string[];
 }
 
 interface ShellInputState {
   currentValue: string;
 }
+
 export class ShellInput extends Component<ShellInputProps, ShellInputState> {
   static propTypes = {
-    onInput: PropTypes.func
+    onInput: PropTypes.func,
+    history: PropTypes.arrayOf(PropTypes.string)
   };
 
   readonly state: ShellInputState = {
     currentValue: ''
   };
 
+  private history: string[];
+  private historyIndex: number;
+  private historyDirtyLastValue = false;
+
+  constructor(props) {
+    super(props);
+    this.history = props.initialHistory || [];
+    this.historyIndex = this.history.length;
+  }
+
   private onChange = (event: ChangeEvent<HTMLTextAreaElement>): void => {
     this.setState({currentValue: event.target.value});
   }
 
-  private onKeyUp = (event: KeyboardEvent<HTMLTextAreaElement>): void => {
-    if (this.state.currentValue === '') {
+  private commitValueToHistory(value: string): void {
+    if (this.historyDirtyLastValue) {
+      this.history.pop();
+    }
+
+    this.history.push(value);
+    this.historyIndex = this.history.length;
+    this.historyDirtyLastValue = false;
+  }
+
+  private historyBack(): void {
+    if (this.historyIndex <= 0) {
       return;
     }
 
-    if (this.isEnterWithoutShift(event)) {
-      this.props.onInput(this.state.currentValue);
-      this.setState({currentValue: ''});
+    if (this.historyIndex === this.history.length && !this.historyDirtyLastValue) {
+      this.historyDirtyLastValue = true;
+      this.history.push(this.state.currentValue);
     }
+
+    this.historyIndex--;
+    const previousValue = this.history[this.historyIndex];
+
+    this.setState({
+      currentValue: previousValue
+    });
+  }
+
+  private historyNext(): void {
+    if (this.historyIndex >= this.history.length - 1) {
+      return;
+    }
+
+    this.historyIndex++;
+
+    const nextValue = this.history[this.historyIndex];
+
+    this.setState({
+      currentValue: nextValue
+    });
+  }
+
+  private onInput = (currentValue: string): void => {
+    if (!currentValue || currentValue.trim() === '') {
+      return;
+    }
+
+    this.commitValueToHistory(currentValue);
+    this.props.onInput(currentValue);
+
+    this.setState({
+      currentValue: ''
+    });
+  }
+
+  private onKeyUp = (event: KeyboardEvent<HTMLTextAreaElement>): void => {
+    if (this.isEnterWithoutShift(event)) {
+      return this.onInput(this.state.currentValue);
+    }
+  }
+
+  private onKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>): void => {
+    this.preventDefaultEnterBehaviour(event);
+
+    if (event.key === 'ArrowUp') {
+      return this.historyBack();
+    }
+
+    if (event.key === 'ArrowDown') {
+      return this.historyNext();
+    }
+  }
+
+  private isEnterWithoutShift = (event: KeyboardEvent<HTMLTextAreaElement>): boolean => {
+    return event.key === 'Enter' && !event.shiftKey;
   }
 
   private preventDefaultEnterBehaviour = (event: KeyboardEvent<HTMLTextAreaElement>): void => {
     if (this.isEnterWithoutShift(event)) {
       event.preventDefault();
     }
-  }
-
-  private isEnterWithoutShift = (event: KeyboardEvent<HTMLTextAreaElement>): boolean => {
-    return event.key === 'Enter' && !event.shiftKey;
   }
 
   render(): JSX.Element {
@@ -51,7 +127,7 @@ export class ShellInput extends Component<ShellInputProps, ShellInputState> {
       value={this.state.currentValue}
       onChange={this.onChange}
       onKeyUp={this.onKeyUp}
-      onKeyDown={this.preventDefaultEnterBehaviour}
+      onKeyDown={this.onKeyDown}
       onKeyPress={this.preventDefaultEnterBehaviour} />);
   }
 }
