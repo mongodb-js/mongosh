4175bf815404a75e2a2cd30519cd0eb3e0e249c7
Irina Shestak <shestak.irina@gmail.com>
2020-01-30 11:18:26 +0100

    extract change history to its own exportable file

---
+++ packages/cli-repl/src/history.ts
@@ -0,0 +1,19 @@
+/**
+ * Modifies command history array based on sensitive information.
+ * If redact option is passed, also redacts sensitive info.
+ *
+ * @param {array} History - Array of commands, where the first command is the
+ * most recent.
+ *
+ * @param {boolean} Redact - Option to redact sensitive info.
+ */
+function changeHistory(history: string[], redact: boolean = false) {
+  const hiddenCommands =
+    RegExp('createUser|auth|updateUser|changeUserPassword', 'ig');
+
+  if (hiddenCommands.test(history[0])) {
+    history.shift();
+  }
+}
+
+export default changeHistory;
