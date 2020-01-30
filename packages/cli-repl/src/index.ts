1bb74aa9cd40c9a705fb173482d691fc28be0a14
Irina Shestak <shestak.irina@gmail.com>
2020-01-30 15:40:33 +0100

    add completer and changeHistory to cli-repl exports

--- packages/cli-repl/src/index.ts
+++ packages/cli-repl/src/index.ts
@@ -2,6 +2,8 @@ import CliRepl from './cli-repl';
 import parseCliArgs from './arg-parser';
 import mapCliToDriver from './arg-mapper';
 import generateUri from './uri-generator';
+import completer from './completer';
+import changeHistory from './history';
 
 export default CliRepl;
-export { CliRepl, parseCliArgs, mapCliToDriver, generateUri };
+export { CliRepl, parseCliArgs, mapCliToDriver, generateUri, completer, changeHistory };
