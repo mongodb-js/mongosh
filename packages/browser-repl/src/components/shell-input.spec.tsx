d51609b1b122b5413a00391250591faea3a0347a
mcasimir <maurizio.cas@gmail.com>
2020-01-31 16:07:53 +0100

    feat(browser-repl): use to ace editor

--- packages/browser-repl/src/components/shell-input.spec.tsx
+++ packages/browser-repl/src/components/shell-input.spec.tsx
@@ -5,40 +5,28 @@ import { expect } from '../../testing/chai';
 import { shallow } from '../../testing/enzyme';
 
 import { ShellInput } from './shell-input';
+import { Editor } from './editor';
 
 function changeValue(wrapper, value): void {
-  wrapper.find('textarea').simulate('change', { target: { value } });
+  wrapper.find(Editor).prop('onChange')(value);
 }
 
-function simulateTextareaKeyEvent(wrapper, eventType, key, event: any = {}): void {
-  wrapper.find('textarea').simulate(eventType, {
-    key,
-    ...event,
-    target: {
-      value: wrapper.state('currentValue'),
-      selectionStart: 0,
-      selectionEnd: 0,
-      ...(event.target || {})
-    }
-  });
+function enter(wrapper): void {
+  wrapper.find(Editor).prop('onEnter')();
 }
 
-function enter(wrapper, event: any = {}): void {
-  simulateTextareaKeyEvent(wrapper, 'keyup', 'Enter', event);
+function arrowUpOnFirstLine(wrapper): void {
+  wrapper.find(Editor).prop('onArrowUpOnFirstLine')();
 }
 
-function arrowUp(wrapper, event: any = {}): void {
-  simulateTextareaKeyEvent(wrapper, 'keydown', 'ArrowUp', event);
-}
-
-function arrowDown(wrapper, event: any = {}): void {
-  simulateTextareaKeyEvent(wrapper, 'keydown', 'ArrowDown', event);
+function arrowDownOnLastLine(wrapper): void {
+  wrapper.find(Editor).prop('onArrowDownOnLastLine')();
 }
 
 describe('<ShellInput />', () => {
-  it('renders an input', () => {
+  it('renders an editor', () => {
     const wrapper = shallow(<ShellInput />);
-    expect(wrapper.find('textarea')).to.have.lengthOf(1);
+    expect(wrapper.find('Editor')).to.have.lengthOf(1);
   });
 
   it('calls onInput with the current value when enter is pressed', () => {
@@ -51,16 +39,6 @@ describe('<ShellInput />', () => {
     expect(onInput).to.have.been.calledWith('value');
   });
 
-  it('does not call onInput with the current value when enter is pressed with shift', () => {
-    const onInput = sinon.spy();
-    const wrapper = shallow(<ShellInput onInput={onInput}/>);
-
-    changeValue(wrapper, 'value');
-    enter(wrapper, {shiftKey: true});
-
-    expect(onInput).to.not.have.been.called;
-  });
-
   it('does not call onInput if the input is empty', () => {
     const onInput = sinon.spy();
     const wrapper = shallow(<ShellInput onInput={onInput}/>);
@@ -68,78 +46,70 @@ describe('<ShellInput />', () => {
     expect(onInput).to.not.have.been.called;
   });
 
-  it.skip('does not add new line to the output when enter is pressed', () => {
-    // TODO: hard/impossible to test with enzyme
-  });
-
-  it.skip('allows newline when shift+enter is pressed', () => {
-    // TODO: hard/impossible to test with enzyme
-  });
-
   describe('history', () => {
     it('navigates history backward on ArrowUp', () => {
       const wrapper = shallow(<ShellInput initialHistory={['value1', 'value2']} />);
 
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value2');
 
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value1');
     });
 
     it('navigates history backward and stops on first element', () => {
       const wrapper = shallow(<ShellInput initialHistory={['value1']} />);
 
-      arrowUp(wrapper);
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value1');
     });
 
     it('navigates history forward', () => {
       const wrapper = shallow(<ShellInput initialHistory={['value1', 'value2']} />);
 
-      arrowUp(wrapper);
-      arrowUp(wrapper);
-      arrowDown(wrapper);
+      arrowUpOnFirstLine(wrapper);
+      arrowUpOnFirstLine(wrapper);
+      arrowDownOnLastLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value2');
     });
 
     it('does not move the history index past the last element', () => {
       const wrapper = shallow(<ShellInput initialHistory={['value1', 'value2']} />);
 
-      arrowDown(wrapper);
+      arrowDownOnLastLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('');
 
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value2');
 
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value1');
     });
 
     it('navigates forward back to currentValue', () => {
       const wrapper = shallow(<ShellInput initialHistory={['value1', 'value2']} />);
 
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value2');
 
-      arrowDown(wrapper);
+      arrowDownOnLastLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('');
     });
 
     it('navigates forward back to current value after change', () => {
       const wrapper = shallow(<ShellInput initialHistory={['value1', 'value2']} />);
 
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value2');
 
-      arrowDown(wrapper);
+      arrowDownOnLastLine(wrapper);
       changeValue(wrapper, 'value3');
 
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value2');
 
-      arrowDown(wrapper);
+      arrowDownOnLastLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value3');
     });
 
@@ -150,10 +120,10 @@ describe('<ShellInput />', () => {
       const wrapper = shallow(<ShellInput initialHistory={['value1', 'value2']} />);
 
       changeValue(wrapper, 'value3');
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       enter(wrapper);
 
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value2');
     });
 
@@ -161,50 +131,12 @@ describe('<ShellInput />', () => {
       const wrapper = shallow(<ShellInput initialHistory={['value1', 'value2']} />);
 
       changeValue(wrapper, 'value3');
-      arrowUp(wrapper);
-      arrowDown(wrapper);
+      arrowUpOnFirstLine(wrapper);
+      arrowDownOnLastLine(wrapper);
       enter(wrapper);
 
-      arrowUp(wrapper);
+      arrowUpOnFirstLine(wrapper);
       expect(wrapper.state('currentValue')).to.equal('value3');
-    });
-
-    it('does not navigate back unless cursor is on first line', () => {
-      const wrapper = shallow(<ShellInput initialHistory={['value1', 'value2']} />);
-
-      changeValue(wrapper, '1\n2\n3');
-
-      arrowUp(wrapper, { target: { selectionStart: 4, selectionEnd: 4 } });
-      expect(wrapper.state('currentValue')).to.equal('1\n2\n3');
-
-      arrowUp(wrapper, { target: { selectionStart: 0, selectionEnd: 0 } });
-      expect(wrapper.state('currentValue')).to.equal('value2');
-    });
-
-    it('does not navigate forward unless cursor is on last line', () => {
-      const wrapper = shallow(<ShellInput initialHistory={['1\n2\n3']} />);
-
-      arrowUp(wrapper);
-      expect(wrapper.state('currentValue')).to.equal('1\n2\n3');
-
-      arrowDown(wrapper);
-      expect(wrapper.state('currentValue')).to.equal('1\n2\n3');
-
-      arrowDown(wrapper, { target: { selectionStart: 6, selectionEnd: 6 } });
-      expect(wrapper.state('currentValue')).to.equal('');
-    });
-
-    it('does not navigate back or forward if text is selected', () => {
-      const wrapper = shallow(<ShellInput initialHistory={['value1', 'value2']} />);
-
-      arrowUp(wrapper);
-      expect(wrapper.state('currentValue')).to.equal('value2');
-
-      arrowDown(wrapper, { target: { selectionStart: 1, selectionEnd: 3 } });
-      expect(wrapper.state('currentValue')).to.equal('value2');
-
-      arrowUp(wrapper, {target: { selectionStart: 1, selectionEnd: 3 }});
-      expect(wrapper.state('currentValue')).to.equal('value2');
     });
   });
 });
--- packages/browser-repl/src/components/shell-input.tsx
+++ packages/browser-repl/src/components/shell-input.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/sort-comp */
-import React, { Component, ChangeEvent, KeyboardEvent } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Editor } from './editor';
+import { History } from './history';
 
 interface ShellInputProps {
   onInput?(code: string): void | Promise<void>;
@@ -21,77 +23,48 @@ export class ShellInput extends Component<ShellInputProps, ShellInputState> {
     currentValue: ''
   };
 
-  private history: string[];
-  private historyIndex: number;
-  private historyDirtyLastValue = false;
+  private history: History;
 
   constructor(props) {
     super(props);
-    this.history = props.initialHistory || [];
-    this.historyIndex = this.history.length;
+    this.history = new History(props.initialHistory || []);
   }
 
-  private onChange = (event: ChangeEvent<HTMLTextAreaElement>): void => {
-    this.setState({currentValue: event.target.value});
+  private onChange = (value: string): void => {
+    this.setState({currentValue: value});
   }
 
-  private commitValueToHistory(value: string): void {
-    if (this.historyDirtyLastValue) {
-      this.history.pop();
-    }
+  private historyBack = (): void => {
+    const value = this.history.back(this.state.currentValue);
 
-    this.history.push(value);
-    this.historyIndex = this.history.length;
-    this.historyDirtyLastValue = false;
-  }
-
-  private historyBack(): void {
-    if (this.historyIndex <= 0) {
+    if (value === undefined) {
       return;
     }
 
-    if (this.historyDirtyLastValue && this.historyIndex === this.history.length - 1) {
-      // if already went up once and back down we will
-      // replace the last entry
-      this.history.pop();
-    }
-
-    if (this.historyIndex === this.history.length) {
-      // if we are in the new line
-      // we save the entry in the history temporarily
-
-      this.historyDirtyLastValue = true;
-      this.history.push(this.state.currentValue);
-    }
-
-    this.historyIndex--;
-    const previousValue = this.history[this.historyIndex];
-
     this.setState({
-      currentValue: previousValue
+      currentValue: value
     });
   }
 
-  private historyNext(): void {
-    if (this.historyIndex >= this.history.length - 1) {
+  private historyNext = (): void => {
+    const value = this.history.next();
+
+    if (value === undefined) {
       return;
     }
 
-    this.historyIndex++;
-
-    const nextValue = this.history[this.historyIndex];
-
     this.setState({
-      currentValue: nextValue
+      currentValue: value
     });
   }
 
-  private onInput = (currentValue: string): void => {
+  private onEnter = (): void => {
+    const currentValue = this.state.currentValue;
     if (!currentValue || currentValue.trim() === '') {
       return;
     }
 
-    this.commitValueToHistory(currentValue);
+    this.history.commit(currentValue);
 
     if (this.props.onInput) {
       this.props.onInput(currentValue);
@@ -102,83 +75,13 @@ export class ShellInput extends Component<ShellInputProps, ShellInputState> {
     });
   }
 
-  private onKeyUp = (event: KeyboardEvent<HTMLTextAreaElement>): void => {
-    if (this.isEnterWithoutShift(event)) {
-      return this.onInput(this.state.currentValue);
-    }
-  }
-
-  private onKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>): void => {
-    this.preventDefaultEnterBehaviour(event);
-
-    if (event.key === 'ArrowUp' && this.isCursorPositionFirstLine(event)) {
-      return this.historyBack();
-    }
-
-    if (event.key === 'ArrowDown' && this.isCursorPositionLastLine(event)) {
-      return this.historyNext();
-    }
-  }
-
-  private isEnterWithoutShift = (event: KeyboardEvent<HTMLTextAreaElement>): boolean => {
-    return event.key === 'Enter' && !event.shiftKey;
-  }
-
-  private preventDefaultEnterBehaviour = (event: KeyboardEvent<HTMLTextAreaElement>): void => {
-    if (this.isEnterWithoutShift(event)) {
-      event.preventDefault();
-    }
-  }
-
-  private hasSelection = (event): boolean => {
-    const textarea = event.target;
-    const selectionStart = textarea.selectionStart;
-    const selectionEnd = textarea.selectionEnd;
-
-    return selectionStart !== selectionEnd;
-  }
-
-  private isCursorPositionFirstLine = (event): boolean => {
-    if (this.hasSelection(event)) {
-      return false;
-    }
-
-    const { currentLine } = this.getCursorLine(event);
-    return currentLine === 0;
-  }
-
-  private isCursorPositionLastLine = (event): boolean => {
-    if (this.hasSelection(event)) {
-      return false;
-    }
-
-    const { currentLine, totalLines } = this.getCursorLine(event);
-    return currentLine === totalLines;
-  }
-
-  private getCursorLine(event: KeyboardEvent<HTMLTextAreaElement>): {
-    currentLine: number;
-    totalLines: number;
-  } {
-    const textarea = event.target as HTMLTextAreaElement;
-    const selectionStart = textarea.selectionStart;
-    const value = textarea.value || '';
-    const currentLine = value.substr(0, selectionStart).split('\n').length - 1;
-    const totalLines = value.split('\n').length - 1;
-    return { currentLine, totalLines };
-  }
-
   render(): JSX.Element {
-    return (<textarea
-      style={{
-        width: '100%',
-        resize: 'none'
-      }}
-      rows={5}
+    return (<Editor
       value={this.state.currentValue}
       onChange={this.onChange}
-      onKeyUp={this.onKeyUp}
-      onKeyDown={this.onKeyDown}
-      onKeyPress={this.preventDefaultEnterBehaviour} />);
+      onEnter={this.onEnter}
+      onArrowUpOnFirstLine={this.historyBack}
+      onArrowDownOnLastLine={this.historyNext}
+    />);
   }
 }
