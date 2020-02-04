792d9e451e392c98cfeddc6eba70a098d502d6c7
mcasimir <maurizio.cas@gmail.com>
2020-02-04 19:04:10 +0100

    test(compass-shell): add tests for component

--- packages/compass-shell/test/karma-setup.js
+++ packages/compass-shell/test/karma-setup.js
@@ -3,6 +3,6 @@ const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const context = require.context('.', true, /\.spec\.js$/);
+const context = require.context('.', true, /\.spec\.jsx?$/);
 
 context.keys().forEach(context);
