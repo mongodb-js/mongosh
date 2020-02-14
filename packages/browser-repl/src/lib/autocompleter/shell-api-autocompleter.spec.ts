bbfe11be0323c78674f997e0f6f834616eae28b3
mcasimir <maurizio.cas@gmail.com>
2020-02-14 16:43:17 +0100

    test(browser-repl): test autocompleter

---
+++ packages/browser-repl/src/lib/autocompleter/shell-api-autocompleter.spec.ts
@@ -0,0 +1,32 @@
+import { ShellApiAutocompleter } from './shell-api-autocompleter';
+import { expect } from '../../../testing/chai';
+
+describe('Autocompleter', () => {
+  describe('getCompletions', () => {
+    let autocompleter;
+
+    beforeEach(() => {
+      autocompleter = new ShellApiAutocompleter('4.2.1');
+    });
+
+    it('returns completions for text before cursor', async() => {
+      const completions = await autocompleter.getCompletions('db.coll1.');
+
+      expect(completions).to.deep.contain({
+        caption: 'db.coll1.find',
+        value: 'find'
+      });
+    });
+
+    it('returns full completion value with text after dot', async() => {
+      const completions = await autocompleter.getCompletions('db.coll1.f');
+
+      expect(completions).to.deep.contain({
+        caption: 'db.coll1.find',
+        value: 'find'
+      });
+    });
+  });
+});
+
+
