63550284644f690cfde7673f699137444c487403
mcasimir <maurizio.cas@gmail.com>
2020-02-18 19:56:24 +0100

    feat(compass-shell): store and retrieve history

--- packages/compass-shell/src/components/compass-shell/compass-shell.spec.js
+++ packages/compass-shell/src/components/compass-shell/compass-shell.spec.js
@@ -1,17 +1,78 @@
+import sinon from 'sinon';
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Shell } from 'mongosh-browser-repl';
 import { CompassShell } from './compass-shell';
 
+function updateAndWaitAsync(wrapper) {
+  wrapper.update();
+  return new Promise(setImmediate);
+}
+
 describe('CompassShell', () => {
-  it('renders shell if runtime is passed as prop', () => {
-    const fakeRuntime = {};
-    const wrapper = shallow(<CompassShell runtime={fakeRuntime}/>);
-    expect(wrapper.find(Shell).prop('runtime')).to.equal(fakeRuntime);
+  context('when runtime property is not present', () => {
+    it('does not renders a shell if runtime is null', () => {
+      const wrapper = shallow(<CompassShell runtime={null} />);
+      expect(wrapper.find(Shell)).to.have.lengthOf(0);
+    });
   });
 
-  it('does not renders a shell if runtime is null', () => {
-    const wrapper = shallow(<CompassShell runtime={null}/>);
-    expect(wrapper.find(Shell)).to.have.lengthOf(0);
+  context('when runtime property is present', () => {
+    it('renders the Shell', () => {
+      const fakeRuntime = {};
+      const wrapper = shallow(<CompassShell runtime={fakeRuntime} />);
+      expect(wrapper.find(Shell).prop('runtime')).to.equal(fakeRuntime);
+    });
+  });
+
+  context('when historyStorage is not present', () => {
+    it('passes initialHistory={[]} to Shell', () => {
+      const fakeRuntime = {};
+      const wrapper = shallow(<CompassShell runtime={fakeRuntime} />);
+
+      expect(wrapper.find(Shell).prop('initialHistory')).to.deep.equal([]);
+    });
+  });
+
+  context('when historyStorage is present', () => {
+    let fakeStorage;
+    let fakeRuntime;
+
+    beforeEach(() => {
+      fakeStorage = {
+        load: sinon.spy(() => Promise.resolve([])),
+        save: sinon.spy(() => Promise.resolve()),
+      };
+
+      fakeRuntime = {};
+    });
+
+    it('passes loadedHistory as initialHistory to Shell', async() => {
+      fakeStorage.load = sinon.spy(() => Promise.resolve(['line1']));
+
+      const wrapper = shallow(<CompassShell
+        runtime={fakeRuntime}
+        historyStorage={fakeStorage} />);
+
+      await updateAndWaitAsync(wrapper);
+
+      expect(wrapper.find(Shell).prop('initialHistory')).to.deep.equal(['line1']);
+    });
+
+    it('calls historyStorage.save when history changes', async() => {
+      const wrapper = shallow(<CompassShell
+        runtime={fakeRuntime}
+        historyStorage={fakeStorage} />);
+
+      await updateAndWaitAsync(wrapper);
+
+      const onHistoryChanged = wrapper.find(Shell).prop('onHistoryChanged');
+      onHistoryChanged(['line1']);
+
+      expect(
+        fakeStorage.save.calledWith(['line1'])
+      ).to.equal(true);
+    });
   });
 });
+
--- packages/compass-shell/src/plugin.js
+++ packages/compass-shell/src/plugin.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import CompassShell from 'components/compass-shell';
 import CompassShellStore from 'stores';
+import { getUserDataFilePath } from 'modules/get-user-data-file-path';
+import { HistoryStorage } from 'modules/history-storage';
 
 function createPlugin() {
   const store = new CompassShellStore();
   const Plugin = class extends Component {
     static displayName = 'CompassShellPlugin';
+
+    initializeHistoryStorage() {
+      const historyFilePath = getUserDataFilePath('shell-history.json');
+
+      if (!historyFilePath) {
+        return;
+      }
+
+      return new HistoryStorage(historyFilePath);
+    }
 
     /**
      * Connect the Plugin to the store and render.
@@ -16,7 +28,7 @@ function createPlugin() {
     render() {
       return (
         <Provider store={store.reduxStore}>
-          <CompassShell />
+          <CompassShell historyStorage={this.initializeHistoryStorage()} />
         </Provider>
       );
     }
