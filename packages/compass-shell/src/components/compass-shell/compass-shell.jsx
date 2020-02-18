57ca6ceff4651485fa19a68d12869c9d25679b0b
mcasimir <maurizio.cas@gmail.com>
2020-02-18 19:56:24 +0100

    feat(compass-shell): store and retrieve history

--- packages/compass-shell/src/components/compass-shell/compass-shell.jsx
+++ packages/compass-shell/src/components/compass-shell/compass-shell.jsx
@@ -7,15 +7,58 @@ import { connect } from 'react-redux';
 import { Shell } from 'mongosh-browser-repl';
 
 export class CompassShell extends Component {
-  static displayName = 'CompassShellComponent';
-
   static propTypes = {
-    runtime: PropTypes.object
+    runtime: PropTypes.object,
+    historyStorage: PropTypes.object
   };
 
   static defaultProps = {
     runtime: null
   };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialHistory: this.props.historyStorage ? null : []
+    };
+  }
+
+  componentDidMount() {
+    this.loadHistory();
+  }
+
+  saveHistory = async(history) => {
+    if (!this.props.historyStorage) {
+      return;
+    }
+
+    try {
+      await this.props.historyStorage.save(history);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  }
+
+  loadHistory = async() => {
+    if (!this.props.historyStorage) {
+      return;
+    }
+
+    try {
+      const history = await this.props.historyStorage.load();
+      this.setState({
+        initialHistory: history
+      });
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      this.setState({
+        initialHistory: []
+      });
+    }
+  }
 
   /**
    * Render CompassShell component.
@@ -23,13 +66,15 @@ export class CompassShell extends Component {
    * @returns {React.Component} The rendered component.
    */
   render() {
-    if (!this.props.runtime) {
+    if (!this.props.runtime || !this.state.initialHistory) {
       return (<div />);
     }
 
     return (
       <div className={classnames(styles.container)}>
-        <Shell runtime={this.props.runtime} />
+        <Shell runtime={this.props.runtime}
+          initialHistory={this.state.initialHistory}
+          onHistoryChanged={this.saveHistory} />
       </div>
     );
   }
