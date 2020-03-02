f5d796505491ed67d0a5304394fb9bb4d735ab51
mcasimir <maurizio.cas@gmail.com>
2020-03-02 17:20:48 +0100

    feat(cli-repl): add name and version to metadata

--- packages/cli-repl/bin/mongosh.js
+++ packages/cli-repl/bin/mongosh.js
@@ -5,15 +5,18 @@ process.title = 'mongosh';
 
 try {
   const options = parseCliArgs(process.argv);
+  const { version } = require('../package.json');
 
   if (options.help) {
     console.log(USAGE);
   } else if (options.version) {
-    console.log(require('../package.json').version);
+    console.log(version);
   } else {
     const driverOptions = mapCliToDriver(options);
     const driverUri = generateUri(options);
-    new CliRepl(driverUri, driverOptions, options);
+    const appname = `${process.title} ${version}`;
+
+    new CliRepl(driverUri, {appname, ...driverOptions}, options);
   }
 } catch (e) {
   console.log(e.message);
