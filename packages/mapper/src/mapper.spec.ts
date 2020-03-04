1f368b357fc4dfd9bf52df2726d1883e8c975683
mcasimir <maurizio.cas@gmail.com>
2020-03-04 19:20:51 +0100

    test(mapper): test it command

--- packages/mapper/src/mapper.spec.ts
+++ packages/mapper/src/mapper.spec.ts
@@ -1,5 +1,4 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
 import Mapper from './mapper';
 
 describe('Mapper', () => {
@@ -7,17 +6,14 @@ describe('Mapper', () => {
   let serviceProvider;
 
   beforeEach(() => {
-    serviceProvider = {
-      listDatabases: sinon.spy()
-    };
-
+    serviceProvider = {};
     mapper = new Mapper(serviceProvider);
   });
 
   describe('commands', () => {
     describe('show databases', () => {
       it('lists databases', async() => {
-        serviceProvider.listDatabases = (): any => Promise.resolve({
+        serviceProvider.listDatabases = (): any => (Promise.resolve({
           databases: [
             { name: 'db1', sizeOnDisk: 10000, empty: false },
             { name: 'db2', sizeOnDisk: 20000, empty: false },
@@ -25,7 +21,7 @@ describe('Mapper', () => {
           ],
           totalSize: 50000,
           ok: 1
-        });
+        }));
 
         const expectedOutput = `db1  10 kB
 db2  20 kB
@@ -40,5 +36,66 @@ db3  30 kB`;
         ).to.equal(expectedOutput);
       });
     });
+
+    describe('it', () => {
+      describe('when cursor is not present', () => {
+        it('returns an empty CursorIterationResult', async() => {
+          const result = await mapper.it();
+          expect(result.shellApiType()).to.equal('CursorIterationResult');
+          expect(result).to.have.lengthOf(0);
+        });
+      });
+
+      describe('when cursor is present', () => {
+        let cursor;
+
+        beforeEach(async() => {
+          cursor = {
+            isClosed: (): boolean => false,
+            hasNext: (): Promise<boolean> => Promise.resolve(true),
+            next: (): Promise<any> => Promise.resolve({})
+          };
+
+          serviceProvider.find = (): any => cursor;
+          await mapper.find({}, {}, {});
+        });
+
+        it('returns CursorIterationResult', async() => {
+          const result = await mapper.it();
+          expect(result.shellApiType()).to.equal('CursorIterationResult');
+        });
+
+        it('returns the next 20 documents', async() => {
+          const result = await mapper.it();
+          expect(result).to.have.lengthOf(20);
+        });
+
+        describe('when hasNext returns false', () => {
+          beforeEach(() => {
+            let i = 3;
+            cursor.hasNext = (): Promise<boolean> => Promise.resolve(i-- > 0);
+          });
+
+          it('stops', async() => {
+            const result = await mapper.it();
+            expect(result).to.have.lengthOf(3);
+          });
+        });
+
+        describe('when invoked with a closed cursor', () => {
+          beforeEach(() => {
+            cursor.isClosed = (): boolean => true;
+            cursor.hasNext = (): any => { throw new Error(''); };
+          });
+
+          it('returns an empty CursorIterationResult', async() => {
+            const result = await mapper.it();
+            expect(result.shellApiType()).to.equal('CursorIterationResult');
+            expect(result).to.have.lengthOf(0);
+          });
+        });
+      });
+    });
   });
 });
+
