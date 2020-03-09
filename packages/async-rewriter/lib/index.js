209c8234dfcbf1ff5f8466cbae6a2e8ac4aee7d9
aherlihy <anna.herlihy@10gen.com>
2020-03-09 14:29:25 +0100

    AsyncWriter to TypeScript

--- packages/async-rewriter/lib/index.js
+++ packages/async-rewriter/lib/index.js
@@ -1,8 +1,13 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var async_rewrite_1 = __importDefault(require("./async-rewrite"));
-exports.default = async_rewrite_1.default;
+var async_rewriter_1 = __importStar(require("./async-rewriter"));
+exports.SymbolTable = async_rewriter_1.SymbolTable;
+exports.default = async_rewriter_1.default;
 //# sourceMappingURL=index.js.map
