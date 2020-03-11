b3adef60cf9d28f8081bf5791a00c483a16a70d5
aherlihy <anna.herlihy@10gen.com>
2020-03-11 13:52:55 +0100

    small transform

--- packages/async-rewriter/src/async-writer-babel.ts
+++ packages/async-rewriter/src/async-writer-babel.ts
@@ -1,19 +1,21 @@
 import * as babel from '@babel/core';
-const plugin = () => {
-  return {
-    visitor: {
-      AssignmentExpression(path) {
-        path.node.right.value = 'rewritten value';
-      }
-    }
-  };
-};
 
-export default (code) => {
-  console.log(`rewriting ${code}`);
-  return babel.transform(code, {
-    plugins: [plugin],
-    code: true,
-    ast: true,
-  });
+export default (symbols, types) => {
+  const plugin = ({ types: t }) => {
+    return {
+      visitor: {
+        AssignmentExpression(path): any {
+          symbols.add('test', {});
+          path.node.right.value = 'rewritten value';
+        }
+      }
+    };
+  };
+  return (code) => {
+    return babel.transform(code, {
+      plugins: [plugin],
+      code: true,
+      ast: true,
+    }).code;
+  };
 };
