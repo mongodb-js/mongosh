55e7efbef8885012d9de7ed856f26219eb62d172
aherlihy <anna.herlihy@10gen.com>
2020-03-11 13:52:55 +0100

    small transform

--- packages/async-rewriter/local-test.js
+++ packages/async-rewriter/local-test.js
@@ -1,9 +1,14 @@
-// const { types } = require('mongosh-shell-api');
-//
+const SymbolTable = require('./lib/symbol-table').default;
+
+const { types } = require('mongosh-shell-api');
+
 // const AsyncWriter = require('./lib/async-writer-antlr.js').default;
-//
-// const writer = new AsyncWriter(types);
+const getWriter = require('./lib/async-writer-babel.js').default;
+
+const symbols = new SymbolTable({}, types);
+const writer = getWriter(symbols, types);
 // writer.symbols.add('db', types.Database);
+console.log(writer('x = "old value"'));
 // console.log(writer.compile('db.coll.insertOne({})'));
 //
 // console.log(writer.compile('y = db'));
@@ -11,7 +16,5 @@
 //
 // console.log(writer.compile('function returnsDb() { return db; }'));
 // console.log(writer.compile('returnsDb().coll.insertOne({})'));
-// writer.symbols.print();
+symbols.print();
 
-const writer = require('./lib/async-writer-babel.js').default;
-console.log(writer('x = "initial"').code);
