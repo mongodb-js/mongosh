cc52430b136f627f489a3ce3ce9b4faec215f441
aherlihy <anna.herlihy@10gen.com>
2020-03-12 15:14:53 +0100

    infertype v1

--- packages/async-rewriter/local-test.js
+++ packages/async-rewriter/local-test.js
@@ -1,14 +1,15 @@
 const SymbolTable = require('./lib/symbol-table').default;
+const getWriter = require('./lib/async-writer-babel.js').default;
 
 const { types } = require('mongosh-shell-api');
 
-// const AsyncWriter = require('./lib/async-writer-antlr.js').default;
-const getWriter = require('./lib/async-writer-babel.js').default;
 
-const symbols = new SymbolTable({}, types);
+// const AsyncWriter = require('./lib/async-writer-antlr.js').default;
+
+const symbols = new SymbolTable({ db: types.Database }, types);
 const writer = getWriter(symbols, types);
-// writer.symbols.add('db', types.Database);
-console.log(writer('x = "old value"'));
+const input = 'db.coll.insertOne()';
+console.log(`"${input}" ==> "${writer(input)}"`);
 // console.log(writer.compile('db.coll.insertOne({})'));
 //
 // console.log(writer.compile('y = db'));
@@ -16,5 +17,5 @@ console.log(writer('x = "old value"'));
 //
 // console.log(writer.compile('function returnsDb() { return db; }'));
 // console.log(writer.compile('returnsDb().coll.insertOne({})'));
-symbols.print();
+// symbols.print();
 
