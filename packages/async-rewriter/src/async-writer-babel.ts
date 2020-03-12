5773c9e1e02860a7a30c5c5821c8554ebcee7741
aherlihy <anna.herlihy@10gen.com>
2020-03-12 15:14:53 +0100

    infertype v1

--- packages/async-rewriter/src/async-writer-babel.ts
+++ packages/async-rewriter/src/async-writer-babel.ts
@@ -1,13 +1,64 @@
 import * as babel from '@babel/core';
+import printAST from 'ast-pretty-print';
 
-export default (symbols, types) => {
+export default (symbols, shellTypes) => {
+  const inferTypeVisitor = {
+    exit(path) {
+      let printStr = `INFER: ${path.node.type}`;
+      let type = path.node.shellType;
+      if (this.t.isIdentifier(path.node)) {
+        printStr = `${printStr} name=${path.node.name}`;
+        const id = path.node.name;
+        type = symbols.lookup(id);
+        if (typeof type === 'string') {
+          type = shellTypes[type];
+        }
+      } else if (this.t.isMemberExpression(path.node)) {
+        const rhs = path.node.property.name;
+        const lhsType = path.node.object.shellType;
+        printStr = `${printStr} lhsType=${lhsType.type} and attr=${rhs}`;
+        if (lhsType.attributes === undefined) {
+          type = shellTypes.unknown;
+        } else if (rhs in lhsType.attributes) {
+          type = lhsType.attributes[rhs];
+        } else if (lhsType.type === 'Database') {
+          type = shellTypes.Collection;
+        }
+      } else if (type === undefined) {
+        type = shellTypes.unknown;
+      }
+      path.node.shellType = type;
+      path.findParent(()=>true).node.shellType = type;
+      console.log(`${printStr} ==> ${type.type}`);
+    }
+  };
+
   const plugin = ({ types: t }) => {
     return {
+      // pre(state) {},
+      post(state) {
+        console.log('transformed AST:');
+        // console.log(printAST(state.ast));
+      },
       visitor: {
-        AssignmentExpression(path): any {
-          symbols.add('test', {});
-          path.node.right.value = 'rewritten value';
-        }
+        CallExpression: {
+          exit(path) {
+            const opts = { t: t };
+            path.traverse(inferTypeVisitor, opts);
+            console.log('after traversing with inferType, shellType=');
+            console.log(path.node.shellType);
+            if (path.node.shellType.type === 'function' && path.node.shellType.returnsPromise) {
+              path.replaceWith(t.awaitExpression(path.node));
+              path.skip();
+            }
+          }
+        },
+        // AssignmentExpression(path) {
+        //   // const lhs = path.left;
+        //   // const rhs = path.right;
+        //   // console.log(rhs);
+        //   // symbols.add(lhs, {});
+        // }
       }
     };
   };
@@ -15,7 +66,7 @@ export default (symbols, types) => {
     return babel.transform(code, {
       plugins: [plugin],
       code: true,
-      ast: true,
+      ast: true
     }).code;
   };
 };
