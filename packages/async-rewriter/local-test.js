0a157d1bdd360a3a98eaa9b12073d2babf1c4d67
aherlihy <anna.herlihy@10gen.com>
2020-03-16 13:28:17 +0100

    Fix up async-writer api

--- packages/async-rewriter/local-test.js
+++ packages/async-rewriter/local-test.js
@@ -1,11 +1,10 @@
 /* eslint no-console:0 */
 const SymbolTable = require('./lib/symbol-table').default;
-const getWriter = require('./lib/async-writer-babel.js').default;
+const AsyncWriter = require('./lib/async-writer-babel.js').default;
 
 const { types } = require('mongosh-shell-api');
 
-const symbols = new SymbolTable({ db: types.Database }, types);
-const writer = getWriter(symbols);
+const writer = new AsyncWriter({ db: types.Database }, types);
 const input = [
   'db.coll.insertOne()',
   'y = db',
@@ -16,6 +15,6 @@ const input = [
   'x().coll.insertOne()'
 ];
 
-input.forEach((i) => console.log(`"${i}" ==> "${writer(i)}"`));
-symbols.print();
+input.forEach((i) => console.log(`"${i}" ==> "${writer.compile(i)}"`));
+writer.symbols.print();
 
--- packages/async-rewriter/src/index.ts
+++ packages/async-rewriter/src/index.ts
@@ -1,5 +1,2 @@
 import AsyncWriter from './async-writer-babel';
-import SymbolTable from './symbol-table';
-
-export { SymbolTable };
 export default AsyncWriter;
