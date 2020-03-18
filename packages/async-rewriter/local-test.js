6698a6675dadcc7c624b2fb6ac1c9d529751d4a9
aherlihy <anna.herlihy@10gen.com>
2020-03-18 18:27:31 +0100

    More tests

--- packages/async-rewriter/local-test.js
+++ packages/async-rewriter/local-test.js
@@ -4,19 +4,33 @@ const AsyncWriter = require('./lib/async-writer-babel.js').default;
 
 const { types } = require('mongosh-shell-api');
 
-const writer = new AsyncWriter({ db: types.Database }, types);
+const empty_types = { unknown: types.unknown };
+
+const sinon = require('sinon');
+
+const scope = [
+  { x: { type: 'function', returnsPromise: true, returnType: { type: 'new' } } },
+  { db: types.Database }
+];
+
+const st = new SymbolTable(scope[1], empty_types);
+const spy = sinon.spy(st);
+const writer = new AsyncWriter(scope[1], empty_types, st);
 const input = [
   // 'db.coll.insertOne()',
   // 'y = db',
+  'var x = db;'
   // 'y.coll.insertOne()',
   // '() => {return db; 1}',
   // 'f = () => {return db; 1}',
   // 'function x() {return db;}',
   // 'x().coll.insertOne()',
   // 'f().coll.insertOne()',
-  'db.coll[x()]'
+  // 'db.coll[x()]',
+  // 'x(x(1))'
 ];
 
 input.forEach((i) => console.log(`"${i}" ==> "${writer.compile(i)}"`));
-// writer.symbols.print();
+writer.symbols.print();
 
+// console.log(spy.add.getCalls());
