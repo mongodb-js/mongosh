be564b8b5a48b27fe2416287c769d059461fc91d
mcasimir <maurizio.cas@gmail.com>
2020-03-23 14:13:06 +0100

    docs(mapper): change comment

--- packages/mapper/src/mapper.ts
+++ packages/mapper/src/mapper.ts
@@ -964,8 +964,7 @@ export default class Mapper {
   }
 
   /**
-   * Returns an array that holds a list of documents that identify
-   * key patters for the indexes defined on the collection.
+   * Returns an array of key patters for the indexes defined on the collection.
    *
    * @param {Collection} collection
    *
