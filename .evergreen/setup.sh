f6f2e41c4d178dbc32d30fec7553721d3f561a24
Durran Jordan <durran@gmail.com>
2020-03-27 19:59:53 +0100

    Echo out NVM setup

--- .evergreen/setup.sh
+++ .evergreen/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export NVM_DIR="$HOME/.nvm"
+echo "Setting NVM environment home: $NVM_DIR"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
