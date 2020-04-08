f6690a7b3d34f8ec08d4a465275deeea8f3818fa
Durran Jordan <durran@gmail.com>
2020-04-08 13:44:20 +0200

    Adding in windows and posix zip functions

--- packages/build/src/zip.spec.ts
+++ packages/build/src/zip.spec.ts
@@ -1,24 +1,101 @@
+import path from 'path';
+import os from 'os';
+import fs from 'fs';
 import { expect } from 'chai';
 import Platform from './platform';
-import zip, { zipPath } from './zip';
+import zip, {
+  zipPath,
+  zipPosix,
+  zipWindows
+} from './zip';
 
 describe('zip module', () => {
   describe('.zipPath', () => {
     context('when the platform is windows', () => {
-      it('returns the windows zip', () => {
-        expect(zipPath('', Platform.Windows, '1.0.0')).to.equal('mongosh-1.0.0-win32.zip');
+      it('returns the windows zip name', () => {
+        expect(zipPath('', Platform.Windows, '1.0.0')).
+          to.equal('mongosh-1.0.0-win32.zip');
       });
     });
 
     context('when the platform is macos', () => {
-      it('returns the tarball', () => {
-        expect(zipPath('', Platform.MacOs, '1.0.0')).to.equal('mongosh-1.0.0-darwin.tgz');
+      it('returns the tarball name', () => {
+        expect(zipPath('', Platform.MacOs, '1.0.0')).
+          to.equal('mongosh-1.0.0-darwin.tgz');
       });
     });
 
     context('when the platform is linux', () => {
-      it('returns the tarball', () => {
-        expect(zipPath('', Platform.Linux, '1.0.0')).to.equal('mongosh-1.0.0-linux.tgz');
+      it('returns the tarball name', () => {
+        expect(zipPath('', Platform.Linux, '1.0.0')).
+          to.equal('mongosh-1.0.0-linux.tgz');
+      });
+    });
+  });
+
+  describe('.zipPosix', () => {
+    const version = '1.0.0';
+    const expectedZip = zipPath(__dirname, Platform.Linux, version);
+    const inputFile = path.join(__dirname, '..', 'examples', 'input.js');
+
+    before(() => {
+      return zipPosix(inputFile, __dirname, expectedZip);
+    });
+
+    after((done) => {
+      fs.unlink(expectedZip, done);
+    });
+
+    it('builds the executable', (done) => {
+      fs.stat(expectedZip, (error, stats) => {
+        expect(error).to.equal(null);
+        expect(stats.size).to.be.above(0);
+        done();
+      });
+    });
+  });
+
+  describe('.zipWindows', () => {
+    const version = '1.0.0';
+    const expectedZip = zipPath(__dirname, Platform.Windows, version);
+    const inputFile = path.join(__dirname, '..', 'examples', 'input.js');
+
+    before(() => {
+      return zipWindows(inputFile, expectedZip);
+    });
+
+    after((done) => {
+      fs.unlink(expectedZip, done);
+    });
+
+    it('builds the executable', (done) => {
+      fs.stat(expectedZip, (error, stats) => {
+        expect(error).to.equal(null);
+        expect(stats.size).to.be.above(0);
+        done();
+      });
+    });
+  });
+
+  describe('.zip', () => {
+    const platform = os.platform();
+    const version = '1.0.0';
+    const expectedZip = zipPath(__dirname, platform, version);
+    const inputFile = path.join(__dirname, '..', 'examples', 'input.js');
+
+    before(() => {
+      return zip(inputFile, __dirname, platform, version);
+    });
+
+    after((done) => {
+      fs.unlink(expectedZip, done);
+    });
+
+    it('builds the executable', (done) => {
+      fs.stat(expectedZip, (error, stats) => {
+        expect(error).to.equal(null);
+        expect(stats.size).to.be.above(0);
+        done();
       });
     });
   });
--- packages/build/src/zip.ts
+++ packages/build/src/zip.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import tar from 'tar';
+import AdmZip from 'adm-zip';
 import Platform from './platform';
 
 /**
@@ -17,6 +18,30 @@ const zipPath = (outputDir: string, platform: string, version: string): string =
 };
 
 /**
+ * Create a zip archive for windows.
+ *
+ * @param {string} input - The file to zip.
+ * @param {string} outputDir - The output directory.
+ * @param {string} filename - the zip filename.
+ */
+const zipPosix = async(input: string, outputDir: string, filename: string) => {
+  const options = { gzip: true, file: filename, cwd: outputDir };
+  await tar.c(options, [ input ]);
+};
+
+/**
+ * Create a zip archive for windows.
+ *
+ * @param {string} input - The file to zip.
+ * @param {string} filename - the zip filename.
+ */
+const zipWindows = async(input: string, filename: string) => {
+  const admZip = new AdmZip();
+  admZip.addLocalFile(input)
+  await admZip.writeZip(filename);
+};
+
+/**
  * Create a gzipped tarball or zip for the provided options.
  *
  * @param {string} input - The file location to zip.
@@ -27,15 +52,12 @@ const zipPath = (outputDir: string, platform: string, version: string): string =
 const zip = async(input: string, outputDir: string, platform: string, version: string) => {
   const filename = zipPath(outputDir, platform, version);
   console.log('mongosh: zipping:', filename);
-  await tar.c(
-    {
-      gzip: true,
-      file: filename,
-      cwd: outputDir
-    },
-    [input]
-  );
+  if (platform === Platform.Windows) {
+    zipWindows(input, filename);
+  } else {
+    zipPosix(input, outputDir, filename);
+  }
 };
 
 export default zip;
-export { zipPath };
+export { zipPath, zipPosix, zipWindows };
