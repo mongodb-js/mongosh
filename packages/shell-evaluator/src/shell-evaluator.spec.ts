b080448cd833458565cfdcf76cff6742c8913d6a
mcasimir <maurizio.cas@gmail.com>
2020-04-15 15:51:59 +0200

    test(shell-evaluator): readded commented tests

--- packages/shell-evaluator/src/shell-evaluator.spec.ts
+++ packages/shell-evaluator/src/shell-evaluator.spec.ts
@@ -1,28 +1,48 @@
-import { expect } from 'chai';
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+const { expect } = chai;
 
-describe('ShellEvaluator placeholder', () => {
-  // TODO: move to ShellApi tests
-  // describe('setCtx', () => {
-  //   let ctx;
-  //   beforeEach(() => {
-  //     ctx = {};
-  //     mapper.setCtx(ctx);
-  //   });
-  //
-  //   it('sets shell api globals', () => {
-  //     expect(ctx).to.include.all.keys('it', 'help', 'show', 'use');
-  //   });
-  //
-  //   it('sets db', () => {
-  //     expect(ctx.db).to.be.instanceOf(Database);
-  //   });
-  //
-  //   it('sets the object as context for the mapper', () => {
-  //     expect((mapper as any).context).to.equal(ctx);
-  //   });
-  // });
+import ShellEvaluator from './shell-evaluator';
+import { EventEmitter } from 'events';
 
-  it('passes', () => {
-    expect(true);
+describe('ShellEvaluator', () => {
+  let shellEvaluator: ShellEvaluator;
+  let serviceProviderMock;
+  let containerMock;
+  let busMock;
+
+  beforeEach(() => {
+    serviceProviderMock = {} as any;
+    busMock = new EventEmitter();
+    containerMock = { toggleTelemetry: sinon.spy() };
+
+    shellEvaluator = new ShellEvaluator(
+      serviceProviderMock,
+      busMock,
+      containerMock
+    );
+  });
+
+  describe('setCtx', () => {
+    let ctx;
+
+    beforeEach(() => {
+      ctx = {};
+      shellEvaluator.setCtx(ctx);
+    });
+
+    it('sets shell api globals', () => {
+      expect(ctx).to.include.all.keys('it', 'help', 'show', 'use');
+    });
+
+    it('sets db', () => {
+      expect(ctx.db.constructor.name).to.equal('Database');
+    });
+
+    it('sets the object as context for the mapper', () => {
+      expect((shellEvaluator as any).mapper.context).to.equal(ctx);
+    });
   });
 });
