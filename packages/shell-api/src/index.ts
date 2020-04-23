488e7394e4c54c70b563cdc9e8eacf4c5b8c1b70
aherlihy <anna.herlihy@10gen.com>
2020-04-23 12:17:04 +0200

    Add BSON to evaluator

--- packages/shell-api/src/index.ts
+++ packages/shell-api/src/index.ts
@@ -1,5 +1,7 @@
 import * as signatures from './shell-api-signatures';
+import ShellBson from './shell-bson';
 export { signatures };
+export { ShellBson };
 
 export { Help } from './help';
 export { CursorIterationResult } from './cursor-iteration-result';
