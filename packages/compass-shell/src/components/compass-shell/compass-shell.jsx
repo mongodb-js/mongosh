54899980cf62fb92fb280bd5411820ef0cd28e04
Rhys Howell <rhys@rhysh@live.com>
2020-05-09 17:54:02 +0200

    Add resizing handle and toggle to shell ui

--- packages/compass-shell/src/components/compass-shell/compass-shell.jsx
+++ packages/compass-shell/src/components/compass-shell/compass-shell.jsx
@@ -4,11 +4,15 @@ import styles from './compass-shell.less';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Resizable } from 're-resizable';
+import IconButton from '@leafygreen-ui/icon-button';
+import Icon from '@leafygreen-ui/icon';
 
 import { Shell } from '@mongosh/browser-repl';
 
+import { ResizeHandle } from '../resize-handle';
+
 const resizeableDirections = {
-  top: true,
+  top: false, // This property is controlled in the component.
   right: false,
   bottom: false,
   left: false,
@@ -17,6 +21,9 @@ const resizeableDirections = {
   bottomLeft: false,
   topLeft: false
 };
+
+const defaultShellHeightClosed = 24;
+const defaultShellHeightOpened = 240;
 
 export class CompassShell extends Component {
   static propTypes = {
@@ -27,18 +34,21 @@ export class CompassShell extends Component {
   static defaultProps = {
     runtime: null
   };
-
   constructor(props) {
     super(props);
 
     this.state = {
-      initialHistory: this.props.historyStorage ? null : []
+      initialHistory: this.props.historyStorage ? null : [],
+      isExpanded: false
     };
   }
 
   componentDidMount() {
     this.loadHistory();
   }
+
+  resizableRef = null;
+  lastOpenHeight = defaultShellHeightOpened;
 
   saveHistory = async(history) => {
     if (!this.props.historyStorage) {
@@ -72,32 +82,87 @@ export class CompassShell extends Component {
     }
   }
 
+  shellToggleClicked = () => {
+    if (this.state.isExpanded) {
+      this.lastOpenHeight = this.resizableRef.sizeStyle.height;
+
+      this.resizableRef.updateSize({
+        width: '100%',
+        height: defaultShellHeightClosed
+      });
+    } else {
+      this.resizableRef.updateSize({
+        width: '100%',
+        height: this.lastOpenHeight
+      });
+    }
+
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
+
   /**
    * Render CompassShell component.
    *
    * @returns {React.Component} The rendered component.
    */
   render() {
+    const {
+      isExpanded
+    } = this.state;
+
     if (!this.props.runtime || !this.state.initialHistory) {
       return (<div />);
     }
 
     return (
       <Resizable
-        className={classnames(styles['compass-shell'])}
+        className={styles['compass-shell']}
         defaultSize={{
           width: '100%',
-          height: 240
+          height: defaultShellHeightClosed
         }}
         id="content"
-        minHeight={30}
-        enable={resizeableDirections}
+        minHeight={defaultShellHeightClosed}
+        enable={{
+          ...resizeableDirections,
+          top: isExpanded
+        }}
+        ref={c => { this.resizableRef = c; }}
+        handleComponent={{
+          top: <ResizeHandle />,
+        }}
       >
-        <Shell
-          runtime={this.props.runtime}
-          initialHistory={this.state.initialHistory}
-          onHistoryChanged={this.saveHistory}
-        />
+        <div className={styles['compass-shell-header']}>
+          <button
+            className={styles['compass-shell-toggle']}
+            onClick={this.shellToggleClicked}
+          >
+            &gt;_MongoSH v0.9 Beta
+          </button>
+          {isExpanded && (
+            <div>
+              <IconButton
+                className={styles['compass-shell-close-btn']}
+                variant="dark"
+                aria-label="Some Menu"
+                onClick={this.shellToggleClicked}
+              >
+                <Icon glyph="X" />
+              </IconButton>
+            </div>
+          )}
+        </div>
+        {isExpanded && (
+          <div className={classnames(styles['compass-shell-shell-container'])}>
+            <Shell
+              runtime={this.props.runtime}
+              initialHistory={this.state.initialHistory}
+              onHistoryChanged={this.saveHistory}
+            />
+          </div>
+        )}
       </Resizable>
     );
   }
