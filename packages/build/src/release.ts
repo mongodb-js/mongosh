2cf7ecc0404b6ffdd19cc49b534bbd3ef78a3417
Durran Jordan <durran@gmail.com>
2020-05-11 17:22:56 +0200

    Only do download config on MacOS

--- packages/build/src/release.ts
+++ packages/build/src/release.ts
@@ -2,6 +2,7 @@ import os from 'os';
 import Config from './config';
 import compileExec from './compile-exec';
 import createDownloadCenterConfig from './download-center';
+import Platform from './platform';
 import zip from './zip';
 
 /**
@@ -20,25 +21,27 @@ const release = async(config: Config) => {
   // 3. Zip the executable.
   await zip(config.input, config.outputDir, platform, config.version);
 
-  // 4. Create & sign the .deb
-  // 5. Create & sign the .rpm
-  // 6. Create & sign the .msi
+  // 4. Create & sign the .deb (only on linux)
+  // 5. Create & sign the .rpm (only on linux)
+  // 6. Create & sign the .msi (only on win)
   // 
   // If this is a new release tag.
   //
-  // 1. Publish the .deb
-  // 2. Publish the .rpm
-  // 3. Create PR for Homebrew
+  // 1. Publish the .deb (only on linux)
+  // 2. Publish the .rpm (only on linux)
+  // 3. Create PR for Homebrew (only on macos)
 
   // 4. Upload artifacts to S3 for Evergreen and downloads. (Handled in .evergreen.yml)
 
-  // 5. Create download center config.
-  await createDownloadCenterConfig(config.version, config.outputDir);
+  // 5. Create download center config. (only on macos)
+  if (platform === Platform.MacOs) {
+    await createDownloadCenterConfig(config.version, config.outputDir);
+  }
 
   // 6. Upload download center config. (Handled in .evergreen.yml);
 
   // 7. Create Github release.
-  // 8. Publish to NPM.
+  // 8. Publish to NPM. (only on macos)
 }; 
 
 export default release;
