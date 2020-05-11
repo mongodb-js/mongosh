1e64124879eb9ce0ea75067b415a3122073b94ae
Durran Jordan <durran@gmail.com>
2020-05-11 15:30:59 +0200

    Automatically upload to download center

--- packages/build/src/download-center.spec.ts
+++ packages/build/src/download-center.spec.ts
@@ -1,12 +1,20 @@
+import path from 'path';
+import { promises as fs } from 'fs';
 import { expect } from 'chai';
-import { createDownloadCenterConfig } from './download-center';
+import createDownloadCenterConfig from './download-center';
 
 describe('download center module', () => {
   describe('.createDownloadCenterConfig', () => {
     let config;
+    const expectedExecutable = path.join(__dirname, 'mongosh.json');
 
-    before(() => {
-      config = createDownloadCenterConfig('1.2.2');
+    before(async() => {
+      await createDownloadCenterConfig('1.2.2', __dirname);
+      config = await fs.readFile(expectedExecutable, { encoding: 'UTF8' });
+    });
+
+    after(() => {
+      return fs.unlink(expectedExecutable);
     });
 
     it('returns the string with the macos version injected', () => {
