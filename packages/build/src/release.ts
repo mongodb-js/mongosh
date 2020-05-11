f3e8f7918bfc48706e127f27c108c1cbc4f7223e
Durran Jordan <durran@gmail.com>
2020-05-11 15:42:06 +0200

    Add download center config creation to release.ts

--- packages/build/src/release.ts
+++ packages/build/src/release.ts
@@ -1,6 +1,7 @@
 import os from 'os';
 import Config from './config';
 import compileExec from './compile-exec';
+import createDownloadCenterConfig from './download-center';
 import zip from './zip';
 
 /**
@@ -13,9 +14,12 @@ const release = async(config: Config) => {
 
   // 1. Build the executable.
   await compileExec(config.input, config.outputDir, platform);
+
   // 2. Sign the executable for each OS.
+
   // 3. Zip the executable.
   await zip(config.input, config.outputDir, platform, config.version);
+
   // 4. Create & sign the .deb
   // 5. Create & sign the .rpm
   // 6. Create & sign the .msi
@@ -25,9 +29,12 @@ const release = async(config: Config) => {
   // 1. Publish the .deb
   // 2. Publish the .rpm
   // 3. Publish to Homebrew
-  // 4. Upload to download center.
-  // 5. Change download center config.
-  // 6. Upload download center config.
+  // 4. Upload artifacts to download center. (Handled in .evergreen.yml)
+
+  // 5. Create download center config.
+  createDownloadCenterConfig(config.version, config.outputDir);
+
+  // 6. Upload download center config. (Handled in .evergreen.yml);
   // 7. Create Github release.
   // 8. Publish to NPM.
 }; 
