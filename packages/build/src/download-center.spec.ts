4e2f7befd7fcc877192731be8d40940cc5dfe543
Durran Jordan <durran@gmail.com>
2020-05-11 15:07:07 +0200

    Generate download center template

---
+++ packages/build/src/download-center.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { createDownloadCenterConfig } from './download-center';
+
+describe('download center module', () => {
+  describe('.createDownloadCenterConfig', () => {
+    let config;
+
+    before(() => {
+      config = createDownloadCenterConfig('1.2.2');
+    });
+
+    it('returns the string with the macos version injected', () => {
+      expect(config).to.include('mongosh-1.2.2-darwin.tgz');
+    });
+
+    it('returns the string with the linux version injected', () => {
+      expect(config).to.include('mongosh-1.2.2-linux.tgz');
+    });
+
+    it('returns the string with the win version injected', () => {
+      expect(config).to.include('mongosh-1.2.2-win.zip');
+    });
+  });
+});
