2277bd0f9cdc0d0c7fee50519bb8ecf39d36264e
Rhys Howell <rhys@rhysh@live.com>
2020-05-12 17:58:46 +0200

    Add tests for new components

--- packages/compass-shell/src/components/shell-header/shell-header.spec.js
+++ packages/compass-shell/src/components/shell-header/shell-header.spec.js
@@ -1,34 +1,52 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import IconButton from '@leafygreen-ui/icon-button';
 
 import ShellHeader from './shell-header';
+import styles from './shell-header.less';
 
 describe('ShellHeader', () => {
   context('when isExpanded prop is true', () => {
-    it('renders an X button to close', () => {
-      const wrapper = mount(<ShellHeader isExpanded />);
+    it('renders a close button', () => {
+      const wrapper = mount(<ShellHeader
+        isExpanded
+        onShellToggleClicked={() => {}}
+      />);
 
-      expect(wrapper.find(IconButton).exists()).toBe(true);
-      expect(wrapper.find('.compass-shell-header-close-btn').exists()).toBe(true);
+      expect(wrapper.find(IconButton).exists()).to.equal(true);
+    });
+
+    it('renders an actions area', () => {
+      const wrapper = mount(<ShellHeader
+        isExpanded
+        onShellToggleClicked={() => {}}
+      />);
+
+      expect(wrapper.find(`.${styles['compass-shell-header-right-actions']}`)).to.be.present();
     });
   });
 
   context('when isExpanded prop is false', () => {
     it('does not render an X button', () => {
-      const wrapper = mount(<ShellHeader isExpanded={false} />);
+      const wrapper = shallow(<ShellHeader
+        isExpanded={false}
+        onShellToggleClicked={() => {}}
+      />);
 
-      expect(wrapper.find(IconButton).exists()).toBe(false);
-      expect(wrapper.find('.compass-shell-header-close-btn').exists()).toBe(false);
+      expect(wrapper.find(IconButton).exists()).to.equal(false);
+      expect(wrapper.find(`.${styles['compass-shell-header-close-btn']}`).exists()).to.equal(false);
     });
   });
 
   context('when rendered', () => {
     it('has a button to toggle the container', async() => {
-      const wrapper = mount(<div className="some-class" />);
+      const wrapper = shallow(<ShellHeader
+        isExpanded={false}
+        onShellToggleClicked={() => {}}
+      />);
 
-      expect(wrapper.find('button').exists()).toBe(true);
-      expect(wrapper.find('.compass-shell-header-toggle').exists()).toBe(true);
+      expect(wrapper.find('button').exists()).to.equal(true);
+      expect(wrapper.find(`.${styles['compass-shell-header-toggle']}`)).to.be.present();
     });
   });
 });
