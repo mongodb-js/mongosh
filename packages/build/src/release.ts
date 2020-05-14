f501e47fc1dc4f4a490b4d6e3622402521823ed8
Durran Jordan <durran@gmail.com>
2020-05-14 15:01:08 +0200

    Fix missing import

--- packages/build/src/release.ts
+++ packages/build/src/release.ts
@@ -2,6 +2,7 @@ import os from 'os';
 import Config from './config';
 import compileExec from './compile-exec';
 import uploadArtifactToEvergreen from './evergreen';
+import uploadArtifactToDownloads from './upload-artifact';
 import uploadDownloadCenterConfig from './download-center';
 import Platform from './platform';
 import zip from './zip';
@@ -15,25 +16,25 @@ import S3 from 'aws-sdk/clients/s3';
 const release = async(config: Config) => {
   const platform = os.platform();
 
-  // 1. Build the executable.
+  // - Build the executable.
   await compileExec(config.input, config.outputDir, platform);
 
-  // 2. Sign the executable for each OS.
+  // - Sign the executable for each OS.
 
-  // 3. Zip the executable.
+  // - Zip the executable.
   const artifact = await zip(config.input, config.outputDir, platform, config.version);
 
-  // 4. Create & sign the .deb (only on linux)
-  // 5. Create & sign the .rpm (only on linux)
-  // 6. Create & sign the .msi (only on win)
-  // 
-  // If this is a new release tag.
+  // - Create & sign the .deb (only on linux)
+  // - Create & sign the .rpm (only on linux)
+  // - Create & sign the .msi (only on win)
+  // -
+  // - this is a new release tag.
   //
-  // 1. Publish the .deb (only on linux)
-  // 2. Publish the .rpm (only on linux)
-  // 3. Create PR for Homebrew (only on macos)
+  // - Publish the .deb (only on linux)
+  // - Publish the .rpm (only on linux)
+  // - Create PR for Homebrew (only on macos)
 
-  // 4. Upload artifacts to S3 for Evergreen and downloads.
+  // - Upload artifacts to S3 for Evergreen and downloads.
   await uploadArtifactToEvergreen(
     artifact,
     config.evgAwsKey,
@@ -49,10 +50,12 @@ const release = async(config: Config) => {
     config.revision
   );
 
-  // 5. Create Github release.
+  // - Create Github release.
 
-  // 6. Create download center config and upload. (only on macos)
-  // 7. Publish to NPM. (only on macos)
+  // - Create download center config and upload.
+  // - Publish to NPM.
+  //
+  // These only need to happen once so we only run them on MacOS.
   if (platform === Platform.MacOs) {
     await uploadDownloadCenterConfig(
       config.version,
