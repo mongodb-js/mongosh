76071e2832acb5cf84ca4d38312d6d3a6f56671a
Durran Jordan <durran@gmail.com>
2020-05-17 15:03:38 +0200

    Add Github latest release checking

--- packages/build/src/release.ts
+++ packages/build/src/release.ts
@@ -1,7 +1,9 @@
 import os from 'os';
+import { Octokit } from '@octokit/rest';
 import Config from './config';
 import compileExec from './compile-exec';
 import uploadArtifactToEvergreen from './evergreen';
+import createRelease, { isReleasable, uploadAsset } from './github';
 import uploadArtifactToDownloads from './upload-artifact';
 import uploadDownloadCenterConfig from './download-center';
 import Platform from './platform';
@@ -17,10 +19,16 @@ const release = async(config: Config) => {
   const platform = os.platform();
   console.log('mongosh: releasing with config:', config);
 
+  const octokit = new Octokit({
+    auth: config.githubToken,
+    userAgent: `mongosh ${config.version}`
+  });
+
   // - Build the executable.
   await compileExec(config.input, config.outputDir, platform);
 
-  // - Sign the executable for each OS.
+  // - Sign the executable for each OS. Sign executable or zip?
+  // - Notarise the zip (only on macos)
 
   // - Zip the executable.
   const artifact = await zip(config.input, config.outputDir, platform, config.version);
@@ -28,14 +36,8 @@ const release = async(config: Config) => {
   // - Create & sign the .deb (only on linux)
   // - Create & sign the .rpm (only on linux)
   // - Create & sign the .msi (only on win)
-  // -
-  // - this is a new release tag.
-  //
-  // - Publish the .deb (only on linux)
-  // - Publish the .rpm (only on linux)
-  // - Create PR for Homebrew (only on macos)
 
-  // - Upload artifacts to S3 for Evergreen and downloads.
+  // - Upload artifacts to S3 for Evergreen.
   await uploadArtifactToEvergreen(
     artifact,
     config.evgAwsKey,
@@ -43,26 +45,36 @@ const release = async(config: Config) => {
     config.project,
     config.revision
   );
-  await uploadArtifactToDownloads(
-    artifact,
-    config.downloadsAwsKey,
-    config.downloadsAwsSecret,
-    config.project,
-    config.revision
-  );
 
-  // - Create Github release.
+  if (await isReleasable(config.version, octokit)) {
+    const releaseId = await createRelease(config.version, octokit);
+    await uploadAsset(artifact, octokit);
 
-  // - Create download center config and upload.
-  // - Publish to NPM.
-  //
-  // These only need to happen once so we only run them on MacOS.
-  if (platform === Platform.MacOs) {
-    await uploadDownloadCenterConfig(
-      config.version,
+    // - Publish the .deb (only on linux)
+    // - Publish the .rpm (only on linux)
+    // - Create PR for Homebrew (only on macos)
+
+    // - Upload the artifact to downloads.10gen.com
+    await uploadArtifactToDownloads(
+      artifact,
       config.downloadCenterAwsKey,
-      config.downloadCenterAwsSecret
+      config.downloadCenterAwsSecret,
+      config.project,
+      config.revision
     );
+
+    // - Create download center config and upload.
+    // - Publish to NPM.
+    //
+    // These only need to happen once so we only run them on MacOS.
+    if (platform === Platform.MacOs) {
+      await uploadDownloadCenterConfig(
+        config.version,
+        config.downloadCenterAwsKey,
+        config.downloadCenterAwsSecret
+      );
+      // npm publish.
+    }
   }
 }; 
 
