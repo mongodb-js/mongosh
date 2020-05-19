2bac8fdc13a8c3db5d985911e628b223e3989553
Durran Jordan <durran@gmail.com>
2020-05-19 12:36:15 +0200

    Zip on OSX for notarization

--- packages/build/src/release.ts
+++ packages/build/src/release.ts
@@ -17,7 +17,6 @@ import S3 from 'aws-sdk/clients/s3';
  */
 const release = async(config: Config) => {
   const platform = os.platform();
-  // console.log('mongosh: releasing with config:', config);
 
   const octokit = new Octokit({
     auth: config.githubToken,
