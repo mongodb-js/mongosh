c8d6a89862fa379fc09661f7ac1d58c8becaabf1
Durran Jordan <durran@gmail.com>
2020-05-25 14:07:31 +0200

    Dont install Node with msi

--- .evergreen/Bootstrap.ps1
+++ .evergreen/Bootstrap.ps1
@@ -1,18 +1,24 @@
 $version = "12.4.0"
-$url = "https://nodejs.org/download/release/v$version/node-v$version-x86.msi"
+$url = "https://nodejs.org/download/release/v$version/node-v$version-win-x64.zip"
 $filename = "node.msi"
-$node_msi = "$PSScriptRoot\$filename"
+$node_zip = "$PSScriptRoot\temp\$filename"
 
+New-Item -Path .\temp -Item-Type directory
 Write-Host "[NODE] downloading nodejs install"
 Write-Host "url : $url"
 $start_time = Get-Date
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $node_msi)
+$wc.DownloadFile($url, $node_zip)
 Write-Output "$filename downloaded"
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-Start-Process msiexec.exe -Wait -ArgumentList "/i $node_msi /quiet"
-Write-Output "$node_msi installed."
+Expand-Archive $node_zip -DestinationPath .\temp
+Get-ChildItem -Path .\temp
+Set-Location -Path .\temp
+
+.\node.exe .\node_modules\npm2\bin\npm-cli.js i -g npm@latest
+
+Set-Location -Path $PSScriptRoot
 
 npm i -g npm@latest
 npm run bootstrap
