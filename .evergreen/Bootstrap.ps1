fed20295ca9128f2431da622dee9a1f35da3a38f
Durran Jordan <durran@gmail.com>
2020-05-25 14:46:33 +0200

    Attempt to install npm globally

--- .evergreen/Bootstrap.ps1
+++ .evergreen/Bootstrap.ps1
@@ -1,9 +1,10 @@
 $version = "12.4.0"
 $url = "https://nodejs.org/download/release/v$version/node-v$version-win-x64.zip"
-$filename = "node.msi"
-$node_zip = "$PSScriptRoot\temp\$filename"
+$filename = "node.zip"
+$node_zip = "$PSScriptRoot\$filename"
+$node_exe = "$PSScriptRoot\node-v$version-win-x64\node.exe"
+$npm = "$PSScriptRoot\node-v$version-win-x64\node_modules\npm2\bin\npm-cli.js"
 
-New-Item -Path .\temp -ItemType directory
 Write-Host "[NODE] downloading nodejs install"
 Write-Host "url : $url"
 $start_time = Get-Date
@@ -12,12 +13,10 @@ $wc.DownloadFile($url, $node_zip)
 Write-Output "$filename downloaded"
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-Expand-Archive $node_zip -DestinationPath .\temp
-Get-ChildItem -Path .\temp
-Set-Location -Path .\temp
+Expand-Archive $node_zip -DestinationPath $PSScriptRoot
+Get-ChildItem -Path $PSScriptRoot
 
-.\node.exe .\node_modules\npm2\bin\npm-cli.js i -g npm@latest
+$node_exe $npm i -g npm@latest
 
-Set-Location -Path $PSScriptRoot
+Set-Location -Path $PSScriptRoot\..\
 
-npm run bootstrap
