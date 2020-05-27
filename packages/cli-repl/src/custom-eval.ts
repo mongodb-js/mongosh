85e1421a13011f86453dc8afc04f227e240a4a9c
Irina Shestak <shestak.irina@gmail.com>
2020-05-27 14:20:20 +0200

    use fastq for queueing, mv recoverable error outside of the queue

--- packages/cli-repl/src/custom-eval.ts
+++ packages/cli-repl/src/custom-eval.ts
@@ -1,35 +1,30 @@
 import isRecoverableError from 'is-recoverable-error';
 import { Recoverable } from 'repl';
+import queue from 'fastq';
 import util from 'util';
-import PQueue from 'p-queue';
 
 export function createCustomEval(replEval: Function, shellEvaluatorCustomEval: Function): Function {
-  const queue = new PQueue({ concurrency: 1 });
+  const q = queue(worker, 1);
 
   const originalEval = util.promisify(replEval);
 
   const customEval = async function(input, context, filename, callback): Promise<any> {
-    async function evalTask(): Promise<void> {
-      try {
-        const result = await shellEvaluatorCustomEval(
-          originalEval,
-          input,
-          context,
-          filename
-        );
-
-        return callback(null, result);
-      } catch (err) {
-        if (isRecoverableError(input)) {
-          return callback(new Recoverable(err));
-        }
-
-        return callback(err);
-      }
+    if (isRecoverableError(input)) {
+      return callback(new Recoverable(new SyntaxError()));
     }
-
-    await queue.add(evalTask);
+    q.push({ input, context, filename }, callback);
   };
+
+  async function worker(opts, cb) {
+    const { input, context, filename } = opts;
+    let result;
+    try {
+      result = await shellEvaluatorCustomEval(originalEval, input, context, filename);
+    } catch (err) {
+      result = err;
+    }
+    return cb(null, result);
+  }
 
   return customEval;
 }
