8fd5902bb98cbafa086303b310ecb03d09403575
Liudmila Kornilova <kornilova203@gmail.com>
2020-05-29 19:36:32 +0300

    [java-shell] add test method for each test case so it's easier to run specific test

--- packages/java-shell/src/test/kotlin/com/mongodb/mongosh/LiteralsTest.kt
+++ packages/java-shell/src/test/kotlin/com/mongodb/mongosh/LiteralsTest.kt
@@ -1,26 +1,51 @@
 package com.mongodb.mongosh
 
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
-import java.io.File
 
-@RunWith(Parameterized::class)
-class LiteralsTest(private val testName: String) : ShellTestCase() {
-    companion object {
-        private const val TEST_DATA_PATH = "src/test/resources/literal"
+class LiteralsTest : ShellTestCase() {
 
-        @JvmStatic
-        @Parameterized.Parameters(name = "{0}")
-        fun fileNames(): Collection<*> {
-            return getTestNames(TEST_DATA_PATH).map { arrayOf(it) }
+    @Test fun testArray()         = test()
+    @Test fun testBinData()       = test()
+    @Test fun testBool()          = test()
+    @Test fun testBsonsize()      = test()
+    @Test fun testBSONSymbol()    = test()
+    @Test fun testCode()          = test()
+    @Test fun testDate()          = test()
+    @Test fun testDateNow()       = test()
+    @Test fun testDBRef()         = test()
+    @Test fun testDouble()        = test()
+    @Test fun testEmptyArray()    = test()
+    @Test fun testEmptyObject()   = test()
+    @Test fun testFloat()         = test()
+    @Test fun testHexData()       = test()
+    @Test fun testInt()           = test()
+    @Test fun testISODate()       = test()
+    @Test fun testLong()          = test()
+    @Test fun testMaxKey()        = test()
+    @Test fun testMD5()           = test()
+    @Test fun testMinKey()        = test()
+    @Test fun testNull()          = test()
+    @Test fun testNumberDecimal() = test()
+    @Test fun testNumberInt()     = test()
+    @Test fun testNumberLong()    = test()
+    @Test fun testObject()        = test()
+    @Test fun testObjectId()      = test()
+    @Test fun testString()        = test()
+    @Test fun testSymbol()        = test()
+    @Test fun testTimestamp()     = test()
+    @Test fun testUndefined()     = test()
+    @Test fun testUnknownSymbol() = test()
+    @Test fun testUUID()          = test()
+    
+    fun test() {
+        val name = (Throwable()).stackTrace[1].methodName.removePrefix("test")
+
+        withShell { shell ->
+            doTest(name, shell, TEST_DATA_PATH)
         }
     }
 
-    @Test
-    fun test() {
-        withShell { shell ->
-            doTest(testName, shell, TEST_DATA_PATH)
-        }
+    companion object {
+        private const val TEST_DATA_PATH = "src/test/resources/literal"
     }
 }
