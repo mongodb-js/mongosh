f7272d09b54a23a4915870bc9337896c1ed4733f
Irina Shestak <shestak.irina@gmail.com>
2020-06-02 17:07:08 +0200

    mv node --version to top level script

--- scripts/compile-exec.js
+++ scripts/compile-exec.js
@@ -1,9 +1,12 @@
 const os = require('os');
 const path = require('path');
 const { compileExec } = require('@mongosh/build');
+const { execSync } = require('child_process');
 const config = require(path.join(__dirname, '..', 'config', 'build.conf.js'));
 
 const run = async() => {
+  console.log(`node --version ${execSync('node --version')}`);
+
   await compileExec(
     config.input,
     config.execInput,
