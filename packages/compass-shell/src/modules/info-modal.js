1c0d4bee7dc53e4c2c03b1fbb78ba93240c634fa
Rhys Howell <rhys@rhysh@live.com>
2020-06-08 07:29:57 +0200

    Check package lock and clean up modal code

--- packages/compass-shell/src/modules/info-modal.js
+++ packages/compass-shell/src/modules/info-modal.js
@@ -1,19 +1,16 @@
 export const SET_SHOW_INFO_MODAL = 'SHELL/INFO_MODAL/SET_SHOW_INFO_MODAL';
 
-/**
- * The initial state.
- */
 export const INITIAL_STATE = {
   isInfoModalVisible: false
 };
 
 /**
- * Reducer function for handling data service connected actions.
+ * Reducer function for handling actions with the info modal.
  *
- * @param {Object} state - The data service state.
+ * @param {Object} state - The info modal state.
  * @param {Object} action - The action.
  *
- * @returns {String} The new state.
+ * @returns {Object} The new state.
  */
 export default function reducer(state = INITIAL_STATE, action) {
   if (action.type === SET_SHOW_INFO_MODAL) {
