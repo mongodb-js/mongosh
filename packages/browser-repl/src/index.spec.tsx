f5dbbafd1ea5f5dbe38de3a31e51580e06868d58
Rhys Howell <rhys@rhysh@live.com>
2020-06-19 15:27:58 +0200

    Add general test for browser-repl

---
+++ packages/browser-repl/src/index.spec.tsx
@@ -0,0 +1,23 @@
+// import sinon from 'sinon';
+import React from 'react';
+import { expect } from '../testing/chai';
+import { shallow } from '../testing/enzyme';
+
+import { Shell } from './components/shell';
+import { Shell as BrowserRepl } from './index';
+
+describe('BrowserRepl', () => {
+  context('when it is required', () => {
+    it('renders', () => {
+      const fakeRuntime: any = {};
+      const wrapper = shallow(
+        <div>
+          <BrowserRepl runtime={fakeRuntime} />
+        </div>
+      );
+      console.log('wrapper', wrapper);
+      expect(wrapper.find(Shell)).to.have.lengthOf(1);
+    });
+  });
+});
+
