da5504bffdd087545355650cde6d15fa27d38813
mcasimir <maurizio.cas@gmail.com>
2020-06-21 17:58:39 +0200

    run config upload on osx only

--- packages/build/src/release.ts
+++ packages/build/src/release.ts
@@ -7,6 +7,7 @@ import uploadArtifactToEvergreen from './evergreen';
 import { GithubRepo } from './github-repo';
 import uploadArtifactToDownloadCenter from './upload-artifact';
 import { zip, ZipFile } from './zip';
+import Platform from './platform';
 
 /**
  * Run the release process.
@@ -14,7 +15,10 @@ import { zip, ZipFile } from './zip';
  * @param {Config} config - the configuration, usually config/build.config.js.
  */
 export default async function release(config: Config): Promise<void> {
-  log('beginning release with config:', safeToLogConfig(config));
+  console.log(
+    'mongosh: beginning release with config:',
+    safeToLogConfig(config)
+  );
 
   const octokit = new Octokit({
     auth: config.githubToken
@@ -24,25 +28,21 @@ export default async function release(config: Config): Promise<void> {
 
   // Build the executable.
   const zipFile = await compileAndZipExecutable(config);
-  log('created zipfile:', zipFile);
+  console.log('mongosh: created zipfile:', zipFile);
 
   // Always release internally.
   await releaseIntenally(zipFile, config);
-  log('internal release completed.');
+  console.log('mongosh: internal release completed.');
 
   // Only release to public from master and when tagged with the right version.
   if (shouldDoPublicRelease(githubRepo, config)) {
-    log('start public release.');
+    console.log('mongosh: start public release.');
 
     await releaseToDownloadCenter(zipFile, config);
     await releaseToGithub(zipFile, githubRepo, config);
   }
 
-  log('finished release process.');
-}
-
-function log(...message): void {
-  console.info('mongosh:', ...message);
+  console.log('mongosh: finished release process.');
 }
 
 function safeToLogConfig(config: Config): any {
@@ -93,22 +93,21 @@ async function releaseIntenally(artifact: ZipFile, config: Config): Promise<void
 }
 
 async function shouldDoPublicRelease(githubRepo: GithubRepo, config: Config): Promise<boolean> {
-  if (config.branch === 'master' ) {
-    log('skip public release: is not master');
-
+  if (config.branch === 'master') {
+    console.log('mongosh: skip public release: is not master');
     return false;
   }
 
   const commitTag = await githubRepo.getTagByCommitSha(config.revision);
 
   if (!commitTag) {
-    log('skip public release: commit is not tagged');
-
+    console.log('mongosh: skip public release: commit is not tagged');
     return false;
   }
 
   if (semver.neq(commitTag.name, config.version)) {
-    log('skip public release: the commit tag', commitTag.name,
+    console.log(
+      'mongosh: skip public release: the commit tag', commitTag.name,
       'is different from the release version', config.version
     );
 
@@ -127,11 +126,13 @@ async function releaseToDownloadCenter(artifact: ZipFile, config: Config): Promi
     config.revision
   );
 
-  await uploadDownloadCenterConfig(
-    config.version,
-    config.downloadCenterAwsKey,
-    config.downloadCenterAwsSecret
-  );
+  await runOnlyOnOnePlatform('upload to download center', config, async() => {
+    await uploadDownloadCenterConfig(
+      config.version,
+      config.downloadCenterAwsKey,
+      config.downloadCenterAwsSecret
+    );
+  });
 }
 
 async function releaseToGithub(artifact: ZipFile, githubRepo: GithubRepo, config: Config): Promise<void> {
@@ -147,4 +148,26 @@ async function releaseToGithub(artifact: ZipFile, githubRepo: GithubRepo, config
 
 function jiraReleaseNotesLink(version: string): string {
   return `https://jira.mongodb.org/issues/?jql=project%20%3D%20MONGOSH%20AND%20fixVersion%20%3D%20${version}`;
+}
+
+function runOnlyOnOnePlatform(codeSectionLabel, config, fn: Function): any {
+  // Since evergreen runs everything with a matrix strategy we
+  // have to run the parts of the code that can't run more than once
+  // only on the mac os runner.
+
+  const platformForSingleRun = Platform.MacOs;
+
+  if (config.platform === platformForSingleRun) {
+    console.log(
+      'mongosh: running', codeSectionLabel,
+      'since platform ===', platformForSingleRun
+    );
+    return fn();
+  }
+
+  console.log(
+    'mongosh: skipping', codeSectionLabel,
+    'since platform !==', platformForSingleRun
+  );
+  return;
 }
