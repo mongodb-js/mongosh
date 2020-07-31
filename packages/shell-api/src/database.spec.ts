b2935ce9d6e95bbf02c40e83d2271db90d279bb3
aherlihy <anna.herlihy@10gen.com>
2020-07-31 15:47:17 +0200

    Add help

--- packages/shell-api/src/database.spec.ts
+++ packages/shell-api/src/database.spec.ts
@@ -151,6 +151,12 @@ describe('Database', () => {
       });
     });
 
+    describe('getMongo', () => {
+      it('returns the name of the DB', async() => {
+        expect(database.getMongo()).to.equal(mongo);
+      });
+    });
+
     describe('runCommand', () => {
       it('calls serviceProvider.runCommand on the database', async() => {
         await database.runCommand({ someCommand: 'someCollection' });
