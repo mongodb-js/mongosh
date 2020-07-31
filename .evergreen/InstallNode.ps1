6722037968667e5a2aa88c2013cb5c04d2b353b5
mcasimir <maurizio.cas@gmail.com>
2020-07-31 12:25:25 +0200

    Skip compile on bootstrap

--- .evergreen/InstallNode.ps1
+++ .evergreen/InstallNode.ps1
@@ -20,8 +20,8 @@ Remove-Item .\npm
 Remove-Item .\npm.cmd
 Remove-Item .\npx
 Remove-Item .\npx.cmd
-Move-Item .\node_modules\npm -Destination .\node_modules\npm2 
+Move-Item .\node_modules\npm -Destination .\node_modules\npm2
 .\node.exe .\node_modules\npm2\bin\npm-cli.js i -g npm@latest
 
 Set-Location -Path $PSScriptRoot\..\
-npm run bootstrap
+npm run bootstrap-ci
