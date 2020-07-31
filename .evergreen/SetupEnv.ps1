566869ef76cba866301cddeef29c6e978c99007c
mcasimir <maurizio.cas@gmail.com>
2020-07-31 23:40:02 +0200

    fix libexec\git-core path

--- .evergreen/SetupEnv.ps1
+++ .evergreen/SetupEnv.ps1
@@ -1,2 +1,2 @@
 $version = "12.4.0"
-$Env:PATH = "$PSScriptRoot\node-v$version-win-x64;C:\Program Files\Git\mingw32/libexec/git-core;$Env:PATH"
+$Env:PATH = "$PSScriptRoot\node-v$version-win-x64;C:\Program Files\Git\mingw32\libexec\git-core;$Env:PATH"
