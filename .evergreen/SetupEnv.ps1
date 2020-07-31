8ff05bf8c0233e63b1011c12640707ad341f1ae5
mcasimir <maurizio.cas@gmail.com>
2020-07-31 23:12:51 +0200

    fix node path

--- .evergreen/SetupEnv.ps1
+++ .evergreen/SetupEnv.ps1
@@ -1,4 +1,2 @@
 $version = "12.4.0"
-$node_path = "$PSScriptRoot\node-v$version-win-x64\node.exe"
-$npm_path = "$PSScriptRoot\node-v$version-win-x64\npm.cmd"
-$Env:PATH = "$node_path;$npm_path;$Env:PATH"
+$Env:PATH = "$PSScriptRoot\node-v$version-win-x64;$npm_path;$Env:PATH"
