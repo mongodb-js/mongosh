29c1a1c0918943644f4f78336c346ad831418f65
mcasimir <maurizio.cas@gmail.com>
2020-08-05 18:53:07 +0200

    add skipIfServerVersion to skip tests if <= 4.4

--- testing/integration-testing-hooks.ts
+++ testing/integration-testing-hooks.ts
@@ -1,8 +1,14 @@
 const mongodbRunnerBefore = require('mongodb-runner/mocha/before');
 const mongodbRunnerAfter = require('mongodb-runner/mocha/after');
+const semver = require('semver');
+const { MongoClient } = require('mongodb');
 
 export const LOCAL_INSTANCE_PORT = 27018;
 export const LOCAL_INSTANCE_HOST = 'localhost';
+
+const envConnectionString = process.env.MONGOSH_TEST_SERVER_URL;
+const localConnectionString = `mongodb://${LOCAL_INSTANCE_HOST}:${LOCAL_INSTANCE_PORT}`;
+const connectionString = envConnectionString || localConnectionString;
 
 /**
  * Starts a local server unless the `MONGOSH_TEST_SERVER_URL`
@@ -18,10 +24,6 @@ export const LOCAL_INSTANCE_HOST = 'localhost';
  * @returns {string} - uri that can be used to connect to the server.
  */
 export function startTestServer(): string {
-  const envConnectionString = process.env.MONGOSH_TEST_SERVER_URL;
-  const localConnectionString = `mongodb://${LOCAL_INSTANCE_HOST}:${LOCAL_INSTANCE_PORT}`;
-  const connectionString = envConnectionString || localConnectionString;
-
   if (!envConnectionString) {
     console.info(
       'MONGOSH_TEST_SERVER_URL not provided. A local server will be started on',
@@ -51,4 +53,42 @@ export function startTestServer(): string {
   }
 
   return connectionString;
+}
+
+let testServerVersion;
+
+async function getServerVersion(connectionString: string) {
+  let client;
+  try {
+    client = await MongoClient.connect(connectionString, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+
+    return (await client.db().admin().serverStatus()).version;
+  } finally {
+    if (client) {
+      client.close();
+    }
+  }
+}
+
+/**
+ * Skip tests in the suite if the test server version matches a specific semver
+ * condition.
+ *
+ * describe('...', () => {
+ *   ie. skipIfServerVersion('< 4.4')
+ * });
+ *
+ * @export
+ * @returns {string} - uri that can be used to connect to the server.
+ */
+export function skipIfServerVersion(semverCondition) {
+  before(async function() {
+    testServerVersion = testServerVersion || await getServerVersion(connectionString);
+    if (semver.satisfies(testServerVersion, semverCondition)) {
+      this.skip();
+    }
+  });
 }
