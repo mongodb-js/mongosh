26a9290bece4f88f2ae1c9926a9055517b51c3e5
Irina Shestak <shestak.irina@gmail.com>
2020-08-31 10:09:23 +0200

    add some quick tests for attached listeners in logger

--- packages/cli-repl/src/logger.spec.ts
+++ packages/cli-repl/src/logger.spec.ts
@@ -2,7 +2,6 @@
 import logger from './logger';
 import Nanobus from 'nanobus';
 import { expect } from 'chai';
-import sinon from 'ts-sinon';
 import tmp from 'tmp';
 
 describe('logger', () => {
@@ -11,27 +10,82 @@ describe('logger', () => {
   logger(bus, logDirectory.name);
 
   context('mongosh:connect', () => {
-    const connectEvent = {
-      is_atlas: false,
-      is_localhost: true,
-      server_version: '4.40',
-      is_enterprise: false,
-      is_data_lake: false,
-      is_genuine: true,
-      non_genuine_server_name: 'mongodb',
-      node_version: '12.4.0',
-      uri: 'localhost:27017'
-    };
     const listener = bus.listeners('mongosh:connect');
 
     it('mongosh:connect listener is not undefined', () => {
       expect(listener).to.not.be.undefined;
     });
+  });
 
-    it('mongosh:connect listener is called', () => {
-      const spy = sinon.spy(listener);
-      bus.emit('mongosh:connect', connectEvent);
-      expect(spy).to.have.been.called;
+  context('mongosh:new-user', () => {
+    const listener = bus.listeners('mongosh:new-user');
+
+    it('mongosh:new-user listener is not undefined', () => {
+      expect(listener).to.not.be.undefined;
+    });
+  });
+
+  context('mongosh:update-user', () => {
+    const listener = bus.listeners('mongosh:update-user');
+
+    it('mongosh:update-user listener is not undefined', () => {
+      expect(listener).to.not.be.undefined;
+    });
+  });
+
+  context('mongosh:error', () => {
+    const listener = bus.listeners('mongosh:error');
+
+    it('mongosh:error listener is not undefined', () => {
+      expect(listener).to.not.be.undefined;
+    });
+  });
+
+  context('mongosh:help', () => {
+    const listener = bus.listeners('mongosh:help');
+
+    it('mongosh:help listener is not undefined', () => {
+      expect(listener).to.not.be.undefined;
+    });
+  });
+
+  context('mongosh:rewritten-async-input', () => {
+    const listener = bus.listeners('mongosh:rewritten-async-input');
+
+    it('mongosh:rewritten-async-input listener is not undefined', () => {
+      expect(listener).to.not.be.undefined;
+    });
+  });
+
+  context('mongosh:use', () => {
+    const listener = bus.listeners('mongosh:use');
+
+    it('mongosh:use listener is not undefined', () => {
+      expect(listener).to.not.be.undefined;
+    });
+  });
+
+  context('mongosh:show', () => {
+    const listener = bus.listeners('mongosh:show');
+
+    it('mongosh:show listener is not undefined', () => {
+      expect(listener).to.not.be.undefined;
+    });
+  });
+
+  context('mongosh:setCtx', () => {
+    const listener = bus.listeners('mongosh:setCtx');
+
+    it('mongosh:setCtx listener is not undefined', () => {
+      expect(listener).to.not.be.undefined;
+    });
+  });
+
+  context('mongosh:api-call', () => {
+    const listener = bus.listeners('mongosh:apiEvent');
+
+    it('mongosh:api-call listener is not undefined', () => {
+      expect(listener).to.not.be.undefined;
     });
   });
 });
