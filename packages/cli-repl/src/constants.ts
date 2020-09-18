2633ecf2d7d32c16541b821e7d922044bc288603
Massimiliano Marcon <max.marcon@mongodb.com>
2020-09-18 14:37:48 +0200

    Help cleanup (#333)
    * Removed options we currently don't support
    
    * Removed options that don't exist in the driver
    
    - ipv6: the driver tries it automatically
    - tlsCRLFile: does not seem to be an option in the node driver
    
    * Reference to the PR

--- packages/cli-repl/src/constants.ts
+++ packages/cli-repl/src/constants.ts
@@ -10,23 +10,20 @@ export const MONGOSH_WIKI = `
 ${i18n.__('cli-repl.cli-repl.wiki.info')} ${clr(i18n.__('cli-repl.cli-repl.wiki.link'), 'bold')}
 `;
 
+// See this PR for the options that were removed:
+// https://github.com/mongodb-js/mongosh/pull/333
 export const USAGE = `
 
   ${clr(i18n.__('cli-repl.args.usage'), 'bold')}
 
   ${clr(i18n.__('cli-repl.args.options'), ['bold', 'yellow'])}
 
-    -h, --help                                 ${i18n.__('cli-repl.args.help')} 
-        --ipv6                                 ${i18n.__('cli-repl.args.ipv6')}
+    -h, --help                                 ${i18n.__('cli-repl.args.help')}
         --host [arg]                           ${i18n.__('cli-repl.args.host')}
         --port [arg]                           ${i18n.__('cli-repl.args.port')}
         --version                              ${i18n.__('cli-repl.args.version')}
-        --shell                                ${i18n.__('cli-repl.args.shell')}
         --nodb                                 ${i18n.__('cli-repl.args.nodb')}
-        --norc                                 ${i18n.__('cli-repl.args.norc')}
-        --eval [arg]                           ${i18n.__('cli-repl.args.eval')}
         --retryWrites                          ${i18n.__('cli-repl.args.retryWrites')}
-${''/* TODO:        --disableImplicitSessions              ${i18n.__('cli-repl.args.disableImplicitSessions')}*/}
 
   ${clr(i18n.__('cli-repl.args.authenticationOptions'), ['bold', 'yellow'])}
 
@@ -34,8 +31,6 @@ ${''/* TODO:        --disableImplicitSessions              ${i18n.__('cli-repl.a
     -p, --password [arg]                       ${i18n.__('cli-repl.args.password')}
         --authenticationDatabase [arg]         ${i18n.__('cli-repl.args.authenticationDatabase')}
         --authenticationMechanism [arg]        ${i18n.__('cli-repl.args.authenticationMechanism')}
-        --gssapiServiceName [arg] (=mongodb)   ${i18n.__('cli-repl.args.')}
-        --gssapiHostName [arg]                 ${i18n.__('cli-repl.args.retryWrites')}
 
   ${clr(i18n.__('cli-repl.args.tlsOptions'), ['bold', 'yellow'])}
 
@@ -43,19 +38,10 @@ ${''/* TODO:        --disableImplicitSessions              ${i18n.__('cli-repl.a
         --tlsCertificateKeyFile [arg]          ${i18n.__('cli-repl.args.tlsCertificateKeyFile')}
         --tlsCertificateKeyFilePassword [arg]  ${i18n.__('cli-repl.args.tlsCertificateKeyPassword')}
         --tlsCAFile [arg]                      ${i18n.__('cli-repl.args.tlsCAFile')}
-        --tlsCRLFile [arg]                     ${i18n.__('cli-repl.args.tlsCRFile')}
         --tlsAllowInvalidHostnames             ${i18n.__('cli-repl.args.tlsAllowInvaludHostnames')}
         --tlsAllowInvalidCertificates          ${i18n.__('cli-repl.args.tlsAllowInvalidCertificates')}
         --tlsCertificateSelector [arg]         ${i18n.__('cli-repl.args.tlsCertificateSelector')}
         --tlsDisabledProtocols [arg]           ${i18n.__('cli-repl.args.tlsDisabledProtocols')}
-
-  ${clr(i18n.__('cli-repl.args.fleAwsOptions'), ['bold', 'yellow'])}
-
-        --awsAccessKeyId [arg]                 ${i18n.__('cli-repl.args.awsAccessKeyId')}
-        --awsSecretAccessKey [arg]             ${i18n.__('cli-repl.args.awsSecretAccessKey')}
-        --awsSessionToken [arg]                ${i18n.__('cli-repl.args.awsSessionToken')}
-        --keyVaultNamespace [arg]              ${i18n.__('cli-repl.args.keyVaultNamespace')}
-        --kmsURL [arg]                         ${i18n.__('cli-repl.args.kmsURL')}
 
   ${clr(i18n.__('cli-repl.args.dbAddressOptions'), ['bold', 'yellow'])}
 
@@ -63,10 +49,6 @@ ${''/* TODO:        --disableImplicitSessions              ${i18n.__('cli-repl.a
         192.168.0.5/foo                        ${i18n.__('cli-repl.args.dbAddress/192/foo')}
         192.168.0.5:9999/foo                   ${i18n.__('cli-repl.args.dbAddress/192/host/foo')}
         mongodb://192.168.0.5:9999/foo         ${i18n.__('cli-repl.args.dbAddress/connectionURI')}
-
-  ${clr(i18n.__('cli-repl.args.fileNames'), ['bold', 'yellow'])}
-
-        ${i18n.__('cli-repl.args.filenameDescription')}
 
   ${clr(i18n.__('cli-repl.args.examples'), ['bold', 'yellow'])}
 
