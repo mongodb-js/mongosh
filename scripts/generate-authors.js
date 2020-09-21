e9dd57c451180ab5b03715d466d99aded6b6af10
Maurizio Casimirri <maurizio.cas@gmail.com>
2020-09-21 16:05:12 +0200

    chore: generate authors by first commit (#336)

--- scripts/generate-authors.js
+++ scripts/generate-authors.js
@@ -17,31 +17,20 @@ const packageRootPath = path.resolve(__dirname, '..');
 
 function getAuthorsGitLog(packagePath) {
   return execSync(
-    `git log --format='%aN <%aE>' --use-mailmap -- ${packagePath}`,
+    `git log --reverse --format='%aN <%aE>' --use-mailmap -- ${packagePath}`,
     { cwd: packageRootPath }
   ).toString().trim().split('\n');
 }
 
-function getAuthorsOrderedByCommitNumber(packagePath) {
-  const authorsMap = {};
+function getAuthorsOrderedByFirstCommit(packagePath) {
+  const alreadyAdded = new Set();
+  const authors = [];
 
   for (const authorName of getAuthorsGitLog(packagePath)) {
-    authorsMap[authorName] =  authorName in authorsMap ? authorsMap[authorName] + 1 : 1;
+    if (alreadyAdded.has(authorName)) { continue; }
+    alreadyAdded.add(authorName);
+    authors.push(authorName);
   }
-
-  const compareAuthors = ([name1, commitCount1], [name2, commitCount2]) => {
-    if (commitCount1 === commitCount2) {
-      return (name1 > name2) ? 1: -1;
-    }
-
-    return commitCount1 > commitCount2 ? -1 : 1;
-  };
-
-  const authors = Object.entries(authorsMap)
-    .sort(compareAuthors)
-    .map(([name]) => {
-      return name;
-    });
 
   return authors;
 }
@@ -60,11 +49,11 @@ const packages = getAllPackages();
 
 for (const { location } of packages) {
   const packagePath = path.relative(packageRootPath, location);
-  const authors = getAuthorsOrderedByCommitNumber(packagePath);
+  const authors = getAuthorsOrderedByFirstCommit(packagePath);
   fs.writeFileSync(path.resolve(packagePath, 'AUTHORS'), renderAuthorsFileContent(authors));
 }
 
 fs.writeFileSync(
   path.resolve(packageRootPath, 'AUTHORS'),
-  renderAuthorsFileContent(getAuthorsOrderedByCommitNumber('.'))
+  renderAuthorsFileContent(getAuthorsOrderedByFirstCommit('.'))
 );
