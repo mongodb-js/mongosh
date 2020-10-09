32d52ad06608af259d65b7630c2b46fe11b138d0
Liudmila Kornilova <kornilova203@gmail.com>
2020-10-09 12:57:44 +0800

    [java-shell] remove unused MongoShellDatabase class

--- packages/java-shell/src/main/kotlin/com/mongodb/mongosh/result/DatabaseResult.kt
+++
@@ -1,8 +0,0 @@
-package com.mongodb.mongosh.result
-
-import org.graalvm.polyglot.Value
-
-class MongoShellDatabase internal constructor(private val db: Value) {
-    fun _asPrintable(): String = db.getMember("_asPrintable").execute().asString()
-    fun _name(): String = _asPrintable()
-}
