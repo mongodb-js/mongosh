13d2f1023c17cb848991961eb859314aa882d568
Sergey Petushkov <petushkov.sergey@gmail.com>
2020-12-16 11:10:14 +0100

    refactor(compass-shell): Better serialization for the rpc, only {de}serilalize args when needed; Handle both message port and process when sending message

--- packages/compass-shell/src/modules/worker-runtime/rpc.js
+++ packages/compass-shell/src/modules/worker-runtime/rpc.js
@@ -1,16 +1,33 @@
 import { serialize, deserialize } from 'v8';
 import { expose, caller } from 'postmsg-rpc';
 
+function isRPCMessage(data) {
+  return data.sender && data.id && data.func;
+}
+
+function send(process, data) {
+  return process.send ? process.send(data) : process.postMessage(data);
+}
+
 function getRPCOptions(process) {
   return {
     addListener: process.on.bind(process),
     removeListener: process.off.bind(process),
-    postMessage: (data) => {
-      data.args = removeTrailingUndefined(data.args);
-      return process.send(serialize(data).toString('base64'));
+    postMessage(data) {
+      if (isRPCMessage(data) && data.args) {
+        data.args = serialize(removeTrailingUndefined(data.args)).toString(
+          'base64'
+        );
+      }
+
+      return send(process, data);
     },
-    getMessageData: (data) => {
-      return deserialize(Buffer.from(data, 'base64'));
+    getMessageData(data) {
+      if (isRPCMessage(data) && data.args && typeof data.args === 'string') {
+        data.args = deserialize(Buffer.from(data.args, 'base64'));
+      }
+
+      return data;
     },
   };
 }
