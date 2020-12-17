6604243bb87a2352280c4460171f1bd60dad679f
rosem <michael_rose@gmx.de>
2020-12-17 17:26:48 +0100

    add CRL file extraction in mapper

--- packages/cli-repl/src/arg-mapper.ts
+++ packages/cli-repl/src/arg-mapper.ts
@@ -1,4 +1,6 @@
-import { MongoClientOptions, CliOptions } from '@mongosh/service-provider-server';
+import { MongoshInvalidInputError } from '@mongosh/errors';
+import { CliOptions, MongoClientOptions } from '@mongosh/service-provider-server';
+import { promises as fs } from 'fs';
 import setValue from 'lodash.set';
 
 /**
@@ -11,17 +13,32 @@ const MAPPINGS = {
   authenticationMechanism: 'authMechanism',
   keyVaultNamespace: 'autoEncryption.keyVaultNamespace',
   password: 'auth.password',
-  quiet: [ 'loggerLevel', 'error' ],
+  quiet: { opt: 'loggerLevel', val: 'error' },
   retryWrites: 'retryWrites',
   tls: 'tls',
   tlsAllowInvalidCertificates: 'tlsAllowInvalidCertificates',
   tlsAllowInvalidHostnames: 'tlsAllowInvalidHostnames',
   tlsCAFile: 'tlsCAFile',
+  tlsCRLFile: { opt: 'sslCRL', val: readCrlFileContent },
   tlsCertificateKeyFile: 'tlsCertificateKeyFile',
   tlsCertificateKeyFilePassword: 'tlsCertificateKeyFilePassword',
   username: 'auth.username',
-  verbose: [ 'loggerLevel', 'debug' ]
+  verbose: { opt: 'loggerLevel', val: 'debug' }
 };
+
+async function readCrlFileContent(crlFilePath: string): Promise<string | Buffer> {
+  let error: Error | undefined = undefined;
+  try {
+    if ((await fs.stat(crlFilePath)).isFile()) {
+      return await fs.readFile(crlFilePath, { encoding: 'utf-8' });
+    }
+  } catch (e) {
+    error = e;
+  }
+  throw new MongoshInvalidInputError(
+    `The file specified by --tlsCRLFile does not exist or cannot be read${error ? ': ' + error.message : ''}`
+  );
+}
 
 function isExistingMappingKey(key: string, options: CliOptions): key is keyof typeof MAPPINGS {
   return MAPPINGS.hasOwnProperty(key) && options.hasOwnProperty(key);
@@ -39,18 +56,24 @@ async function mapCliToDriver(options: CliOptions): Promise<MongoClientOptions> 
   // @note: Durran: TS wasn't liking shorter reduce function here.
   //   come back an revisit to refactor.
   const nodeOptions = {};
-  Object.keys(MAPPINGS).forEach((cliOption) => {
+  await Promise.all(Object.keys(MAPPINGS).map(async(cliOption) => {
     if (isExistingMappingKey(cliOption, options)) {
       const mapping = MAPPINGS[cliOption as keyof typeof MAPPINGS];
-      if (Array.isArray(mapping)) {
-        if ((options as any)[cliOption]) {
-          setValue(nodeOptions, mapping[0], mapping[1]);
+      if (typeof mapping === 'object') {
+        const cliValue = (options as any)[cliOption];
+        if (cliValue) {
+          const { opt, val } = mapping;
+          if (typeof val === 'function') {
+            setValue(nodeOptions, opt, await val(cliValue));
+          } else {
+            setValue(nodeOptions, opt, val);
+          }
         }
       } else {
         setValue(nodeOptions, mapping, (options as any)[cliOption]);
       }
     }
-  });
+  }));
   return nodeOptions;
 }
 
