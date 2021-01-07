c239c9905c9115305e21d2accfc8f49002857ecd
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-01-07 16:29:09 +0100

    feat(node-runtime-worker-thread): Implement worker runtime public interface

---
+++ packages/node-runtime-worker-thread/src/index.spec.ts
@@ -0,0 +1,54 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import chaiAsPromised from 'chai-as-promised';
+import { startTestServer } from '../../../testing/integration-testing-hooks';
+import { WorkerRuntime } from '../dist/index';
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+describe('WorkerRuntime', () => {
+  describe('evaluate', () => {
+    it('should evaluate and return basic values', async() => {
+      const runtime = new WorkerRuntime('mongodb://nodb/', {}, { nodb: true });
+      const result = await runtime.evaluate('1+1');
+
+      expect(result.rawValue).to.equal(2);
+
+      await runtime.terminate();
+    });
+  });
+
+  describe('getCompletions', () => {
+    const testServer = startTestServer('shared');
+
+    it('should return completions', async() => {
+      const runtime = new WorkerRuntime(await testServer.connectionString());
+      const completions = await runtime.getCompletions('db.coll1.f');
+
+      expect(completions).to.deep.contain({ completion: 'db.coll1.find' });
+
+      await runtime.terminate();
+    });
+  });
+
+  describe('setEvaluationListener', () => {
+    it('allows to set evaluation listener for runtime', async() => {
+      const evalListener = {
+        onPrompt: sinon.spy(() => 'password123')
+      };
+
+      const runtime = new WorkerRuntime('mongodb://nodb/', {}, { nodb: true });
+
+      runtime.setEvaluationListener(evalListener);
+
+      const password = await runtime.evaluate('passwordPrompt()');
+
+      expect(evalListener.onPrompt).to.have.been.called;
+      expect(password.rawValue).to.equal('password123');
+
+      await runtime.terminate();
+    });
+  });
+});
--- packages/node-runtime-worker-thread/src/index.ts
+++ packages/node-runtime-worker-thread/src/index.ts
@@ -1,3 +1,119 @@
-class WorkerRuntime {}
+import { ChildProcess } from 'child_process';
+import { MongoClientOptions } from '@mongosh/service-provider-core';
+import { Runtime } from '@mongosh/browser-runtime-core';
+import { EvaluationListener } from '@mongosh/shell-evaluator';
+import spawnChildFromSource from './spawn-child-from-source';
+import { Caller, createCaller, exposeAll, WithClose } from './rpc';
+import type { WorkerRuntime as WorkerThreadWorkerRuntime } from './worker-runtime';
+import childProcessProxySrc from 'inline-entry-loader!./child-process-proxy';
 
-export default WorkerRuntime;
+type ChildProcessRuntime = Caller<WorkerThreadWorkerRuntime>;
+
+class WorkerEvaluationListener {
+  exposedListener: WithClose<EvaluationListener>;
+
+  constructor(workerRuntime: WorkerRuntime, childProcess: ChildProcess) {
+    this.exposedListener = exposeAll<EvaluationListener>(
+      {
+        onPrompt(question, type) {
+          if (workerRuntime.evaluationListener?.onPrompt) {
+            return workerRuntime.evaluationListener.onPrompt(question, type);
+          }
+
+          return '';
+        },
+        onPrint(value) {
+          if (workerRuntime.evaluationListener?.onPrint) {
+            workerRuntime.evaluationListener.onPrint(value);
+          }
+        },
+        toggleTelemetry(enabled) {
+          if (workerRuntime.evaluationListener?.toggleTelemetry) {
+            workerRuntime.evaluationListener.toggleTelemetry(enabled);
+          }
+        },
+        onClearCommand() {
+          if (workerRuntime.evaluationListener?.onClearCommand) {
+            workerRuntime.evaluationListener.onClearCommand();
+          }
+        },
+        onExit() {
+          if (workerRuntime.evaluationListener?.onExit) {
+            return workerRuntime.evaluationListener.onExit();
+          }
+
+          return Promise.resolve() as Promise<never>;
+        }
+      },
+      childProcess
+    );
+  }
+}
+
+class WorkerRuntime implements Runtime {
+  private initOptions: {
+    uri: string;
+    driverOptions: MongoClientOptions;
+    cliOptions: { nodb?: boolean };
+  };
+
+  evaluationListener: EvaluationListener | null = null;
+
+  private childProcessEvaluationListener!: WorkerEvaluationListener;
+
+  private childProcess!: ChildProcess;
+
+  private childProcessRuntime!: ChildProcessRuntime;
+
+  private initWorkerPromise: Promise<void>;
+
+  constructor(
+    uri: string,
+    driverOptions: MongoClientOptions = {},
+    cliOptions: { nodb?: boolean } = {}
+  ) {
+    this.initOptions = { uri, driverOptions, cliOptions };
+    this.initWorkerPromise = this.initWorker();
+  }
+
+  private async initWorker() {
+    this.childProcess = await spawnChildFromSource(childProcessProxySrc);
+
+    this.childProcessRuntime = createCaller(
+      ['init', 'evaluate', 'getCompletions', 'setEvaluationListener'],
+      this.childProcess
+    );
+
+    this.childProcessEvaluationListener = new WorkerEvaluationListener(
+      this,
+      this.childProcess
+    );
+
+    const { uri, driverOptions, cliOptions } = this.initOptions;
+
+    await this.childProcessRuntime.init(uri, driverOptions, cliOptions);
+  }
+
+  async evaluate(code: string) {
+    await this.initWorkerPromise;
+    return this.childProcessRuntime.evaluate(code);
+  }
+
+  async getCompletions(code: string) {
+    await this.initWorkerPromise;
+    return await this.childProcessRuntime.getCompletions(code);
+  }
+
+  setEvaluationListener(listener: EvaluationListener | null) {
+    const prev = this.evaluationListener;
+    this.evaluationListener = listener;
+    return prev;
+  }
+
+  async terminate() {
+    await this.initWorkerPromise;
+    this.childProcess.kill('SIGTERM');
+  }
+}
+
+export { WorkerRuntime };
