be9ba41664a31bad894622b1c3ce747ce30c8cd6
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-01-27 13:48:30 +0100

    fix(browser-repl): Fix more failing tests that depended on cursor printable

--- packages/browser-repl/src/components/types/cursor-iteration-result-output.spec.tsx
+++ packages/browser-repl/src/components/types/cursor-iteration-result-output.spec.tsx
@@ -7,13 +7,18 @@ import { ObjectOutput } from './object-output';
 
 describe('CursorIterationResultOutput', () => {
   it('renders no ObjectOutput if value is empty', () => {
-    const wrapper = shallow(<CursorIterationResultOutput value={[]} />);
+    const printable = { documents: [], cursorHasMore: false };
+    const wrapper = shallow(<CursorIterationResultOutput value={printable} />);
 
     expect(wrapper.text()).to.contain('no cursor');
   });
 
   it('renders a ObjectOutput for each element in value', () => {
-    const wrapper = shallow(<CursorIterationResultOutput value={[{ doc: 1 }, { doc: 2 }]} />);
+    const printable = {
+      documents: [{ doc: 1 }, { doc: 2 }],
+      cursorHasMore: false
+    };
+    const wrapper = shallow(<CursorIterationResultOutput value={printable} />);
 
     expect(wrapper.find(ObjectOutput)).to.have.lengthOf(2);
   });
