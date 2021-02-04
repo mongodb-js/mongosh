70c893253775768bd93de53f7dcce9c16c282c70
Anna Henningsen <anna.henningsen@mongodb.com>
2021-02-04 22:04:54 +0100

    fixup! feat(build): add MSI artifact MONGOSH-125

--- .evergreen/test-package-win32.sh
+++ .evergreen/test-package-win32.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-if echo "$ARTIFACT_URL" | grep -f .zip; then
+if echo "$ARTIFACT_URL" | grep -q .zip; then
   curl -sSfL "$ARTIFACT_URL" > mongosh.zip
   export ARTIFACT_PATH="$PWD/mongosh.zip"
   (cd /cygdrive/c/Program\ Files/ && rm -rf mongosh && mkdir mongosh && cd mongosh && unzip "$ARTIFACT_PATH" && chmod -v +x bin/*)
+  export PATH="/cygdrive/c/Program Files/mongosh/bin:$PATH"
 else
   curl -sSfL "$ARTIFACT_URL" > mongosh.msi
-  export ARTIFACT_PATH="$PWD/mongosh.msi"
-  (cd /cygdrive/c/Program\ Files/ && rm -rf mongosh && mkdir mongosh && cd mongosh && msiexec /i /jm "$(cygpath -w "$ARTIFACT_PATH")" /qn && chmod -v +x bin/*)
+  export ARTIFACT_PATH="$(cygpath -w "$PWD/mongosh.msi")"
+  (msiexec /i "$ARTIFACT_PATH" ALLUSERS=1 /qn && cd /cygdrive/c/Program\ Files/mongosh && chmod +x *.exe)
+  export PATH="/cygdrive/c/Program Files/mongosh:$PATH"
 fi
-export PATH="/cygdrive/c/Program Files/mongosh/bin:$PATH"
 mongosh --smokeTests
 (cd /cygdrive/c/Program\ Files/ && rm -rf mongosh)
