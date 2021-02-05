bd1ed5cb4d2d97d6cf8a589663cdd97ea5c22ace
Anna Henningsen <anna.henningsen@mongodb.com>
2021-02-05 11:30:13 +0100

    fixup! feat(build): add MSI artifact MONGOSH-125

--- packages/build/src/build-variant.spec.ts
+++ packages/build/src/build-variant.spec.ts
@@ -4,42 +4,13 @@ import BuildVariant, { ALL_BUILD_VARIANTS } from './build-variant';
 describe('BuildVariant', () => {
   describe('all build variants', () => {
     it('has all of them', () => {
-      expect(ALL_BUILD_VARIANTS).to.have.length(5);
+      expect(ALL_BUILD_VARIANTS).to.have.length(6);
       expect(ALL_BUILD_VARIANTS).to.contain(BuildVariant.Windows);
+      expect(ALL_BUILD_VARIANTS).to.contain(BuildVariant.WindowsMSI);
       expect(ALL_BUILD_VARIANTS).to.contain(BuildVariant.MacOs);
       expect(ALL_BUILD_VARIANTS).to.contain(BuildVariant.Linux);
       expect(ALL_BUILD_VARIANTS).to.contain(BuildVariant.Debian);
       expect(ALL_BUILD_VARIANTS).to.contain(BuildVariant.Redhat);
-    });
-  });
-
-  describe('BuildVariant.Windows', () => {
-    it('returns win32', () => {
-      expect(BuildVariant.Windows).to.equal('win32');
-    });
-  });
-
-  describe('BuildVariant.MacOs', () => {
-    it('returns darwin', () => {
-      expect(BuildVariant.MacOs).to.equal('darwin');
-    });
-  });
-
-  describe('BuildVariant.Linux', () => {
-    it('returns linux', () => {
-      expect(BuildVariant.Linux).to.equal('linux');
-    });
-  });
-
-  describe('BuildVariant.Debian', () => {
-    it('returns debian', () => {
-      expect(BuildVariant.Debian).to.equal('debian');
-    });
-  });
-
-  describe('BuildVariant.Redhat', () => {
-    it('returns rhel', () => {
-      expect(BuildVariant.Redhat).to.equal('rhel');
     });
   });
 });
