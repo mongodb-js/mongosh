6bfb6208de7165f37d59831b01e3cdda90f86f2a
Michael Rose <michael_rose@gmx.de>
2021-02-11 09:32:31 +0100

    chore(build): refactor package

--- packages/build/src/package/run-package.ts
+++ packages/build/src/package/run-package.ts
@@ -1,10 +1,10 @@
 import { constants as fsConstants, promises as fs } from 'fs';
 import os from 'os';
-import Config from './config';
+import Config from '../config';
 import { downloadMongocrypt } from './download-mongocryptd';
-import macOSSignAndNotarize from './macos-sign';
-import Platform from './platform';
-import { createTarball, TarballFile } from './tarball';
+import { macOSSignAndNotarize } from './macos-sign';
+import Platform from '../platform';
+import { createTarball, TarballFile } from '../tarball';
 
 export async function runPackage(
   config: Config,
@@ -22,10 +22,14 @@ export async function runPackage(
   // Zip the executable, or, on macOS, do it as part of the
   // notarization/signing step.
   if (os.platform() === Platform.MacOs) {
-    return await macOSSignAndNotarize([
-      config.executablePath,
-      config.mongocryptdPath
-    ], config, runCreateTarball);
+    return await macOSSignAndNotarize(
+      [
+        config.executablePath,
+        config.mongocryptdPath
+      ],
+      config,
+      runCreateTarball
+    );
   }
   return await runCreateTarball();
 }
