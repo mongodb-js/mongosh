fdd96f03443f4d1bbe69083762ef1c1aeffea4b3
Michael Rose <michael_rose@gmx.de>
2021-02-18 16:22:44 +0100

    fixup

--- packages/build/src/helpers/user-input.ts
+++ packages/build/src/helpers/user-input.ts
@@ -2,20 +2,16 @@
 import readline from 'readline';
 
 export async function ask(prompt: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    try {
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
+  return new Promise(resolve => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-      rl.question(`${prompt} `, (answer) => {
-        rl.close();
-        resolve(answer);
-      });
-    } catch (e) {
-      reject(e);
-    }
+    rl.question(`${prompt} `, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
   });
 }
 
