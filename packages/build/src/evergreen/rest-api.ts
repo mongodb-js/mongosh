8829ff56ca035b50c1230d04d8f5ef35874befc9
Michael Rose <michael_rose@gmx.de>
2021-02-26 09:11:44 +0100

    chore(build): fix detection of tasks for latest draft build
    The Evergreen REST API returns all tasks that ever ran _based_ on the commit SHA. This includes multiple waterfall builds (e.g.  because these were triggered by tags), and also patch builds that are based on the given SHA.
    
    We therefore have to filter based on the draft tag we are looking for.

--- packages/build/src/evergreen/rest-api.ts
+++ packages/build/src/evergreen/rest-api.ts
@@ -46,13 +46,26 @@ export class EvergreenApi {
     );
   }
 
+  /**
+   * This will return the tasks that were executed for a given commit SHA.
+   * HOWEVER, Evergreen will return tasks from _all_ patch / waterfull runs based on the commit.
+   * One way to narrow the tasks down is to filter by a given tag that was used to trigger
+   * the waterfall build.
+   *
+   * @param project The Evergreen project
+   * @param commitSha The commit SHA of the desired commit to get tasks for
+   * @param tagFilter An optional filter to only look for tasks triggered by a specific tag
+   */
   public async getTasks(
     project: string,
-    commitSha: string
+    commitSha: string,
+    tagFilter?: string
   ): Promise<EvergreenTask[]> {
-    return await this.apiGET<EvergreenTask[]>(
-      `/projects/${project}/revisions/${commitSha}/tasks`
+    // we use limit=5000 to make sure we really get all of those tasks (default limit is 100)
+    const tasks = await this.apiGET<EvergreenTask[]>(
+      `/projects/${project}/revisions/${commitSha}/tasks?limit=5000`
     );
+    return !tagFilter ? tasks : tasks.filter(t => t.version_id.startsWith(`${project}_${tagFilter.replace(/-/g, '_')}`));
   }
 
   private async apiGET<T>(path: string): Promise<T> {
