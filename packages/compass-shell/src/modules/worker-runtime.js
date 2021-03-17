f5e64c0b9dd3035e1f2ac38382fd6ba7d46fb794
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-03-17 09:36:19 +0100

    fix(compass-shell): Change runtime resolution to fix running compass-shell locally

--- packages/compass-shell/src/modules/worker-runtime.js
+++ packages/compass-shell/src/modules/worker-runtime.js
@@ -1,11 +1,14 @@
-import { createRequire } from 'module';
-
 /**
  * @type {{ WorkerRuntime: typeof import('@mongosh/node-runtime-worker-thread').WorkerRuntime }}
  */
 const { WorkerRuntime } = (() => {
   // Workaround for webpack require that overrides global require
-  const req = createRequire(__filename);
+  const req =
+    // eslint-disable-next-line camelcase
+    typeof __webpack_require__ === 'function'
+      ? // eslint-disable-next-line camelcase, no-undef
+      __non_webpack_require__
+      : require;
   const realModulePath = req.resolve('@mongosh/node-runtime-worker-thread');
   // Runtime needs to be outside the asar bundle to function properly, so if we
   // resolved it inside of one, we will try to import it from outside (and hard
