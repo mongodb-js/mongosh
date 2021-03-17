87b0ad4fa41ded50d528e26bb8c30f2ae0755198
Anna Henningsen <anna.henningsen@mongodb.com>
2021-03-17 15:16:45 +0100

    fixup: update pipelines on 4.2+ only

--- packages/shell-api/src/integration.spec.ts
+++ packages/shell-api/src/integration.spec.ts
@@ -754,14 +754,17 @@ describe('Shell API (integration)', function() {
         ).to.deep.include({ doc: 3 });
       });
 
-      it('allows update pipelines', async() => {
-        await collection.findAndModify({
-          query: { doc: 1 }, new: true, update: [ { $set: { foo: 'bar' } } ]
-        });
+      context('on server 4.2+', () => {
+        skipIfServerVersion(testServer, '< 4.2');
+        it('allows update pipelines', async() => {
+          await collection.findAndModify({
+            query: { doc: 1 }, new: true, update: [ { $set: { foo: 'bar' } } ]
+          });
 
-        expect(
-          await findAllWithoutId(dbName, collectionName)
-        ).to.deep.include({ doc: 1, foo: 'bar' });
+          expect(
+            await findAllWithoutId(dbName, collectionName)
+          ).to.deep.include({ doc: 1, foo: 'bar' });
+        });
       });
     });
 
