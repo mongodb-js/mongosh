06dff02c144428bd3b4d4b7051e3046c1258ed70
Anna Henningsen <anna.henningsen@mongodb.com>
2021-04-23 18:39:51 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! feat: add global shell settings API MONGOSH-635

--- packages/i18n/src/locales/en_US.ts
+++ packages/i18n/src/locales/en_US.ts
@@ -1247,6 +1247,11 @@ const translations: Catalog = {
           }
         }
       },
+      DBQuery: {
+        help: {
+          description: 'Deprecated -- use cursor.batchSize(value) or config.set("batchSize", value) instead of DBQuery.batchSize = value',
+        }
+      },
       Explainable: {
         help: {
           description: 'Explainable Class',
