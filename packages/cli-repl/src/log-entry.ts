b862a00166a083002b04ff7cf17cc6af521b30e9
Anna Henningsen <anna.henningsen@mongodb.com>
2021-05-04 11:27:21 +0200

    fix(cli-repl): handle pre-4.4 logs for startup warnings MONGOSH-743 (#837)

---
+++ packages/cli-repl/src/log-entry.ts
@@ -0,0 +1,37 @@
+export type LogEntry = {
+  timestamp: string;
+  severity: string;
+  component: string;
+  context: string;
+  message: string;
+  id: number | undefined;
+  attr: any;
+};
+
+// Parse a log line from mongod < 4.4, i.e. before structured logging came into
+// existence. You may have seen code like this before. :)
+function parseOldLogEntry(line: string): LogEntry {
+  const re = /^(?<timestamp>\S*) *(?<severity>\S*) *(?<component>\S*) *\[(?<context>[^\]]+)\]\s*(?<message>.*)$/;
+  const match = line.trim().match(re);
+  if (!match) {
+    throw new Error(`Could not parse line ${JSON.stringify(line)}`);
+  }
+  return match.groups as unknown as LogEntry;
+}
+
+export function parseAnyLogEntry(line: string): LogEntry {
+  try {
+    const newFormat = JSON.parse(line);
+    return {
+      id: newFormat.id,
+      timestamp: newFormat.t?.$date,
+      severity: newFormat.s,
+      component: newFormat.c,
+      context: newFormat.ctx,
+      message: newFormat.msg,
+      attr: newFormat.attr
+    };
+  } catch {
+    return parseOldLogEntry(line);
+  }
+}
