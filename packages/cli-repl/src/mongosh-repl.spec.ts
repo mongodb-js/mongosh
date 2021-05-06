26a0de27bb285b1990d1b3891c118f0fe5d777f4
Anna Henningsen <anna.henningsen@mongodb.com>
2021-05-06 13:21:03 +0200

    fixup: regression test for execution order problem

--- packages/cli-repl/src/mongosh-repl.spec.ts
+++ packages/cli-repl/src/mongosh-repl.spec.ts
@@ -226,6 +226,19 @@ describe('MongoshNodeRepl', () => {
       await waitEval(bus);
       expect(mongoshRepl.runtimeState().repl.listenerCount('SIGINT')).to.equal(1);
     });
+
+    it('does not run statements that should not run', async() => {
+      input.write(`
+      sleep(0);
+      throw new Error();
+      if (false)
+        print ('!this should not run!');
+      `);
+      for (let i = 0; i < 20; i++) {
+        await tick();
+      }
+      expect(output).not.to.include('!this should not run!');
+    });
   });
 
   context('with terminal: true', () => {
