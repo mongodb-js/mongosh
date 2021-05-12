f9c7dc49d9e65477a2617b2ecb8d9a4806df95fb
Anna Henningsen <anna.henningsen@mongodb.com>
2021-05-12 15:44:32 +0200

    chore(ci): match server OS/arch matrix MONGOSH-424

---
+++ scripts/docker/amazonlinux2-rpm.Dockerfile
@@ -0,0 +1,7 @@
+FROM amazonlinux:2
+
+ARG artifact_url=""
+ADD ${artifact_url} /tmp
+RUN yum repolist
+RUN yum install -y /tmp/*mongosh-*-x86_64.rpm
+ENTRYPOINT [ "mongosh" ]
--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -5,11 +5,11 @@ cd "$(dirname "$0")"
 
 if [ x"$ARTIFACT_URL" = x"" ]; then
   SHA=`git rev-parse origin/main`
-  VERSION=`git show ${SHA}:lerna.json | grep version | cut -d ":" -f 2 | cut -d '"' -f 2`
-  if echo "$1" | grep -q -- deb.Dockerfile; then
-    ARTIFACT_URL="https://s3.amazonaws.com/mciuploads/mongosh/${SHA}/mongosh_${VESRION}_amd64.deb"
+  VERSION=`git show ${SHA}:../../lerna.json | grep version | cut -d ":" -f 2 | cut -d '"' -f 2`
+  if echo "$1" | grep -q -- 'deb$'; then
+    ARTIFACT_URL="https://s3.amazonaws.com/mciuploads/mongosh/${SHA}/mongodb-mongosh_${VERSION}_amd64.deb"
   else
-    ARTIFACT_URL="https://s3.amazonaws.com/mciuploads/mongosh/${SHA}/mongosh-${VERSION}-x86_64.rpm"
+    ARTIFACT_URL="https://s3.amazonaws.com/mciuploads/mongosh/${SHA}/mongodb-mongosh-${VERSION}-x86_64.rpm"
   fi
 fi
 
