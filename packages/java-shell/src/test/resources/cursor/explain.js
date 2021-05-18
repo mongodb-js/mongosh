c923cffb751d904a529af45f78670a7de1e4346e
Anna Henningsen <anna.henningsen@mongodb.com>
2021-05-18 11:54:49 +0200

    chore(java-shell): skip executionStats validation in tests (#873)
    The SBE engine has been turned on by default in the latest server
    versions, which returns a different explain output in executionStats.
    Therefore, skip validation in the java-shell tests, which currently
    expect the pre-5.0 format.

--- packages/java-shell/src/test/resources/cursor/explain.js
+++ packages/java-shell/src/test/resources/cursor/explain.js
@@ -13,11 +13,5 @@ db.coll.find().explain();
 db.coll.find().explain("executionStats");
 // command containsProperty=executionStats
 db.coll.find().explain("queryPlanner");
-// command extractProperty=executionStats extractProperty=executionStages extractProperty=limitAmount
-db.coll.find().limit(42).explain("executionStats");
-// command extractProperty=executionStats extractProperty=executionStages extractProperty=filter
-db.coll.find({name: "Vasya"}).explain("executionStats");
-// command extractProperty=executionStats extractProperty=executionStages extractProperty=transformBy
-db.coll.find({}, {_id: -1}).explain("executionStats");
 // clear
 db.coll.drop();
