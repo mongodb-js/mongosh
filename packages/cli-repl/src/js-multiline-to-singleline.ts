a3f993f2711f22618d06d6cdf5b554e83acf48be
Michael Rose <michael_rose@gmx.de>
2021-05-21 13:11:27 +0200

    fix(cli-repl): do not throw internal errors for invalid user code MONGOSH-777 (#895)

--- packages/cli-repl/src/js-multiline-to-singleline.ts
+++ packages/cli-repl/src/js-multiline-to-singleline.ts
@@ -1,7 +1,7 @@
 import { transformSync } from '@babel/core';
 
 export function makeMultilineJSIntoSingleLine(src: string): string {
-  // We use babel without any actual transofmration steps, and only for ASI
+  // We use babel without any actual transformation steps, and only for ASI
   // effects here, e.g. turning `return\n42` into `return;\n42;`
   // since without the added semicolons semantics would be different.
   // This unfortunately modifies the code in some other ways as well, e.g.
@@ -9,12 +9,20 @@ export function makeMultilineJSIntoSingleLine(src: string): string {
   // important here than keeping aesthetics intact.
   // It would be nice to have a dedicated package at some point that does
   // ASI and *only* ASI and leaves the source intact otherwise.
-  const postASI = transformSync(src, {
-    retainLines: true,
-    compact: false,
-    code: true,
-    comments: false
-  })?.code ?? src;
+  let postASI: string;
+  try {
+    postASI = transformSync(src, {
+      retainLines: true,
+      compact: false,
+      code: true,
+      comments: false
+    })?.code ?? src;
+  } catch {
+    // The src might still be invalid, e.g. because a recoverable error was not fixed
+    // and is now an unrecoverable error. Best we can do is just keep the lines now.
+    postASI = src;
+  }
+
   const asSingleLine = postASI.split(/[\r\n]+/g)
     .map(line => line.trim())
     .join(' ')
