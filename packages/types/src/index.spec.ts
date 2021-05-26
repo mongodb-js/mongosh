9044d8c6776e0302dfe2291ef8cea3ec4234d176
Anna Henningsen <anna.henningsen@mongodb.com>
2021-05-26 10:55:53 +0200

    chore(types): increase coverage (#903)

--- packages/types/src/index.spec.ts
+++ packages/types/src/index.spec.ts
@@ -1,4 +1,4 @@
-import { CliUserConfigValidator } from './';
+import { CliUserConfig, CliUserConfigValidator } from './';
 import { expect } from 'chai';
 
 describe('config validation', () => {
@@ -29,5 +29,17 @@ describe('config validation', () => {
     expect(await validate('enableTelemetry', -1)).to.equal('enableTelemetry must be a boolean');
     expect(await validate('enableTelemetry', false)).to.equal(null);
     expect(await validate('enableTelemetry', true)).to.equal(null);
+    expect(await validate('inspectCompact', 'foo')).to.equal('inspectCompact must be a boolean or a positive integer');
+    expect(await validate('inspectCompact', -1)).to.equal('inspectCompact must be a boolean or a positive integer');
+    expect(await validate('inspectCompact', false)).to.equal(null);
+    expect(await validate('inspectCompact', true)).to.equal(null);
+    expect(await validate('inspectCompact', 32)).to.equal(null);
+  });
+
+  it('allows default CliUserConfig values', async() => {
+    const userConfig = new CliUserConfig();
+    for (const key of Object.keys(userConfig) as (keyof CliUserConfig)[]) {
+      expect(await CliUserConfigValidator.validate(key, userConfig[key])).to.equal(null);
+    }
   });
 });
