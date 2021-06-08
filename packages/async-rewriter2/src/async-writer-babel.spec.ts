fbad71a60ea68ccd05ef3788ef36a3ccc080d837
Anna Henningsen <anna.henningsen@mongodb.com>
2021-06-08 11:32:50 +0200

    fixup: more tests

--- packages/async-rewriter2/src/async-writer-babel.spec.ts
+++ packages/async-rewriter2/src/async-writer-babel.spec.ts
@@ -370,8 +370,20 @@ describe('AsyncWriter', () => {
       expect(obj).to.deep.equal({});
     });
 
+    it('supports delete for implicitly awaited values (indexed)', async() => {
+      const obj = { foo: 'bar' };
+      implicitlyAsyncFn.resolves(obj);
+      expect(await runTranspiledCode('delete implicitlyAsyncFn()["foo"]')).to.equal(true);
+      expect(obj).to.deep.equal({});
+    });
+
     it('supports delete for plain values', async() => {
       expect(runTranspiledCode('const obj = { x: "x", y: "y" }; delete obj.x; obj'))
+        .to.deep.equal({ y: 'y' });
+    });
+
+    it('supports delete for plain values (idnexed)', async() => {
+      expect(runTranspiledCode('const obj = { x: "x", y: "y" }; delete obj["x"]; obj'))
         .to.deep.equal({ y: 'y' });
     });
 
