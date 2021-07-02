4233e6b078ab94fdd27f39bf35514bd23425cd34
Michael Rose <michael_rose@gmx.de>
2021-07-02 14:36:26 +0200

    chore(test): add E2E connectivity tests for Kerberos MONGOSH-837

--- packages/connectivity-tests/test/all.sh
+++ packages/connectivity-tests/test/all.sh
@@ -9,13 +9,14 @@ TEST_TMPDIR="$MONGOSH_ROOT_DIR"/tmp/connectivitiy
 rm -rf "$TEST_TMPDIR"
 mkdir -p "$TEST_TMPDIR"
 cd "$TEST_TMPDIR"
-TEST_TMPDIR="$PWD"
+export TEST_TMPDIR="$PWD"
 
 git clone git@github.com:mongodb-js/devtools-docker-test-envs.git test-envs
 cd test-envs
 
-git checkout 3d04063586ba95a3bd72d38f5569469c81e54987
+git checkout e7fc1feeb724e262f6c12ebea76a82d4662dffaa
 
 source "$CONNECTIVITY_TEST_SOURCE_DIR/ldap.sh"
 source "$CONNECTIVITY_TEST_SOURCE_DIR/localhost.sh"
 source "$CONNECTIVITY_TEST_SOURCE_DIR/atlas.sh"
+source "$CONNECTIVITY_TEST_SOURCE_DIR/kerberos.sh"
---
+++ packages/connectivity-tests/test/kerberos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+set -x
+
+FAILED=no
+docker-compose \
+  -f "$TEST_TMPDIR/test-envs/kerberos/docker-compose.yaml" \
+  -f "$CONNECTIVITY_TEST_SOURCE_DIR/kerberos/docker-compose.kerberos.yaml" \
+  --no-ansi \
+  up --exit-code-from kerberos_jumphost --abort-on-container-exit || FAILED=yes
+
+docker-compose \
+  -f "$TEST_TMPDIR/test-envs/kerberos/docker-compose.yaml" \
+  -f "$CONNECTIVITY_TEST_SOURCE_DIR/kerberos/docker-compose.kerberos.yaml" \
+  --no-ansi \
+  down -v
+
+if [ $FAILED = yes ]; then
+  exit 1
+fi
