813200d8216b1faaa7dc26a289b14c72a03f5546
Michael Rose <michael_rose@gmx.de>
2021-07-30 16:26:49 +0200

    chore(test): split test execution by package MONGOSH-906 (#1046)

---
+++ scripts/run-if-package-requested.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+'use strict';
+const path = require('path');
+const packageDir = path.basename(process.cwd());
+if (process.env.MONGOSH_RUN_ONLY_IN_PACKAGE && process.env.MONGOSH_RUN_ONLY_IN_PACKAGE !== packageDir) {
+  process.stderr.write(
+    `${packageDir} is not ${process.env.MONGOSH_RUN_ONLY_IN_PACKAGE}, skipping "${process.argv.slice(2).join(' ')}"\n`);
+  return;
+}
+const child_process = require('child_process');
+child_process.spawn(process.argv[2], process.argv.slice(3), { stdio: 'inherit', shell: process.platform === 'win32' })
+  .on('exit', (code, signal) => {
+    if (signal) {
+      process.kill(process.pid, signal);
+    }
+    process.exit(code);
+  });
