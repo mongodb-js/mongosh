b1e630e15e6474f2f76e7c95d417732fbe1439ff
Liudmila Kornilova <kornilova203@gmail.com>
2021-08-18 17:51:19 +0300

    feat(JMongosh): support collection.distinct()

--- packages/java-shell/src/test/resources/collection/distinct.js
+++ packages/java-shell/src/test/resources/collection/distinct.js
@@ -1,9 +1,21 @@
 // before
 db.coll.deleteMany({});
-db.coll.insertOne({a: 1});
-db.coll.insertOne({a: 2});
-db.coll.insertOne({a: 3});
+db.coll.insertOne({num: 1, str: "hello", mixedType: 1,       doc: {},     category: "café"});
+db.coll.insertOne({num: 2, str: "world", mixedType: "hello", doc: {},     category: "cafe"});
+db.coll.insertOne({num: 3, str: "hello", mixedType: 2,       doc: {a: 1}, category: "cafE", onlyInOneDoc: 1});
 // command
-db.coll.distinct("a");
+db.coll.distinct("num");
+// command
+db.coll.distinct("str");
+// command
+db.coll.distinct("mixedType");
+// command
+db.coll.distinct("onlyInOneDoc");
+// command
+db.coll.distinct("doc");
+// command
+db.coll.distinct("str", {num: 1});
+// command
+db.coll.distinct("category", {}, {collation: {locale: "fr", strength: 1}});
 // clear
 db.coll.drop();
