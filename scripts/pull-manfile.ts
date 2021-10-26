9e8b8998a32c7720b8a161bf180eab7c8c7dad57
Basit Chonka <baasit121@yahoo.com>
2021-10-26 12:54:33 +0200

    feat(manfile): setup npm script to fetch manual file

---
+++ scripts/pull-manfile.ts
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+const MAN_URL = 'https://docs.mongodb.com/mongodb-shell/manpages.tar.gz';
+
+const fetchData = async (): Promise<Buffer> => {
+  const response = await fetch(MAN_URL);
+  const data = await response.arrayBuffer();
+  return Buffer.from(data);
+}
+
+const writeDataToFile = (data: Buffer, file: string): void => {
+  writeFileSync(file, data);
+}
+
+(async () => {
+  try {
+    const data = await fetchData();
+    const file = join(__dirname, '..', 'manpages.tar.gz');
+    writeDataToFile(data, file);
+    console.log('Manual file saved.');
+  } catch (e) {
+    console.error(`Failed to write manual file. Error: ${e}`);
+  }
+})();
