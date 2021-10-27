f02e068d3613b7a02454bfe75959c2cafce15cd6
Basit Chonka <baasit121@yahoo.com>
2021-10-27 11:53:33 +0200

    fix(manfile): extract manpage config

---
+++ packages/build/src/config/manpage-config.ts
@@ -0,0 +1,4 @@
+export const manpage = {
+  downloadUrl: 'https://docs.mongodb.com/mongodb-shell/manpages.tar.gz',
+  fileName: 'manpages.tar.gz',
+};
--- packages/build/src/packaging/download-manfile.ts
+++ packages/build/src/packaging/download-manfile.ts
@@ -1,18 +1,16 @@
 import fetch from 'node-fetch';
 import { promises as fs } from 'fs';
 import { join } from 'path';
-
-const MANPAGE_URL = 'https://docs.mongodb.com/mongodb-shell/manpages.tar.gz';
-const MANPAGE_NAME = 'manpages.tar.gz';
+import { manpage as config } from './../config';
 
 const fetchData = async(): Promise<Buffer> => {
-  const response = await fetch(MANPAGE_URL);
+  const response = await fetch(config.downloadUrl);
   const data = await response.arrayBuffer();
   return Buffer.from(data);
 };
 
 export async function downloadManpage(destination: string) {
   const data = await fetchData();
-  await fs.writeFile(data, join(destination, MANPAGE_NAME));
+  await fs.writeFile(join(destination, config.fileName), data);
   console.info('Manual file saved.');
 }
