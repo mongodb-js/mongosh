144b368aae2b16877ec689ae7fa1e134b5cea075
Anna Henningsen <anna.henningsen@mongodb.com>
2021-12-13 16:09:22 +0100

    chore(build): create and upload .sig files for download artifacts MONGOSH-1074 (#1169)

--- packages/build/src/packaging/notary-service.spec.ts
+++ packages/build/src/packaging/notary-service.spec.ts
@@ -1,30 +1,29 @@
 import { expect } from 'chai';
-import os from 'os';
 import path from 'path';
 import sinon from 'sinon';
-import { notarizeMsi, NotarizeMsiOptions } from './msi-sign';
+import { notarizeArtifact, NotarizeOptions } from './notary-service';
 
-describe('packaging msi-sign', () => {
+describe('packaging artifact signing', () => {
   context('with invalid options', () => {
     it('fails when file is missing', async() => {
-      const e = await notarizeMsi('', {} as any).catch(e => e);
+      const e = await notarizeArtifact('', {} as any).catch(e => e);
       expect(e).to.not.be.undefined;
       expect(e.message).to.match(/missing file/);
     });
     it('fails when signingKeyName is missing', async() => {
-      const e = await notarizeMsi('a file', {} as any).catch(e => e);
+      const e = await notarizeArtifact('a file', {} as any).catch(e => e);
       expect(e).to.not.be.undefined;
       expect(e.message).to.match(/missing signing key name/);
     });
     it('fails when authToken is missing', async() => {
-      const e = await notarizeMsi('a file', {
+      const e = await notarizeArtifact('a file', {
         signingKeyName: 'keyName'
       } as any).catch(e => e);
       expect(e).to.not.be.undefined;
       expect(e.message).to.match(/missing auth token/);
     });
     it('fails when signingComment is missing', async() => {
-      const e = await notarizeMsi('a file', {
+      const e = await notarizeArtifact('a file', {
         signingKeyName: 'keyName',
         authToken: 'token'
       } as any).catch(e => e);
@@ -35,7 +34,7 @@ describe('packaging msi-sign', () => {
 
   context('with correct options', () => {
     let spawnSync: sinon.SinonStub;
-    const signingOptions: NotarizeMsiOptions = {
+    const signingOptions: NotarizeOptions = {
       signingKeyName: 'keyName',
       authToken: 'authToken',
       signingComment: 'A Comment'
@@ -46,18 +45,25 @@ describe('packaging msi-sign', () => {
     });
 
     it('runs notary client', async() => {
-      await notarizeMsi(
+      await notarizeArtifact(
         __filename,
         signingOptions,
         spawnSync
       );
 
+      const authTokenFile = spawnSync
+        .getCall(0)
+        .args[1]
+        .find((arg: string) => arg.includes('notary-mongosh-token'));
+
       expect(spawnSync).to.have.been.calledWith(
         'python',
         [
-          'C:\\cygwin\\usr\\local\\bin\\notary-client.py',
+          process.platform === 'win32' ?
+            'C:\\cygwin\\usr\\local\\bin\\notary-client.py' :
+            '/usr/local/bin/notary-client.py',
           '--key-name', 'keyName',
-          '--auth-token-file', path.join(os.homedir(), '.notary-mongosh-token.tmp'),
+          '--auth-token-file', authTokenFile,
           '--comment', 'A Comment',
           '--notary-url', 'http://notary-service.build.10gen.cc:5000/',
           '--outputs', 'sig',
