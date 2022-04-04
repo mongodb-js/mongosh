dbf8fcbe3efa27aa5dca501e2d1574eb4d327903
Anna Henningsen <anna@addaleax.net>
2022-04-04 14:02:41 +0200

    fixup: add evergreen post task for uploading npm logs

--- .evergreen/setup-env.sh
+++ .evergreen/setup-env.sh
@@ -40,6 +40,11 @@ if [ `uname` = Linux ]; then
 fi
 
 export npm_config_registry=https://registry.npmjs.org/
+export npm_config_logs_dir="$PWD/../npm-logs"
+mkdir -p "$npm_config_logs_dir"
+if [ "$OS" == "Windows_NT" ]; then
+  export npm_config_logs_dir="$(cygpath -w "$npm_config_logs_dir")"
+fi
 
 echo "Running on:"
 uname -a
