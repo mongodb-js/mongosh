deec4f7885e330f64298e4f92ade239acf47f9b9
Anna Henningsen <anna@addaleax.net>
2022-04-05 18:39:07 +0200

    Revert "fixup: de-concurrencify"
    This reverts commit 11322ab7d804f9c6c4af31149e4e6fb5c5f69294.

--- .evergreen/install-node.sh
+++ .evergreen/install-node.sh
@@ -50,6 +50,6 @@ npm ci --verbose
 # make lerna give us a useful debugging experience
 sed -i -e 's/opts.stdio = stdio/opts.stdio = "inherit"/' node_modules/@lerna/npm-install/npm-install.js
 
-npm run bootstrap-ci -- --scope @mongosh/build --ignore-prepublish --concurrency=1
-(npm run bootstrap-ci -- --scope @mongosh/cli-repl --include-dependencies --ignore-prepublish --concurrency=1 && test -e packages/service-provider-server/node_modules/mongodb-client-encryption) || \
-  npm run bootstrap-ci -- --scope @mongosh/cli-repl --include-dependencies --ignore-prepublish --ignore-scripts --concurrency=1
+npm run bootstrap-ci -- --scope @mongosh/build --ignore-prepublish
+(npm run bootstrap-ci -- --scope @mongosh/cli-repl --include-dependencies --ignore-prepublish && test -e packages/service-provider-server/node_modules/mongodb-client-encryption) || \
+  npm run bootstrap-ci -- --scope @mongosh/cli-repl --include-dependencies --ignore-prepublish --ignore-scripts
