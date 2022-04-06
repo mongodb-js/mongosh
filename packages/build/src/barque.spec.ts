2736e95fcc7fc2bb45dafb06f7b76b571a704e39
Anna Henningsen <anna@addaleax.net>
2022-04-06 16:23:41 +0200

    chore(ci): add Debian 11 support MONGOSH-1139

--- packages/build/src/barque.spec.ts
+++ packages/build/src/barque.spec.ts
@@ -42,7 +42,11 @@ describe('Barque', () => {
               'https://repo.mongodb.org/apt/debian/dists/stretch/mongodb-org/4.4/main/binary-amd64/mongodb-mongosh_0.1.0_amd64.deb',
               'https://repo.mongodb.com/apt/debian/dists/stretch/mongodb-enterprise/4.4/main/binary-amd64/mongodb-mongosh_0.1.0_amd64.deb',
               'https://repo.mongodb.org/apt/debian/dists/stretch/mongodb-org/5.0/main/binary-amd64/mongodb-mongosh_0.1.0_amd64.deb',
-              'https://repo.mongodb.com/apt/debian/dists/stretch/mongodb-enterprise/5.0/main/binary-amd64/mongodb-mongosh_0.1.0_amd64.deb'
+              'https://repo.mongodb.com/apt/debian/dists/stretch/mongodb-enterprise/5.0/main/binary-amd64/mongodb-mongosh_0.1.0_amd64.deb',
+              'https://repo.mongodb.org/apt/debian/dists/bullseye/mongodb-org/4.4/main/binary-amd64/mongodb-mongosh_0.1.0_amd64.deb',
+              'https://repo.mongodb.com/apt/debian/dists/bullseye/mongodb-enterprise/4.4/main/binary-amd64/mongodb-mongosh_0.1.0_amd64.deb',
+              'https://repo.mongodb.org/apt/debian/dists/bullseye/mongodb-org/5.0/main/binary-amd64/mongodb-mongosh_0.1.0_amd64.deb',
+              'https://repo.mongodb.com/apt/debian/dists/bullseye/mongodb-enterprise/5.0/main/binary-amd64/mongodb-mongosh_0.1.0_amd64.deb'
             ]
           },
           {
