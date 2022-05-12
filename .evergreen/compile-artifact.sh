7d8ac473860725cb51965eb21e25a474358c39d1
Anna Henningsen <anna.henningsen@mongodb.com>
2022-05-12 17:32:31 +0200

    fixup: remove python2 hacks to see what happens

--- .evergreen/compile-artifact.sh
+++ .evergreen/compile-artifact.sh
@@ -13,38 +13,6 @@ if uname -a | grep -q 'Linux.*x86_64'; then
   export CXX="$PWD/tmp/.sccache/sccache g++"
 fi
 
-if uname -p | sed -e 's/s390/matches/' -e 's/ppc/matches/' | grep -q matches; then
-  # On s390x and PPC machines in evergreen, there is a Python 3.x installation
-  # in /opt/mongodbtoolchain/v3/bin, but it's broken in the sense that its bz2
-  # package cannot be loaded, which we need for building Node.js.
-  # We therefore do some PATH wiggling here to make sure that the python
-  # executable used by the Node.js configure script is python 2.x.
-  # This is not a sustainable long-term solution, because Node.js drops Python 2
-  # support for build scripts with Node.js 15 and above.
-  # TODO: Open a build ticket about this, referencing patch/output from
-  # https://spruce.mongodb.com/version/608ad4613e8e8601d5cd3d6f/tasks.
-  rm -rf tmp/python-container
-  mkdir -p tmp/python-container
-  (cd tmp/python-container && ln -s /usr/bin/python python && ln -s python python2.7 && ln -s python python2)
-  export PATH="$PWD/tmp/python-container:$PATH"
-  export FORCE_PYTHON2=1
-
-  echo "Using python version:"
-  python --version
-fi
-
-if [ "$OS" == "Windows_NT" ]; then
-  # Windows machines only have Python 3.10 and 2.7, since Python 3.9
-  # was removed from them. Python 3.10 support may or may not be
-  # backported to Node.js 14: https://github.com/nodejs/node/pull/40296/
-  # For now, let's use Python 2.7.
-  export PATH="/cygdrive/c/Python27/Scripts:/cygdrive/c/Python27:$PATH"
-  export FORCE_PYTHON2=1
-
-  echo "Using python version:"
-  python --version
-fi
-
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
 mkdir -pv /tmp/m # Node.js compilation can fail on long path prefixes
 (env TMP=/tmp/m TMPDIR=/tmp/m npm run evergreen-release compile && rm -rf /tmp/m) || (rm -rf /tmp/m; false)
