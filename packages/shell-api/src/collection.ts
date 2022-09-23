43328fb9ea2082e614652ef6f8a32ce507f1aeb0
Alena Khineika <alena.khineika@gmail.com>
2022-09-23 15:41:51 +0200

    refactor: have no idea what happened with spacing

--- packages/shell-api/src/collection.ts
+++ packages/shell-api/src/collection.ts
@@ -642,10 +642,10 @@ export default class Collection extends ShellApiWithMongoClass {
    * @param {Object} options
    * @returns {StatisticsDocument} The promise of the result.
    */
-   @returnsPromise
-   @returnType('StatisticsDocument')
-   @serverVersions(['6.2.0', ServerVersions.latest])
-   @apiVersions([1])
+  @returnsPromise
+  @returnType('StatisticsDocument')
+  @serverVersions(['6.2.0', ServerVersions.latest])
+  @apiVersions([1])
   async analyze(options?: AnalyzeOptions): Promise<StatisticsDocument> {
     this._emitCollectionApiCall('analyze', { analyzeOptions: options });
     const result = await this._mongo._serviceProvider.analyze(
@@ -672,31 +672,31 @@ export default class Collection extends ShellApiWithMongoClass {
   @deprecated
   @serverVersions([ServerVersions.earliest, '3.6.0'])
   @apiVersions([1])
-   async insert(docs: Document | Document[], options: BulkWriteOptions = {}): Promise<InsertManyResult> {
-     await this._instanceState.printDeprecationWarning(
-       'Collection.insert() is deprecated. Use insertOne, insertMany, or bulkWrite.'
-     );
-     assertArgsDefinedType([docs], [true], 'Collection.insert');
-     // When inserting documents into MongoDB that do not contain the _id field,
-     // one will be added to each of the documents missing it by the Node driver,
-     // mutating the document. To prevent this behaviour we pass not the original document,
-     // but its copy, to keep the original document immutable.
-     // https://github.com/mongodb/node-mongodb-native/blob/3.6/lib/collection.js#L487-L489
-     const docsToInsert: Document[] = Array.isArray(docs) ? docs.map((doc) => ({ ...doc })) : [{ ...docs }];
+  async insert(docs: Document | Document[], options: BulkWriteOptions = {}): Promise<InsertManyResult> {
+    await this._instanceState.printDeprecationWarning(
+      'Collection.insert() is deprecated. Use insertOne, insertMany, or bulkWrite.'
+    );
+    assertArgsDefinedType([docs], [true], 'Collection.insert');
+    // When inserting documents into MongoDB that do not contain the _id field,
+    // one will be added to each of the documents missing it by the Node driver,
+    // mutating the document. To prevent this behaviour we pass not the original document,
+    // but its copy, to keep the original document immutable.
+    // https://github.com/mongodb/node-mongodb-native/blob/3.6/lib/collection.js#L487-L489
+    const docsToInsert: Document[] = Array.isArray(docs) ? docs.map((doc) => ({ ...doc })) : [{ ...docs }];
 
-     this._emitCollectionApiCall('insert', { options });
-     const result = await this._mongo._serviceProvider.insertMany(
-       this._database._name,
-       this._name,
-       docsToInsert,
-       { ...await this._database._baseOptions(), ...options }
-     );
+    this._emitCollectionApiCall('insert', { options });
+    const result = await this._mongo._serviceProvider.insertMany(
+      this._database._name,
+      this._name,
+      docsToInsert,
+      { ...await this._database._baseOptions(), ...options }
+    );
 
-     return new InsertManyResult(
-       !!result.acknowledged,
-       result.insertedIds
-     );
-   }
+    return new InsertManyResult(
+      !!result.acknowledged,
+      result.insertedIds
+    );
+  }
 
   /**
    * Insert multiple documents.
