45667cf9b27b6ea49203f34cbed6768e2705e420
Bailey Pearson <bailey.pearson@mongodb.com>
2023-01-31 14:18:05 -0500

    chore: rework scripts

---
+++ .evergreen/test-node-driver.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## Runs mongosh tests against the latest a particular build of the Node driver.
+## 
+## Params:
+## DRIVER_TARBALL_PATH - the full path to either the Node driver repo to be tested against or a tarball containing the built driver (built with npm pack)
+## MONGOSH_RUN_ONLY_IN_PACKAGE (optional) - a mongosh package name, without the `@mongosh` prefix.  if set, 
+## 											only the tests for the provided mognosh package are run.
+
+MONGOSH_RUN_ONLY_IN_PACKAGE=${MONGOSH_RUN_ONLY_IN_PACKAGE:-""}
+
+export REPLACE_PACKAGE="mongodb:$DRIVER_TARBALL_PATH"
+npm run replace-package
+	
+npm run bootstrap -- --no-ci
+
+npm run compile-ts
+
+if [ -n "$MONGOSH_RUN_ONLY_IN_PACKAGE" ]; then
+	npm run test-ci-nocoverage
+fi
