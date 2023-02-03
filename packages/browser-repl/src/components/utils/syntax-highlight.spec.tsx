7c114a7fd5dc0be47ddf113e716bd63e5f72c0ef
Sergey Petushkov <petushkov.sergey@gmail.com>
2023-02-03 13:22:47 +0100

    chore(browser-repl): adjust tests

--- packages/browser-repl/src/components/utils/syntax-highlight.spec.tsx
+++ packages/browser-repl/src/components/utils/syntax-highlight.spec.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import { Code } from '@mongodb-js/compass-components';
+import { SyntaxHighlight as CompassSyntaxHighlight } from '@mongodb-js/compass-editor';
 import { expect } from '../../../testing/chai';
 import { mount } from '../../../testing/enzyme';
 import { SyntaxHighlight } from './syntax-highlight';
 
 describe('<SyntaxHighlight />', () => {
+  let wrapper;
+
+  afterEach(() => {
+    wrapper.unmount();
+    wrapper = null;
+  });
+
   it('renders Code', () => {
-    const wrapper = mount(<SyntaxHighlight code={'some code'} />);
-    expect(wrapper.find(Code)).to.have.lengthOf(1);
+    wrapper = mount(<SyntaxHighlight code={'some code'} />);
+    expect(wrapper.find(CompassSyntaxHighlight)).to.have.lengthOf(1);
   });
 
   it('passes code to Code', () => {
-    const wrapper = mount(<SyntaxHighlight code={'some code'} />);
-    expect(wrapper.find(Code).children().text()).to.contain('some code');
+    wrapper = mount(<SyntaxHighlight code={'some code'} />);
+    expect(wrapper.find(CompassSyntaxHighlight).prop('text')).to.equal('some code');
   });
 
   it('uses javascript as language', () => {
-    const wrapper = mount(<SyntaxHighlight code={'some code'} />);
-    expect(wrapper.find(Code).prop('language')).to.equal('javascript');
+    wrapper = mount(<SyntaxHighlight code={'some code'} />);
+    expect(wrapper.find(CompassSyntaxHighlight).prop('language')).to.equal('javascript');
   });
 });
