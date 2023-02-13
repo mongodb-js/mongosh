f271825609b3c5d47c58500d59c4131f4887184f
Anna Henningsen <anna.henningsen@mongodb.com>
2023-02-13 12:40:44 +0100

    fix(cli-repl): call net.setDefaultAutoSelectFamily(true) MONGOSH-1378
    See the ticket, this is necessary to avoid connectivity issues
    when using Node.js 17+, which homebrew is doing for us.

--- packages/connectivity-tests/test/localhost.sh
+++ packages/connectivity-tests/test/localhost.sh
@@ -32,10 +32,39 @@ function test_for_version() {
   fi
 }
 
+# https://jira.mongodb.org/browse/MONGOSH-1378
+# Note that this test only has limited significance right now,
+# as we aren't testing the Node.js versions affected here anywhere in CI.
+function try_connect_ipv4only_dualstackhostname() {
+  docker run --rm --network host mongo:6.0 --port 27092 --syslog &
+  DOCKER_PID=$!
+
+  # Use a second docker container to be able to modify /etc/hosts easily
+  cat << 'EOF' | docker run -i --rm --network host -v /:/host ubuntu:22.04 bash && FAILED=no || FAILED=yes
+export PATH=$(echo "$PATH" | sed 's~:~:/host~g')
+
+set -e
+set -x
+
+echo -e '::1 dualstackhost\n127.0.0.1 dualstackhost\n' > /etc/hosts
+
+echo 'db.runCommand({ connectionStatus: 1 })' | \
+  mongosh "mongodb://dualstackhost:27092/?serverSelectionTimeoutMS=2000" | \
+  grep -Fq authenticatedUsers
+EOF
+  if [ $FAILED = yes ]; then
+    ANY_FAILED=yes
+    echo "Localhost test with ipv4-only access failed"
+  fi
+
+  kill -INT "$DOCKER_PID"
+}
+
 ANY_FAILED=no
 test_for_version '4.2'
 test_for_version '4.4'
 test_for_version '5.0'
+try_connect_ipv4only_dualstackhostname
 
 if [ $ANY_FAILED = yes ]; then
   exit 1
