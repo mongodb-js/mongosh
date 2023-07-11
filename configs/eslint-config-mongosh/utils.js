8ec1289c256694ed58ccb1c806277623fa07cedc
Le Roux Bodenstein <lerouxb@gmail.com>
2023-07-11 14:20:03 +0100

    remove debugging

--- configs/eslint-config-mongosh/utils.js
+++ configs/eslint-config-mongosh/utils.js
@@ -2,4 +2,4 @@ exports.fixCygwinPath = function (filepath) {
   // see https://github.com/nodejs/node/issues/34866
   // similar to what we work around in compass' CI
   return filepath.replace(/^Z:\\\\data\\\\mci/, 'C:\\\\data\\mci');
-}
+};
--- packages/history/.eslintrc.js
+++ packages/history/.eslintrc.js
@@ -1,12 +1,5 @@
 const { fixCygwinPath } = require('@mongodb-js/eslint-config-mongosh/utils');
 
-// eslint-disable-next-line no-console
-console.log('paths in .eslintrc.js', {
-  cwd: process.cwd(),
-  __dirname,
-  fixed__dirmame: fixCygwinPath(__dirname),
-});
-
 module.exports = {
   root: true,
   extends: ['@mongodb-js/eslint-config-mongosh'],
