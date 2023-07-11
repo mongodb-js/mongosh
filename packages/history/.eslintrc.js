7b13a3e0976706215ff16f46c364e3ecaf8f1689
Le Roux Bodenstein <lerouxb@gmail.com>
2023-07-11 09:14:44 +0100

    debug paths on windows

--- packages/history/.eslintrc.js
+++ packages/history/.eslintrc.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+
+// eslint-disable-next-line no-console
+console.log('paths in .eslintrc.js', {
+  cwd: process.cwd(),
+  __dirname,
+  realpath__dirname: fs.realpathSync(__dirname),
+});
+
 module.exports = {
   root: true,
   extends: ['@mongodb-js/eslint-config-mongosh'],
