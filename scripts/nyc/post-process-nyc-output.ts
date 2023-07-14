3604cc15b9d885004ed51d847fe40fa393974ef4
Le Roux Bodenstein <lerouxb@gmail.com>
2023-07-14 13:48:05 +0100

    add coverage assertions

--- scripts/nyc/post-process-nyc-output.ts
+++ scripts/nyc/post-process-nyc-output.ts
@@ -1,8 +1,12 @@
+import assert from 'assert';
 import * as path from 'path';
 import { transformCoverageFiles } from './transform-coverage';
 
 const projectRoot = path.resolve(__dirname, '..', '..');
 transformCoverageFiles(
   projectRoot,
-  p => p.replace(projectRoot, '')
+  (p) => {
+    assert.ok(p.startsWith(projectRoot), `${p} must start with ${projectRoot}`);
+    return p.replace(projectRoot, '');
+  }
 );
--- scripts/nyc/pre-process-coverage.ts
+++ scripts/nyc/pre-process-coverage.ts
@@ -1,8 +1,14 @@
+import fs from 'fs';
+import assert from 'assert';
 import * as path from 'path';
 import { transformCoverageFiles } from './transform-coverage';
 
 const projectRoot = path.resolve(__dirname, '..', '..');
 transformCoverageFiles(
   projectRoot,
-  p => projectRoot + p
+  (p) => {
+    const fullPath = projectRoot + p;
+    assert.ok(fs.existsSync(fullPath), `${fullPath} must exist`);
+    return fullPath;
+  }
 );
