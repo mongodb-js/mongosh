435e869fbc49072932504b4e168d2f0a222e4d20
Himanshu Singh <himanshu.singhs@outlook.in>
2023-08-02 17:16:07 +0200

    chore: updates createAndPublishDownloadCenterConfig in a way that it will update an existing config instead of replacing it entirely

--- packages/build/src/download-center/config.ts
+++ packages/build/src/download-center/config.ts
@@ -2,6 +2,7 @@ import {
   DownloadCenter as DownloadCenterCls,
   validateConfigSchema,
 } from '@mongodb-js/dl-center';
+import { major as majorVersion } from 'semver';
 import type { DownloadCenterConfig } from '@mongodb-js/dl-center/dist/download-center-config';
 import { CONFIGURATION_KEY, CONFIGURATIONS_BUCKET } from './constants';
 import type { PackageVariant } from '../config';
@@ -21,7 +22,22 @@ export async function createAndPublishDownloadCenterConfig(
   isDryRun: boolean,
   DownloadCenter: typeof DownloadCenterCls = DownloadCenterCls
 ): Promise<void> {
-  const config = createDownloadCenterConfig(packageInformation);
+  const dlcenter = new DownloadCenter({
+    bucket: CONFIGURATIONS_BUCKET,
+    accessKeyId: awsAccessKeyId,
+    secretAccessKey: awsSecretAccessKey,
+  });
+  const existingDownloadCenterConfig = await dlcenter.downloadConfig(
+    CONFIGURATION_KEY
+  );
+
+  const getVersionConfig = () => createVersionConfig(packageInformation);
+  const config = existingDownloadCenterConfig
+    ? getUpdatedDownloadCenterConfig(
+        existingDownloadCenterConfig,
+        getVersionConfig
+      )
+    : createDownloadCenterConfig(getVersionConfig);
 
   console.warn('Created download center config:');
   console.dir(config, { depth: Infinity });
@@ -33,41 +49,62 @@ export async function createAndPublishDownloadCenterConfig(
     return;
   }
 
-  const dlcenter = new DownloadCenter({
-    bucket: CONFIGURATIONS_BUCKET,
-    accessKeyId: awsAccessKeyId,
-    secretAccessKey: awsSecretAccessKey,
-  });
-
   await dlcenter.uploadConfig(CONFIGURATION_KEY, config);
 }
 
-export function createDownloadCenterConfig(
-  packageInformation: PackageInformationProvider
+export function getUpdatedDownloadCenterConfig(
+  downloadedConfig: DownloadCenterConfig,
+  getVersionConfig: () => ReturnType<typeof createVersionConfig>
 ): DownloadCenterConfig {
-  const { version } = packageInformation('linux-x64').metadata;
+  const versionConfig = getVersionConfig();
+  const toBeReleasedVersion = versionConfig.version;
+  const currentVersions = [...downloadedConfig.versions];
+  const matchingMajorVersionIdx = currentVersions.findIndex(({ version }) => {
+    return majorVersion(toBeReleasedVersion) === majorVersion(version);
+  });
+
+  if (matchingMajorVersionIdx === -1) {
+    currentVersions.push(versionConfig);
+  } else {
+    currentVersions[matchingMajorVersionIdx] = versionConfig;
+  }
+
   return {
-    versions: [
-      {
-        _id: version,
-        version: version,
-        platform: ALL_PACKAGE_VARIANTS.map(
-          (packageVariant: PackageVariant) => ({
-            arch: getArch(packageVariant),
-            os: getDistro(packageVariant),
-            name: getDownloadCenterDistroDescription(packageVariant),
-            download_link:
-              'https://downloads.mongodb.com/compass/' +
-              getPackageFile(packageVariant, packageInformation).path,
-          })
-        ),
-      },
-    ],
+    ...downloadedConfig,
+    versions: currentVersions,
+    release_notes_link: `https://github.com/mongodb-js/mongosh/releases/tag/v${versionConfig.version}`,
+  };
+}
+
+export function createDownloadCenterConfig(
+  getVersionConfig: () => ReturnType<typeof createVersionConfig>
+): DownloadCenterConfig {
+  const versionConfig = getVersionConfig();
+  return {
+    versions: [versionConfig],
     manual_link: 'https://docs.mongodb.org/manual/products/mongosh',
-    release_notes_link: `https://github.com/mongodb-js/mongosh/releases/tag/v${version}`,
+    release_notes_link: `https://github.com/mongodb-js/mongosh/releases/tag/v${versionConfig.version}`,
     previous_releases_link: '',
     development_releases_link: '',
     supported_browsers_link: '',
     tutorial_link: 'test',
+  };
+}
+
+export function createVersionConfig(
+  packageInformation: PackageInformationProvider
+) {
+  const { version } = packageInformation('linux-x64').metadata;
+  return {
+    _id: version,
+    version: version,
+    platform: ALL_PACKAGE_VARIANTS.map((packageVariant: PackageVariant) => ({
+      arch: getArch(packageVariant),
+      os: getDistro(packageVariant),
+      name: getDownloadCenterDistroDescription(packageVariant),
+      download_link:
+        'https://downloads.mongodb.com/compass/' +
+        getPackageFile(packageVariant, packageInformation).path,
+    })),
   };
 }
