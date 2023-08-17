496b8bfbd4458bc787be022759c6f8463a49016b
Kevin Mas Ruiz <kevin.mas@hey.com>
2023-08-17 17:30:28 +0200

    chore: update evergreen.yml

--- .evergreen/compilation-context-expansions.sh
+++ .evergreen/compilation-context-expansions.sh
@@ -1,13 +1,15 @@
 export DISTRO_UPLOADLIST_REGEX="^(rhel70|win|rhel81-power8|amazon2-arm64|macos)"
 
-mkdir -p /tmp/extra
+mkdir -p tmp/
+export COMPILING_CONTEXT_FILE=tmp/compiling-context.yml
+
 if ! [[ "$DISTRO_ID" =~ $DISTRO_UPLOADLIST_REGEX ]]; then
   echo "'$DISTRO_ID' will be disabled as it is not part of the DISTRO_UPLOADLIST_REGEX '$DISTRO_UPLOADLIST_REGEX'."
-  echo "If you need the package DISTRO to be packaged and uploaded, change the DISTRO_UPLOADLIST_REGEX variable in .evergreen/compile-artifact.sh"
-  echo 'extra_upload_tag: "-disabled"' > /tmp/extra/compiling-context.yml
+  echo "If you need the enable '$DISTRO_ID', include it into the DISTRO_UPLOADLIST_REGEX variable in .evergreen/compile-artifact.sh"
+  echo 'extra_upload_tag: "-disabled"' > $COMPILING_CONTEXT_FILE
   export IS_ENABLED=false
 else
-  echo "'$DISTRO_ID' is going to be package and upload as it matches the DISTRO_UPLOADLIST_REGEX."
-  echo 'extra_upload_tag: ""' > /tmp/extra/compiling-context.yml
+  echo "'$DISTRO_ID' is going to be enabled it matches the DISTRO_UPLOADLIST_REGEX."
+  echo 'extra_upload_tag: ""' > $COMPILING_CONTEXT_FILE
   export IS_ENABLED=true
 fi
