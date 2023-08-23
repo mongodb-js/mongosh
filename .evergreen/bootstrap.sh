9d6dc2ce3ca2891765fd1a35e5eece96724afdee
Le Roux Bodenstein <lerouxb@gmail.com>
2023-08-23 16:18:30 +0100

    try --ignore-scripts fallback

--- .evergreen/bootstrap.sh
+++ .evergreen/bootstrap.sh
@@ -3,7 +3,7 @@ set -x
 
 npm ci --verbose
 npm run mark-ci-required-optional-dependencies
-npm install
+(npm install && test -e node_modules/mongodb-client-encryption) || npm install --ignore-scripts
 npm run evergreen-release bump
 
 echo "npm packages after bootstrap"
