28eed2fe30fab3da8fea62c21418833ddc818608
Le Roux Bodenstein <lerouxb@gmail.com>
2023-08-23 18:28:25 +0100

    install should be ci?

--- .evergreen/install-npm-deps.sh
+++ .evergreen/install-npm-deps.sh
@@ -10,7 +10,7 @@ npm run mark-ci-required-optional-dependencies
 # mongodb-client-encryption failed to install (it can't install on some
 # platforms), then install again ignoring scripts so that the package installs
 # along with its types, but npm wouldn't try and compile the addon
-(npm install && test -e node_modules/mongodb-client-encryption) || npm install --ignore-scripts
+(npm ci && test -e node_modules/mongodb-client-encryption) || npm ci --ignore-scripts
 
 npm run evergreen-release bump
 
