1b3ed94c8d746132ea67c1c7e4ea36c139030803
Le Roux Bodenstein <lerouxb@gmail.com>
2023-08-23 13:01:09 +0100

    keep compile on the outside for now

--- .evergreen/bootstrap.sh
+++ .evergreen/bootstrap.sh
@@ -3,17 +3,7 @@ set -x
 
 npm ci --verbose
 
-if [ x"$NPM_DEPS_MODE" = x"cli_build" ]; then
-  npm run compile-cli
-elif [ x"$NPM_DEPS_MODE" = x"all" ]; then
-  npm run compile
-else
-  echo "invalid value of NPM_DEPS_MODE: '$NPM_DEPS_MODE'"
-  exit 1
-fi
+npm run evergreen-release bump
 
 echo "npm packages after bootstrap"
 npm ls || true
-
-
-npm run evergreen-release bump
