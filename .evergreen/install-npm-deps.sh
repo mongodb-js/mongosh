a8ea7e975b7bc48a9826dcb7a02cce6a5e566c3d
Le Roux Bodenstein <lerouxb@gmail.com>
2023-08-28 10:31:08 +0100

    chore: auto compile locally, remove lerna bootstrap from evergreen setup MONGOSH-1517 (#1608)
    * simplify and compassify bootstrap
    
    * try the simplest thing that could possibly work
    
    * compile just the cli when just installing the cli
    
    * rename install-npm-deps.sh to bootstrap.sh
    
    * just do npm ci
    
    * keep compile on the outside for now
    
    * we seem to need mark-ci-required-optional-dependencies
    
    * npm ci after bootstrap in that one path again..
    
    * try --ignore-scripts fallback
    
    * remove npm_deps_mode
    
    * don't npm ci agian
    
    * rename back
    
    * install should be ci?
    
    * tweak comment
    
    * use lerna to automatically sort workspaces before install outside of CI

--- .evergreen/install-npm-deps.sh
+++ .evergreen/install-npm-deps.sh
@@ -1,37 +1,18 @@
 set -e
 set -x
 
-# Depending on $NPM_DEPS_MODE, we select for which packages to install npm
-# dependencies, since we cannot install dependencies of all packages
-# on all hosts.
-
-# Always install top-level dependencies.
 npm ci --verbose
 
-# In CI, dependencies that are otherwise optional can be required.
-# Mark them as necessary here, so that we get decent error messages
-# if installing one of them fails.
+# if we rewrote this script in javascript using just builtin node modules we could skip the npm ci above
 npm run mark-ci-required-optional-dependencies
 
-if [ x"$NPM_DEPS_MODE" = x"cli_build" ]; then
-  # We always need the build package for various tasks.
-  npm run bootstrap-ci -- --scope @mongosh/build
-
-  # Bootstrap cli-repl + dependencies.
-  # mongodb-client-encryption cannot be installed everywhere without prerequisites
-  # (because it's hard to build from source when there's no prebuilts available
-  # because it requires libmongocrypt to be installed globally then), but we still
-  # need its types; so if we do not need all packages to be installed,
-  # we first try to install mongodb-client-encryption, and if that fails, we fall back
-  # to installing with --ignore-scripts (i.e. do not attempt to build addons).
-  (npm run bootstrap-ci -- --scope @mongosh/cli-repl --include-dependencies && test -e packages/service-provider-server/node_modules/mongodb-client-encryption) || \
-    npm run bootstrap-ci -- --scope @mongosh/cli-repl --include-dependencies --ignore-scripts
-elif [ x"$NPM_DEPS_MODE" = x"all" ]; then
-  # Bootstrap app packages, all dependencies.
-  npm run bootstrap-ci
-else
-  echo "invalid value of NPM_DEPS_MODE: '$NPM_DEPS_MODE'"
-  exit 1
-fi
+# install again, this time with all the optional deps. If
+# mongodb-client-encryption failed to install (it can't install on some
+# platforms), then install again ignoring scripts so that the package installs
+# along with its types, but npm wouldn't try and compile the addon
+(npm ci && test -e node_modules/mongodb-client-encryption) || npm ci --ignore-scripts
 
 npm run evergreen-release bump
+
+echo "npm packages after installation"
+npm ls || true
---
+++ scripts/sort-workspaces.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const fs = require('fs').promises;
+const path = require('path');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+process.on('unhandledRejection', (err) => {
+  console.error();
+  console.error(err?.stack || err?.message || err);
+  process.exitCode = 1;
+});
+
+async function main() {
+  if (process.env.CI) return;
+
+  const monorepoRoot = path.resolve(__dirname, '..');
+
+  const packageJSON = JSON.parse(await fs.readFile(
+    path.join(monorepoRoot, 'package.json'),
+    'utf8'
+  ));
+
+  // should use the scopes in lerna.json
+  const { stdout } = await exec('npx -y lerna ls --all --no-since --toposort --json');
+  packageJSON.workspaces = JSON.parse(stdout).map(({ location }) => path.relative(monorepoRoot, location));
+
+  await fs.writeFile(
+    path.join(monorepoRoot, 'package.json'),
+    JSON.stringify(packageJSON, null, 2) + '\n',
+    'utf8'
+  );
+}
+
+main().catch((err) =>
+  process.nextTick(() => {
+    throw err;
+  })
+);
