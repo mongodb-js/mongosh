aee60aa5c532e0f3f3ba154aec76639879c83243
Anna Henningsen <anna.henningsen@mongodb.com>
2023-08-28 14:22:30 +0200

    REVERT ME: use  --legacy-peer-deps

--- .evergreen/install-npm-deps.sh
+++ .evergreen/install-npm-deps.sh
@@ -1,7 +1,7 @@
 set -e
 set -x
 
-npm ci --verbose
+npm ci --verbose --legacy-peer-deps
 
 # if we rewrote this script in javascript using just builtin node modules we could skip the npm ci above
 npm run mark-ci-required-optional-dependencies
@@ -10,7 +10,7 @@ npm run mark-ci-required-optional-dependencies
 # mongodb-client-encryption failed to install (it can't install on some
 # platforms), then install again ignoring scripts so that the package installs
 # along with its types, but npm wouldn't try and compile the addon
-(npm ci && test -e node_modules/mongodb-client-encryption) || npm ci --ignore-scripts
+(npm ci --legacy-peer-deps && test -e node_modules/mongodb-client-encryption) || npm ci --ignore-scripts --legacy-peer-deps
 
 npm run evergreen-release bump
 
