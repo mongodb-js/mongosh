162f7ccc48b1e020f3f62ce7f49d52aef2865607
Anna Henningsen <anna.henningsen@mongodb.com>
2023-08-29 14:29:55 +0200

    fixup: make test changes in the right PR...

--- packages/cli-repl/src/format-output.spec.ts
+++ packages/cli-repl/src/format-output.spec.ts
@@ -189,7 +189,7 @@ for (const colors of [false, true]) {
 
       it('provides violation info if present', function () {
         const err = Object.assign(new Error('Something went wrong.'), {
-          violations: [{ ids: [1] }],
+          violations: [{ ids: [1, { deeply: { nested: [[['something']]] } }] }],
         });
         const output = stripAnsiColors(
           format({
@@ -198,8 +198,8 @@ for (const colors of [false, true]) {
           })
         );
 
-        expect(output).to.equal(
-          '\rError: Something went wrong.\nViolations: [ { ids: [ 1 ] } ]'
+        expect(output.replace(/\s/g, '')).to.equal(
+          "Error:Somethingwentwrong.Violations:[{ids:[1,{deeply:{nested:[[['something']]]}}]}]"
         );
       });
     });
