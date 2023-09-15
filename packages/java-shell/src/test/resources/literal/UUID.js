044fd6be1e90c938bfd503aca467dbc579fa48a6
Liudmila Kornilova <kornilova203@gmail.com>
2023-09-15 15:11:55 +0200

    Test UUID value

--- packages/java-shell/src/test/resources/literal/UUID.js
+++ packages/java-shell/src/test/resources/literal/UUID.js
@@ -1,12 +1,12 @@
 // before
 db.coll.insertOne({"_id": 1, v: UUID('5220b418-8f7d-4cd9-bd27-35b6f8d990c5')});
-// command checkResultClass
+// command checkResultClass dontReplaceId
 new UUID('5220b418-8f7d-4cd9-bd27-35b6f8d990c5')
-// command checkResultClass
+// command checkResultClass dontReplaceId
 UUID('5220b418-8f7d-4cd9-bd27-35b6f8d990c5')
 // command checkResultClass
 new UUID()
-// command checkResultClass
+// command checkResultClass dontReplaceId
 db.coll.find().toArray()[0].v;
 // clear
 db.coll.drop();
