3ccb9008914cd0f3dbfdbba500b34d84600204d8
Le Roux Bodenstein <lerouxb@gmail.com>
2024-01-08 14:16:17 +0000

    update script comments and logging to reflect what it does

--- scripts/replace-package.js
+++ scripts/replace-package.js
@@ -1,12 +1,10 @@
-// Replace a package with symlinks to a specific directory.
 // Example:
-// REPLACE_PACKAGE=mongodb:/home/src/node-mongodb-native node scripts/replace-package.js
-// will make 'mongodb' point to '/home/src/node-mongodb-native' in the root
-// directory and all lerna packages.
+// REPLACE_PACKAGE=mongodb:latest node scripts/replace-package.js
+// will replace the 'mongodb' dep's version with latest in the root directory
+// and all packages.
 'use strict';
 const fs = require('fs');
 const path = require('path');
-const { pathToFileURL } = require('url');
 
 const replacement = process.env.REPLACE_PACKAGE;
 if (!replacement) {
@@ -21,7 +19,6 @@ const { from, to } = parsed.groups;
 for (const dir of ['.', ...fs.readdirSync('packages').map(dir => path.join('packages', dir))]) {
   const packageJson = path.join(dir, 'package.json');
   if (fs.existsSync(packageJson)) {
-    const target = pathToFileURL(path.resolve(to)).href;
     const contents = JSON.parse(fs.readFileSync(packageJson));
     for (const deps of [
       contents.dependencies,
@@ -29,7 +26,7 @@ for (const dir of ['.', ...fs.readdirSync('packages').map(dir => path.join('pack
       contents.optionalDependencies
     ]) {
       if (deps && deps[from]) {
-        console.info('Replacing', from, 'in', dir, 'with', target);
+        console.info(`Replacing deps[${from}]: ${deps[from]} with ${to}`);
         deps[from] = to;
       }
     }
