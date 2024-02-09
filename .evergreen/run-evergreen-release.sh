d43b3d5e211b46226df188a8d6f894175f1b35ef
Himanshu Singh <himanshu.singhs@outlook.in>
2024-02-09 14:59:05 +0100

    Update .evergreen/run-evergreen-release.sh
    Co-authored-by: Anna Henningsen <anna.henningsen@mongodb.com>

--- .evergreen/run-evergreen-release.sh
+++ .evergreen/run-evergreen-release.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "//registry.npmjs.org/:_authToken=${devtoolsbot_npm_token}" > .npmrc
+set -x
 export NODE_JS_VERSION=${node_js_version}
 source .evergreen/setup-env.sh
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
