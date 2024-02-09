1e1110a1a592b55c9f7f2d60eaed87f3b304cae6
Himanshu Singh <himanshu.singhs@outlook.in>
2024-02-09 14:20:39 +0100

    fix: modify shell.exec as per evergreen instructions to avoid leaking any secrets

---
+++ .evergreen/run-evergreen-release.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+set -e
+
+echo "//registry.npmjs.org/:_authToken=${devtoolsbot_npm_token}" > .npmrc
+export NODE_JS_VERSION=${node_js_version}
+source .evergreen/setup-env.sh
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
+npm run evergreen-release publish $@
