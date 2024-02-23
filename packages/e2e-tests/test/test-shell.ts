45d712ab541de8983f4a31a9f02fce4c4e7ba254
Anna Henningsen <anna.henningsen@mongodb.com>
2024-02-23 15:57:52 +0100

    chore(e2e-tests): account for upstream server TLS option changes MONGOSH-1718
    The server will no longer start when TLS is enabled without a CA
    certificate argument. This does not affect our tests’ functionality,
    only how we start the server in them.

--- packages/e2e-tests/test/test-shell.ts
+++ packages/e2e-tests/test/test-shell.ts
@@ -1,6 +1,9 @@
 import type Mocha from 'mocha';
 import assert from 'assert';
-import type { ChildProcess } from 'child_process';
+import type {
+  ChildProcess,
+  ChildProcessWithoutNullStreams,
+} from 'child_process';
 import { spawn } from 'child_process';
 import { once } from 'events';
 import path from 'path';
@@ -20,6 +23,12 @@ const PROMPT_PATTERN = /^([^<>]*> ?)+$/m;
 const ERROR_PATTERN_1 = /Thrown:\n([^>]*)/gm; // node <= 12.14
 const ERROR_PATTERN_2 = /Uncaught[:\n ]+([^>]*)/gm;
 
+function matches(str: string, pattern: string | RegExp): boolean {
+  return typeof pattern === 'string'
+    ? str.includes(pattern)
+    : pattern.test(str);
+}
+
 /**
  * Test shell helper class.
  */
@@ -36,7 +45,7 @@ export class TestShell {
       consumeStdio?: boolean;
     } = { args: [] }
   ): TestShell {
-    let shellProcess: ChildProcess;
+    let shellProcess: ChildProcessWithoutNullStreams;
 
     let env = options.env || process.env;
     if (options.forceTerminal) {
@@ -88,7 +97,7 @@ export class TestShell {
   static async killall(): Promise<void> {
     const exitPromises: Promise<unknown>[] = [];
     while (TestShell._openShells.length) {
-      const shell = TestShell._openShells.pop();
+      const shell = TestShell._openShells.pop()!;
       shell.kill();
       exitPromises.push(shell.waitForExit());
     }
@@ -110,13 +119,16 @@ export class TestShell {
     await TestShell.killall();
   }
 
-  private _process: ChildProcess;
+  private _process: ChildProcessWithoutNullStreams;
 
   private _output: string;
   private _rawOutput: string;
   private _onClose: Promise<number>;
 
-  constructor(shellProcess: ChildProcess, consumeStdio = true) {
+  constructor(
+    shellProcess: ChildProcessWithoutNullStreams,
+    consumeStdio = true
+  ) {
     this._process = shellProcess;
     this._output = '';
     this._rawOutput = '';
@@ -229,9 +241,9 @@ export class TestShell {
     }
   }
 
-  assertContainsOutput(expectedOutput: string): void {
+  assertContainsOutput(expectedOutput: string | RegExp): void {
     const onlyOutputLines = this._getOutputLines();
-    if (!onlyOutputLines.join('\n').includes(expectedOutput)) {
+    if (!matches(onlyOutputLines.join('\n'), expectedOutput)) {
       throw new assert.AssertionError({
         message: `Expected shell output to include ${JSON.stringify(
           expectedOutput
@@ -242,10 +254,10 @@ export class TestShell {
     }
   }
 
-  assertContainsError(expectedError: string): void {
+  assertContainsError(expectedError: string | RegExp): void {
     const allErrors = this._getAllErrors();
 
-    if (!allErrors.find((error) => error.includes(expectedError))) {
+    if (!allErrors.find((error) => matches(error, expectedError))) {
       throw new assert.AssertionError({
         message: `Expected shell errors to include ${JSON.stringify(
           expectedError
@@ -256,9 +268,9 @@ export class TestShell {
     }
   }
 
-  assertNotContainsOutput(unexpectedOutput: string): void {
+  assertNotContainsOutput(unexpectedOutput: string | RegExp): void {
     const onlyOutputLines = this._getOutputLines();
-    if (onlyOutputLines.join('\n').includes(unexpectedOutput)) {
+    if (matches(onlyOutputLines.join('\n'), unexpectedOutput)) {
       throw new assert.AssertionError({
         message: `Expected shell output not  to include ${JSON.stringify(
           unexpectedOutput
@@ -288,6 +300,6 @@ export class TestShell {
     if (!match) {
       return null;
     }
-    return match.groups.logId;
+    return match.groups!.logId;
   }
 }
