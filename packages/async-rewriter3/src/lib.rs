ac0a79d396e307b38f63396265cc58e45bc1f30a
Anna Henningsen <anna.henningsen@mongodb.com>
2024-04-03 18:10:34 +0200

    fixup: IIFE wrapping WIP

--- packages/async-rewriter3/src/lib.rs
+++ packages/async-rewriter3/src/lib.rs
@@ -1,21 +1,66 @@
-use std::collections::VecDeque;
+use std::{borrow::Borrow, collections::VecDeque};
 use wasm_bindgen::prelude::*;
-use rslint_parser::{ast::{ArrowExpr, CallExpr, Expr, ExprOrBlock, FnDecl, FnExpr, ReturnStmt}, parse_text, AstNode, SyntaxNode, TextSize};
+use rslint_parser::{ast::{ArrowExpr, CallExpr, ClassDecl, Expr, ExprOrBlock, FnDecl, FnExpr, ObjectPatternProp, Pattern, PropName, ReturnStmt, VarDecl}, parse_text, AstNode, SyntaxNode, TextSize};
+
+#[derive(Debug)]
+enum InsertionText {
+    Static(&'static str),
+    Dynamic(String)
+}
 
 #[derive(Debug)]
 struct Insertion {
     offset: TextSize,
-    text: &'static str,
+    text: InsertionText,
     original_ordering: Option<u32>
 }
 
 impl Insertion {
     pub const fn new(offset: TextSize, text: &'static str) -> Insertion {
-        return Insertion {
+        Insertion {
             offset,
-            text,
+            text: InsertionText::Static(text),
             original_ordering: None
         }
+    }
+
+    pub const fn new_dynamic(offset: TextSize, text: String) -> Insertion {
+        Insertion {
+            offset,
+            text: InsertionText::Dynamic(text),
+            original_ordering: None
+        }
+    }
+}
+
+struct InsertionList {
+    list: VecDeque<Insertion>,
+    vars: Vec<String>
+}
+
+impl InsertionList {
+    pub const fn new() -> InsertionList {
+        InsertionList {
+            list: VecDeque::new(),
+            vars: Vec::new()
+        }
+    }
+
+    fn append(&mut self, other: &mut Self) {
+        self.list.append(&mut other.list);
+        self.vars.append(&mut other.vars);
+    }
+
+    fn push_back(&mut self, insertion: Insertion) {
+        self.list.push_back(insertion);
+    }
+
+    fn pop_back(&mut self) {
+        self.list.pop_back();
+    }
+
+    fn add_variable(&mut self, variable: String) {
+        self.vars.push(variable);
     }
 }
 
@@ -26,50 +71,30 @@ fn is_block(body: &ExprOrBlock) -> bool {
     }
 }
 
-fn fn_start_insertion(body: &ExprOrBlock) -> VecDeque<Insertion> {
-    let mut ret = VecDeque::new();
-    let mut offset = body.syntax().text_range().start();
-    if !is_block(body) {
-        ret.push_back(Insertion::new(offset, "{"));
-    } else {
-        offset = offset.checked_add(1.into()).unwrap();
+fn make_start_fn_insertion(offset: TextSize) -> Insertion {
+    Insertion::new(offset, r#"
+    const _syntheticPromise = Symbol.for('@@mongosh.syntheticPromise');
+
+    function _markSyntheticPromise(p) {
+        return Object.defineProperty(p, _syntheticPromise, {
+            value: true,
+        });
     }
-    ret.push_back(Insertion::new(offset, r#"
-        const _syntheticPromise = Symbol.for('@@mongosh.syntheticPromise');
 
-        function _markSyntheticPromise(p) {
-            return Object.defineProperty(p, _syntheticPromise, {
-                value: true,
-            });
-        }
-
-        function _isp(p) {
-            return p && p[_syntheticPromise];
-        }
-
-        let _functionState = 'sync', _synchronousReturnValue, _ex;
-
-        const _asynchronousReturnValue = (async () => {
-        try {
-        "#
-    ));
-    if !is_block(body) {
-        ret.push_back(Insertion::new(
-            offset,
-             "return ("
-        ));
+    function _isp(p) {
+        return p && p[_syntheticPromise];
     }
-    ret
+
+    let _functionState = 'sync', _synchronousReturnValue, _ex;
+
+    const _asynchronousReturnValue = (async () => {
+    try {
+    "#
+)
 }
-fn fn_end_insertion(body: &ExprOrBlock) -> VecDeque<Insertion> {
-    let mut ret = VecDeque::new();
-    let mut offset = body.syntax().text_range().end();
-    if is_block(body) {
-        offset = offset.checked_sub(1.into()).unwrap();
-    } else {
-        ret.push_back(Insertion::new(offset, ");"));
-    }
-    ret.push_back(Insertion::new(
+
+fn make_end_fn_insertion(offset: TextSize) -> Insertion {
+    Insertion::new(
         offset,
         r#"
         } catch (err) {
@@ -97,30 +122,158 @@ fn fn_end_insertion(body: &ExprOrBlock) -> VecDeque<Insertion> {
         _functionState = 'async';
         return _markSyntheticPromise(_asynchronousReturnValue);
         "#
-    ));
+    )
+}
+
+fn fn_start_insertion(body: &ExprOrBlock) -> InsertionList {
+    let mut ret = InsertionList::new();
+    let mut offset = body.syntax().text_range().start();
+    if !is_block(body) {
+        ret.push_back(Insertion::new(offset, "{"));
+    } else {
+        offset = offset.checked_add(1.into()).unwrap();
+    }
+    ret.push_back(make_start_fn_insertion(offset));
+    if !is_block(body) {
+        ret.push_back(Insertion::new(
+            offset,
+             "return ("
+        ));
+    }
+    ret
+}
+fn fn_end_insertion(body: &ExprOrBlock) -> InsertionList {
+    let mut ret = InsertionList::new();
+    let mut offset = body.syntax().text_range().end();
+    if is_block(body) {
+        offset = offset.checked_sub(1.into()).unwrap();
+    } else {
+        ret.push_back(Insertion::new(offset, ");"));
+    }
+    ret.push_back(make_end_fn_insertion(offset));
     if !is_block(body) {
         ret.push_back(Insertion::new(offset, "}"));
     }
     ret
 }
 
-fn collect_insertions(node: &SyntaxNode, nesting_depth: u32) -> VecDeque<Insertion> {
+fn add_all_variables_from_declaration(patterns: impl Iterator<Item = impl Borrow<Pattern>>) -> InsertionList {
+    let mut ret = InsertionList::new();
+    for pattern in patterns {
+        match pattern.borrow() {
+            Pattern::SinglePattern(p) => {
+                p.name().map(|name| ret.add_variable(name.to_string()));
+            },
+            Pattern::RestPattern(p) => {
+                let pat = p.pat();
+                if pat.is_some() {
+                    ret.append(&mut add_all_variables_from_declaration([&pat.unwrap()].into_iter()));
+                }
+            },
+            Pattern::AssignPattern(p) => {
+                let key = p.key();
+                if key.is_some() {
+                    ret.append(&mut add_all_variables_from_declaration([&key.unwrap()].into_iter()));
+                }
+            },
+            Pattern::ObjectPattern(p) => {
+                for element in p.elements() {
+                    match element {
+                        ObjectPatternProp::AssignPattern(p) => {
+                            ret.append(&mut add_all_variables_from_declaration([&Pattern::AssignPattern(p)].into_iter()));
+                        }
+                        ObjectPatternProp::KeyValuePattern(p) => {
+                            if p.key().is_some() {
+                                match p.key().unwrap() {
+                                    PropName::Ident(ident) => {
+                                        ret.add_variable(ident.text());
+                                    }
+                                    PropName::Computed(_) => panic!(),
+                                    PropName::Literal(_) => panic!(),
+                                }
+                            }
+                        },
+                        ObjectPatternProp::RestPattern(p) => {
+                            ret.append(&mut add_all_variables_from_declaration([&Pattern::RestPattern(p)].into_iter()));
+                        },
+                        ObjectPatternProp::SinglePattern(p) => {
+                            ret.append(&mut add_all_variables_from_declaration([&Pattern::SinglePattern(p)].into_iter()));
+                        },
+                    }
+                }
+            },
+            Pattern::ArrayPattern(p) => {
+                ret.append(&mut add_all_variables_from_declaration(p.elements()));
+            },
+            Pattern::ExprPattern(_) => panic!(),
+        }
+    }
+    ret
+}
+
+fn collect_insertions(node: &SyntaxNode, nesting_depth: u32) -> InsertionList {
     let is_function_node = FnExpr::can_cast(node.kind()) || FnDecl::can_cast(node.kind()) || ArrowExpr::can_cast(node.kind());
     let has_function_parent = nesting_depth > 0;
-    let mut insertions = VecDeque::new();
+    let mut insertions = InsertionList::new();
     for child in node.children() {
         let range = child.text_range();
         let child_insertions = &mut collect_insertions(&child, nesting_depth + if is_function_node { 1 } else { 0 });
         if FnDecl::can_cast(child.kind()) {
             let as_fn = FnDecl::cast(child).unwrap();
+            let body = ExprOrBlock::Block(as_fn.body().unwrap());
+            if !has_function_parent {
+                match as_fn.ident_token() {
+                    None => {},
+                    Some(name) => {
+                        insertions.push_back(Insertion::new_dynamic(range.start(),
+                            [name.text(), " = "].concat()
+                        ));
+                        insertions.add_variable(name.to_string());
+                    }
+                }
+            }
             if as_fn.async_token().is_none() {
-                let body = ExprOrBlock::Block(as_fn.body().unwrap());
                 insertions.append(&mut fn_start_insertion(&body));
                 insertions.append(child_insertions);
                 insertions.append(&mut fn_end_insertion(&body));
+            } else {
+                insertions.append(child_insertions);
             }
             continue;
         }
+        if ClassDecl::can_cast(child.kind()) && !has_function_parent {
+            let as_class_decl = ClassDecl::cast(child).unwrap();
+            match as_class_decl.name() {
+                None => {},
+                Some(name) => {
+                    insertions.push_back(Insertion::new_dynamic(range.start(),
+                        [name.text().as_str(), " = "].concat()
+                    ));
+                    insertions.add_variable(name.to_string());
+                }
+            }
+            insertions.append(child_insertions);
+            continue;
+        }
+        if VarDecl::can_cast(child.kind()) && !has_function_parent {
+            let as_var_decl = VarDecl::cast(child).unwrap();
+            let declarator_range =
+                as_var_decl.const_token().map(|t| t.text_range())
+                .or(as_var_decl.let_token().map(|t| t.text_range()))
+                .or(as_var_decl.var_token().map(|t| t.text_range()));
+
+            if declarator_range.is_some() {
+                insertions.push_back(Insertion::new(declarator_range.unwrap().start(), "/*"));
+                insertions.push_back(Insertion::new(declarator_range.unwrap().end(), "*/("));
+                insertions.append(&mut add_all_variables_from_declaration(as_var_decl.declared().filter_map(|d| d.pattern())));
+            }
+            insertions.append(child_insertions);
+            if declarator_range.is_some() {
+                insertions.push_back(Insertion::new(as_var_decl.declared().map(|d| d.range().end()).max().unwrap(), ")"));
+            }
+            continue;
+        }
+
         match Expr::cast(child) {
             None => {
                 insertions.append(child_insertions);
@@ -129,12 +282,10 @@ fn collect_insertions(node: &SyntaxNode, nesting_depth: u32) -> VecDeque<Inserti
                 let is_returned_expression = ReturnStmt::can_cast(as_expr.syntax().parent().unwrap().kind());
                 let is_called_expression = CallExpr::can_cast(as_expr.syntax().parent().unwrap().kind());
                 let mut is_dot_call_expression = false;
-                if has_function_parent {
-                    if is_returned_expression {
-                        insertions.push_back(Insertion::new(range.start(), "(_synchronousReturnValue = "));
-                    }
-                    insertions.push_back(Insertion::new(range.start(), "(_ex = "));
+                if is_returned_expression {
+                    insertions.push_back(Insertion::new(range.start(), "(_synchronousReturnValue = "));
                 }
+                insertions.push_back(Insertion::new(range.start(), "(_ex = "));
 
                 match as_expr {
                     Expr::ArrowExpr(as_fn) => {
@@ -164,16 +315,14 @@ fn collect_insertions(node: &SyntaxNode, nesting_depth: u32) -> VecDeque<Inserti
                         insertions.append(child_insertions);
                     },
                 }
-                if has_function_parent {
-                    if !is_dot_call_expression {
-                        insertions.push_back(Insertion::new(range.end(), ", _isp(_ex) ? await _ex : _ex)"));
-                    }
-                    if is_returned_expression {
-                        insertions.push_back(Insertion::new(
-                            range.end(),
-                            ", _functionState === 'async' ? _synchronousReturnValue : null)"
-                        ));
-                    }
+                if !is_dot_call_expression {
+                    insertions.push_back(Insertion::new(range.end(), ", _isp(_ex) ? await _ex : _ex)"));
+                }
+                if is_returned_expression {
+                    insertions.push_back(Insertion::new(
+                        range.end(),
+                        ", _functionState === 'async' ? _synchronousReturnValue : null)"
+                    ));
                 }
             }
         }
@@ -184,26 +333,47 @@ fn collect_insertions(node: &SyntaxNode, nesting_depth: u32) -> VecDeque<Inserti
 #[wasm_bindgen]
 pub fn async_rewrite(input: String, with_debug_tags: bool) -> String {
     let parsed = parse_text(input.as_str(), 0);
-    let mut insertions = collect_insertions(&parsed.syntax(), 0);
+    let mut insertions = InsertionList::new();
+    let mut collected_insertions = collect_insertions(&parsed.syntax(), 0);
+    {
+        let vars = &collected_insertions.vars;
+        for var in vars {
+            insertions.push_back(Insertion::new_dynamic(TextSize::new(0), [
+                "var ", var.as_str(), ";"
+            ].concat()));
+        }
+    }
+    let end = input.len().try_into().unwrap();
+    insertions.push_back(Insertion::new(TextSize::new(0), "(async () => {"));
+    insertions.push_back(make_start_fn_insertion(TextSize::new(0)));
+    insertions.append(&mut collected_insertions);
+    insertions.push_back(make_end_fn_insertion(input.len().try_into().unwrap()));
+    insertions.push_back(Insertion::new(TextSize::new(end), "})()"));
+
     let mut i = 0;
-    for insertion in &mut insertions {
+    for insertion in &mut insertions.list {
         i += 1;
         insertion.original_ordering = Some(i);
     }
-    insertions.make_contiguous().sort_by(|a, b| a.offset.cmp(&b.offset));
+    insertions.list.make_contiguous().sort_by(|a, b| a.offset.cmp(&b.offset));
 
     let mut result = input.to_string();
     let mut debug_tag = "".to_string();
-    for insertion in insertions.iter().rev() {
+    for insertion in insertions.list.iter().rev() {
+        let text;
+        match &insertion.text {
+            InsertionText::Dynamic(str) => { text = str.as_str(); }
+            InsertionText::Static(str) => { text = str; }
+        }
         let (before, after) = result.split_at(insertion.offset.into());
         if with_debug_tags {
             debug_tag = [
                 "/*i", insertion.original_ordering.unwrap().to_string().as_str(), "@",
                 u32::from(insertion.offset).to_string().as_str(),
-                if insertion.text.contains("/*") { "" } else { "*/" }
+                if text.contains("/*") { "" } else { "*/" }
             ].concat();
         }
-        result = [before, debug_tag.as_str(), insertion.text, debug_tag.as_str(), after].concat();
+        result = [before, debug_tag.as_str(), text, debug_tag.as_str(), after].concat();
     }
 
     result
