mongodb-js/mongosh

commit 16cbc95001082ca44e50117c3af3e59ffd7eab89

feat(shell-api): warn users when using background option in aggregate MONGOSH-1766 (#1964)
* warn users when using background option in aggregate

* text change

packages/shell-api/src/collection.ts
@@ -41,6 +41,7 @@ import {
   coerceToJSNumber,
   buildConfigChunksCollectionMatch,
   onlyShardedCollectionsInConfigFilter,
+  aggregateBackgroundOptionNotSupportedHelp,
 } from './helpers';
 import type {
   AnyBulkWriteOperation,
@@ -176,6 +177,11 @@ export default class Collection extends ShellApiWithMongoClass {
     } else {
       options = {};
       pipeline = args || [];
+    }
+    if ('background' in options) {
+      this._instanceState.printWarning(
+        aggregateBackgroundOptionNotSupportedHelp
+      );
     }
     this._emitCollectionApiCall('aggregate', { options, pipeline });
     const { aggOptions, dbOptions, explain } = adaptAggregateOptions(options);

packages/shell-api/src/database.ts
@@ -24,6 +24,7 @@ import {
   shouldRunAggregationImmediately,
   adjustRunCommand,
   getBadge,
+  aggregateBackgroundOptionNotSupportedHelp,
 } from './helpers';
 
 import {
@@ -421,6 +422,11 @@ export default class Database extends ShellApiWithMongoClass {
     pipeline: Document[],
     options?: Document
   ): Promise<AggregationCursor> {
+    if ('background' in (options ?? {})) {
+      this._instanceState.printWarning(
+        aggregateBackgroundOptionNotSupportedHelp
+      );
+    }
     assertArgsDefinedType([pipeline], [true], 'Database.aggregate');
     this._emitDatabaseApiCall('aggregate', { options, pipeline });
 

packages/shell-api/src/helpers.ts
@@ -1187,3 +1187,7 @@ export function buildConfigChunksCollectionMatch(
     ? { uuid: configCollectionsInfo.uuid } // new format
     : { ns: configCollectionsInfo._id }; // old format
 }
+
+export const aggregateBackgroundOptionNotSupportedHelp =
+  'the background option is not supported by the aggregate method and will be ignored, ' +
+  'use runCommand to use { background: true } with Atlas Data Federation';

commit 733d703da15ed129a8c4305d7e83bfc09c7ce12c

chore(shell-api): fix linting errors on main
Introduced in 16cbc9500108

packages/shell-api/src/collection.ts
@@ -179,7 +179,7 @@ export default class Collection extends ShellApiWithMongoClass {
       pipeline = args || [];
     }
     if ('background' in options) {
-      this._instanceState.printWarning(
+      await this._instanceState.printWarning(
         aggregateBackgroundOptionNotSupportedHelp
       );
     }

packages/shell-api/src/database.ts
@@ -423,7 +423,7 @@ export default class Database extends ShellApiWithMongoClass {
     options?: Document
   ): Promise<AggregationCursor> {
     if ('background' in (options ?? {})) {
-      this._instanceState.printWarning(
+      await this._instanceState.printWarning(
         aggregateBackgroundOptionNotSupportedHelp
       );
     }