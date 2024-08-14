4f1efdad997714d97b6282d2bb72f9b188d05c05
Anna Henningsen <anna@addaleax.net>
2024-08-14 21:23:42 +0200

    fixup: ignore snippet/update notifier endpoints for proxy e2e test

--- packages/e2e-tests/test/e2e-proxy.spec.ts
+++ packages/e2e-tests/test/e2e-proxy.spec.ts
@@ -79,11 +79,8 @@ describe('e2e proxy support', function () {
       req: IncomingMessage,
       socket: Socket,
       head: Buffer
-    ) {
+    ): void {
       (req as any).server = this;
-      connectRequests.push(req);
-      socket.unshift(head);
-      socket.write('HTTP/1.0 200 OK\r\n\r\n');
       let host: string;
       let port: string;
       if (req.url?.includes(']:')) {
@@ -91,6 +88,16 @@ describe('e2e proxy support', function () {
       } else {
         [host, port] = (req.url ?? '').split(':');
       }
+      if (host === 'compass.mongodb.com' || host === 'downloads.mongodb.com') {
+        // The snippet loader and update notifier can reach out to thes endpoints,
+        // but we usually do not actually wait for this to happen or not in CI,
+        // so we're just ignoring these requests here to avoid flaky behavior.
+        socket.end();
+        return;
+      }
+      connectRequests.push(req);
+      socket.unshift(head);
+      socket.write('HTTP/1.0 200 OK\r\n\r\n');
       const outbound = connect(+port, host);
       socket.pipe(outbound).pipe(socket);
       // socket.on('data', chk => console.log('[from client] ' + chk.toString()));
