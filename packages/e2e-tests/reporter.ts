883411cbf4d56341080c37b81fce578653b0c845
gagik <me@gagik.co>
2024-10-18 13:40:12 +0200

    remvoe env

--- packages/e2e-tests/reporter.ts
+++ packages/e2e-tests/reporter.ts
@@ -10,7 +10,7 @@ export class MochaReporter extends reporters.Base {
   constructor(runner: Runner, options: MochaOptions) {
     super(runner, options);
     const suiteName = process.env.E2E_TASK_NAME ?? path.basename(process.cwd());
-    console.info(`MOCHAREPORTER:${suiteName} ${JSON.stringify(process.env)}`);
+    console.info(`MOCHAREPORTER:${suiteName}`);
 
     new Spec(runner);
 
