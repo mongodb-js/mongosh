8c02992991c6722163dd2a6db4fc22dc10a87002
Nikola Irinchev <irinchev@me.com>
2024-10-29 14:13:11 +0100

    Regenerate package-lock.json

--- .evergreen/compilation-context-expansions.sh
+++ .evergreen/compilation-context-expansions.sh
@@ -1,5 +1,5 @@
 export DISTRO_UPLOADLIST_REGEX="^(rhel70|win|rhel7-zseries|rhel81-power8|amazon2-arm64|macos)"
-export COMPILING_CONTEXT_FILE="$(pwd)/../tmp/compiling-context.yml"
+export COMPILING_CONTEXT_FILE="$(dirname "$0")/../tmp/compiling-context.yml"
 
 if ! [[ "$DISTRO_ID" =~ $DISTRO_UPLOADLIST_REGEX ]]; then
   echo "'$DISTRO_ID' will be disabled as it is not part of the DISTRO_UPLOADLIST_REGEX '$DISTRO_UPLOADLIST_REGEX'."
