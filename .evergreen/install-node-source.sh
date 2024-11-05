ea58f30ccfa7e775733e97fec55bc26a93226ac7
Nikola Irinchev <irinchev@me.com>
2024-11-05 23:51:12 +0100

    chore: Migrate rhel81-power8 to rhel8-power9 MONGOSH-1835 (#2236)
    * chore: Use new distro ids for MacOS and IBM hosts
    
    * Try a different host name
    
    * Try 3
    
    * Use power instead of power9
    
    * Update context-expansions
    
    * Try to use toolchain v4
    
    * Actually use v4 for ppc hosts

--- .evergreen/install-node-source.sh
+++ .evergreen/install-node-source.sh
@@ -7,6 +7,16 @@ EVGDIR="$ROOT_DIR/.evergreen"
 NVM_DIR="$EVGDIR/.nvm"
 ORIGINAL_PATH="${PATH}"
 
+OS_ARCH="$(uname "-m")"
+if [ "$OS_ARCH" = "ppc64le" ] || [ "$OS_ARCH" = "ppc64" ] ; then
+    echo "[INFO] Choosing v4 because OS_ARCH is $OS_ARCH"
+    export TOOLCHAIN_PATH='/opt/mongodbtoolchain/v4/bin'
+else
+    echo "[INFO] Choosing v3 because OS_ARCH is $OS_ARCH"
+    export TOOLCHAIN_PATH='/opt/mongodbtoolchain/v3/bin'
+fi
+export PATH="$TOOLCHAIN_PATH:/opt/mongodbtoolchain/v4/bin:/opt/mongodbtoolchain/v3/bin:${ORIGINAL_PATH}"
+
 export PATH="/opt/mongodbtoolchain/v4/bin:/opt/mongodbtoolchain/v3/bin:${ORIGINAL_PATH}"
 export CC=gcc
 export CXX=g++
