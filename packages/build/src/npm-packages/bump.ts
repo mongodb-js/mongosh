ad630dc169741b0375d847c03ca06d3a51a3773c
gagik <me@gagik.co>
2024-12-30 12:48:06 +0100

    switch to bump script

--- packages/build/src/npm-packages/bump.ts
+++ packages/build/src/npm-packages/bump.ts
@@ -1,6 +1,8 @@
 import { spawnSync } from '../helpers';
 import { PROJECT_ROOT } from './constants';
 
+const PACKAGES_TO_SKIP_BUMPING = ['mongosh', '@mongosh/cli-repl'];
+
 export function bumpNpmPackages() {
   spawnSync('npm', ['run', 'bump-packages'], {
     stdio: 'inherit',
@@ -8,6 +10,7 @@ export function bumpNpmPackages() {
     encoding: 'utf8',
     env: {
       ...process.env,
+      SKIP_BUMP_PACKAGES: PACKAGES_TO_SKIP_BUMPING.join(','),
     },
   });
 }
