53a35b9a0a4d3371ff491c7df5fb902fcb9866f8
Anna Henningsen <anna@addaleax.net>
2025-01-09 20:52:45 +0100

    chore(shell-api): add test and note about runtime independence MONGOSH-1975

--- packages/service-provider-core/src/textencoder-polyfill.ts
+++ packages/service-provider-core/src/textencoder-polyfill.ts
@@ -11,7 +11,12 @@ if (
   typeof TextEncoder !== 'function'
 ) {
   // eslint-disable-next-line @typescript-eslint/no-implied-eval
-  Object.assign(Function('return this')(), textEncodingPolyfill());
+  const global =
+    (typeof globalThis === 'object' &&
+      globalThis?.Object === Object &&
+      globalThis) ||
+    Function('return this')();
+  Object.assign(global, textEncodingPolyfill());
 }
 
 // Basic encoder/decoder polyfill for java-shell environment (see above)
