11df98e91e3fdecf19c938c99b0b0f986d772157
Anna Henningsen <anna.henningsen@mongodb.com>
2025-01-29 10:57:51 +0100

    fix(i18n): fix help text for `oidcDumpTokens` (#2336)

--- packages/i18n/src/locales/en_US.ts
+++ packages/i18n/src/locales/en_US.ts
@@ -91,7 +91,7 @@ const translations: Catalog = {
       oidcIdTokenAsAccessToken:
         'Use ID tokens in place of access tokens for auth',
       oidcDumpTokens:
-        "Debug OIDC by printing tokens to mongosh's output [full|include-secrets]",
+        "Debug OIDC by printing tokens to mongosh's output [redacted|include-secrets]",
       oidcNoNonce: "Don't send a nonce argument in the OIDC auth request",
     },
     'arg-parser': {
