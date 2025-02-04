9745e3c143889f2a807c55ac67744c3dfd79ba65
Anna Henningsen <anna@addaleax.net>
2025-02-04 17:41:04 +0100

    fix(scripts): drop `--no-since` from `lerna ls`

--- scripts/sort-workspaces.js
+++ scripts/sort-workspaces.js
@@ -22,7 +22,7 @@ async function main() {
   ));
 
   // should use the scopes in lerna.json
-  const { stdout } = await exec('npx -y lerna ls --all --no-since --toposort --json');
+  const { stdout } = await exec('npx -y lerna ls --all --toposort --json');
   packageJSON.workspaces = JSON.parse(stdout).map(({ location }) => path.relative(monorepoRoot, location));
 
   await fs.writeFile(
