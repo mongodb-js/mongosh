fd3231b6d6053afbce50060b696fe0bb53f5ad16
Will Ratner <wratner7@gmail.com>
2025-02-20 09:43:55 -0500

    utilize tmp directory

--- .evergreen/download-crypt-shared-and-generate-sbom.sh
+++ .evergreen/download-crypt-shared-and-generate-sbom.sh
@@ -13,8 +13,13 @@ set +x
 echo "${ARTIFACTORY_PASSWORD}" | docker login artifactory.corp.mongodb.com --username "${ARTIFACTORY_USERNAME}" --password-stdin
 set -x
 
+trap_handler() {
+  rm -f /tmp/kondukto_credentials.env
+}
+trap trap_handler ERR EXIT
+
 docker pull artifactory.corp.mongodb.com/release-tools-container-registry-public-local/silkbomb:2.0
 docker run --rm -v ${PWD}:/pwd artifactory.corp.mongodb.com/release-tools-container-registry-public-local/silkbomb:2.0 update \
   --purls /pwd/dist/.purls.txt --sbom-out /pwd/dist/.sbom-lite.json
-docker run --env-file ${workdir}/kondukto_credentials.env --rm -v ${PWD}:/pwd artifactory.corp.mongodb.com/release-tools-container-registry-public-local/silkbomb:2.0 augment \
+docker run --env-file /tmp/kondukto_credentials.env --rm -v ${PWD}:/pwd artifactory.corp.mongodb.com/release-tools-container-registry-public-local/silkbomb:2.0 augment \
   --repo mongodb-js/mongosh --branch ${KONDUKTO_BRANCH} --sbom-in /pwd/dist/.sbom-lite.json -sbom-out /pwd/dist/.sbom.json
