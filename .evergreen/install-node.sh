deb1aaa0aafa5c5cdd5f7a2d68152d3fab7c8e3f
Anna Henningsen <anna@addaleax.net>
2025-03-07 00:17:11 +0100

    WIP

--- .evergreen/install-node.sh
+++ .evergreen/install-node.sh
@@ -12,21 +12,6 @@ if [ "$OS" == "Windows_NT" ]; then
 
   curl -sSfLO https://raw.githubusercontent.com/mongodb-js/compass/42e6142ae08be6fec944b80ff6289e6bcd11badf/.evergreen/node-gyp-bug-workaround.sh && bash node-gyp-bug-workaround.sh
 else
-  if which realpath; then # No realpath on macOS, but also not needed there
-    export HOME="$(realpath "$HOME")" # Needed to de-confuse nvm when /home is a symlink
-  fi
-  # Some Node.js driver versions leave a ~/.npmrc file lying around
-  # that breaks nvm because it contains a 'prefix=' option (pointing
-  # to a directory that no longer exists anyway).
-  if [ -e "$HOME/.npmrc" ]; then
-    # different `sed` arguments on macOS than for GNU sed ...
-    if [ `uname` == Darwin ]; then
-      sed -i'~' -e 's/^prefix=.*$//' "$HOME/.npmrc"
-    else
-      sed -i "$HOME/.npmrc" -e 's/^prefix=.*$//'
-    fi
-  fi
-
   npm cache clear --force || true # Try to work around `Cannot read property 'pickAlgorithm' of null` errors in CI
 fi
 
