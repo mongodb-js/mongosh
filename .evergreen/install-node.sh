6d306e2347273a04688643cb0deed72fe72c1c08
Anna Henningsen <anna@addaleax.net>
2025-03-11 22:54:55 +0100

    WIP

--- .evergreen/install-node.sh
+++ .evergreen/install-node.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 set -x
 export BASEDIR="$PWD/.evergreen"
@@ -12,6 +13,19 @@ if [ "$OS" == "Windows_NT" ]; then
 
   curl -sSfLO https://raw.githubusercontent.com/mongodb-js/compass/42e6142ae08be6fec944b80ff6289e6bcd11badf/.evergreen/node-gyp-bug-workaround.sh && bash node-gyp-bug-workaround.sh
 else
+  if [ `uname` = Darwin ]; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+    echo "Setting NVM environment home: $NVM_DIR"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+    set +x # nvm is very verbose
+    echo nvm install --no-progress $NODE_JS_VERSION && nvm alias default $NODE_JS_VERSION
+    nvm install --no-progress $NODE_JS_VERSION
+    nvm alias default $NODE_JS_VERSION
+    nvm use $NODE_JS_VERSION
+    set -x
+  fi
   npm cache clear --force || true # Try to work around `Cannot read property 'pickAlgorithm' of null` errors in CI
 fi
 
