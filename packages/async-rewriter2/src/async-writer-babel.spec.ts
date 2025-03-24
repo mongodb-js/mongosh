29f6ab1ef54d78108d1ebc59098790ec51bebb00
Anna Henningsen <anna@addaleax.net>
2025-03-24 13:44:25 -0400

    fixup: un-.only()

--- packages/async-rewriter2/src/async-writer-babel.spec.ts
+++ packages/async-rewriter2/src/async-writer-babel.spec.ts
@@ -276,7 +276,7 @@ describe('AsyncWriter', function () {
       ).to.equal(23);
     });
 
-    it.only('keeps `this` intact for function calls', function () {
+    it('keeps `this` intact for function calls', function () {
       expect('({ foo: 42, method() { return this } }).method().foo').to.equal(
         42
       );
