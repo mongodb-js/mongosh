1704b73c2f1736ab58bb4fc51fb67c91a0f9d6c7
gagik <me@gagik.co>
2025-04-30 16:41:27 +0200

    fix: move getDeviceId to be independent from loggingAndTelemetry

--- packages/logging/src/logging-and-telemetry.ts
+++ packages/logging/src/logging-and-telemetry.ts
@@ -55,6 +55,39 @@ import type {
 } from './types';
 import { createHmac } from 'crypto';
 
+/**
+ * @returns A hashed, unique identifier for the running device or `"unknown"` if not known.
+ */
+export async function getDeviceId({
+  onError,
+}: {
+  onError?: (error: Error) => void;
+} = {}): Promise<string | 'unknown'> {
+  try {
+    // Create a hashed format from the all uppercase version of the machine ID
+    // to match it exactly with the denisbrodbeck/machineid library that Atlas CLI uses.
+    const originalId: string =
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      await require('native-machine-id').getMachineId({
+        raw: true,
+      });
+
+    if (!originalId) {
+      return 'unknown';
+    }
+    const hmac = createHmac('sha256', originalId);
+
+    /** This matches the message used to create the hashes in Atlas CLI */
+    const DEVICE_ID_HASH_MESSAGE = 'atlascli';
+
+    hmac.update(DEVICE_ID_HASH_MESSAGE);
+    return hmac.digest('hex');
+  } catch (error) {
+    onError?.(error as Error);
+    return 'unknown';
+  }
+}
+
 export function setupLoggingAndTelemetry(
   props: MongoshLoggingAndTelemetryArguments
 ): MongoshLoggingAndTelemetry {
@@ -136,39 +169,13 @@ export class LoggingAndTelemetry implements MongoshLoggingAndTelemetry {
     this.resolveDeviceId('unknown');
   }
 
-  /**
-   * @returns A hashed, unique identifier for the running device or `"unknown"` if not known.
-   */
-  private async getDeviceId(): Promise<string | 'unknown'> {
-    try {
-      // Create a hashed format from the all uppercase version of the machine ID
-      // to match it exactly with the denisbrodbeck/machineid library that Atlas CLI uses.
-      const originalId: string =
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        await require('native-machine-id').getMachineId({
-          raw: true,
-        });
-
-      if (!originalId) {
-        return 'unknown';
-      }
-      const hmac = createHmac('sha256', originalId);
-
-      /** This matches the message used to create the hashes in Atlas CLI */
-      const DEVICE_ID_HASH_MESSAGE = 'atlascli';
-
-      hmac.update(DEVICE_ID_HASH_MESSAGE);
-      return hmac.digest('hex');
-    } catch (error) {
-      this.bus.emit('mongosh:error', error as Error, 'telemetry');
-      return 'unknown';
-    }
-  }
-
   private async setupTelemetry(): Promise<void> {
     if (!this.deviceId) {
       this.deviceId = await Promise.race([
-        this.getDeviceId(),
+        getDeviceId({
+          onError: (error) =>
+            this.bus.emit('mongosh:error', error, 'telemetry'),
+        }),
         new Promise<string>((resolve) => {
           this.resolveDeviceId = resolve;
         }),
