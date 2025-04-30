47dbf470c49c8b0a212e76f1efd79525776be4cd
Le Roux Bodenstein <lerouxb@gmail.com>
2025-04-30 15:48:29 +0100

    and again

--- packages/connectivity-tests/test/all.sh
+++ packages/connectivity-tests/test/all.sh
@@ -20,7 +20,7 @@ fi
 git clone https://github.com/mongodb-js/devtools-docker-test-envs.git test-envs
 cd test-envs
 
-git checkout a67e6c66deabf8c3b01a25ba9cc8f5d620c2ed04
+git checkout ac8e6675fcd769f0bfc868c3ac73cdd8bcdcc792
 
 "$CONNECTIVITY_TEST_SOURCE_DIR/ldap.sh"
 "$CONNECTIVITY_TEST_SOURCE_DIR/localhost.sh"
