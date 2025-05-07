fb5dada4eec07a74b27132c6724597e8c4387530
Anna Henningsen <anna@addaleax.net>
2025-05-07 17:53:24 +0200

    fixup: add rust executable variant

---
+++ packages/async-rewriter3/src/main.rs
@@ -0,0 +1,10 @@
+use std::io::Read;
+use async_rewriter3::async_rewrite;
+
+fn main() {
+    let mut input = String::new();
+
+    //std::fs::File::open("../../node_modules/sinon/pkg/sinon.js").unwrap()
+    std::io::stdin().read_to_string(&mut input).unwrap();
+    println!("{}", async_rewrite(input.as_str(), false).unwrap());
+}
