4ad7978f18fdcf1d16d4de6233fbdbf2ab9c8259
Le Roux Bodenstein <lerouxb@gmail.com>
2025-05-08 14:04:19 +0200

    strip down api.ts

--- packages/shell-api/src/api.ts
+++ packages/shell-api/src/api.ts
@@ -1,103 +1,10 @@
-// TODO: does it make sense to have all this stuff? Don't we just need enough for the top-level API, ie. the globals?
-import AggregationCursor from './aggregation-cursor';
-import RunCommandCursor from './run-command-cursor';
 export { Collection } from './collection';
-import Cursor from './cursor';
-import Database, { CollectionNamesWithTypes } from './database';
-import Explainable from './explainable';
-import ExplainableCursor from './explainable-cursor';
-import Help, { HelpProperties } from './help';
-export {
-  ShellInstanceState,
-  EvaluationListener,
-  ShellCliOptions,
-  OnLoadResult,
-  ShellPlugin,
-  AutocompleteParameters,
-} from './shell-instance-state';
+export { ShellBson } from './shell-bson';
+export { Streams } from './streams';
+export { Database } from './database';
 import Shard from './shard';
 import ReplicaSet from './replica-set';
 import ShellApi from './shell-api';
-export {
-  BulkWriteResult,
-  CommandResult,
-  CursorIterationResult,
-  DeleteResult,
-  InsertManyResult,
-  InsertOneResult,
-  UpdateResult,
-} from './result';
 import Mongo from './mongo';
-export {
-  signatures,
-  ShellResult,
-  toShellResult,
-  getShellApiType,
-  TypeSignature,
-  Namespace,
-} from './decorators';
-import { Topologies, ServerVersions } from './enums';
-import { InterruptFlag } from './interruptor';
-import { ShellBson } from './shell-bson';
-export type {
-  GenericCollectionSchema,
-  GenericDatabaseSchema,
-  GenericServerSideSchema,
-  StringKey,
-  FindAndModifyMethodShellOptions,
-  FindAndModifyShellOptions,
-  RemoveShellOptions,
-} from './helpers';
-export type { Streams } from './streams';
-export type { StreamProcessor } from './stream-processor';
 
-export {
-  AggregationCursor,
-  RunCommandCursor,
-  CollectionNamesWithTypes,
-  Cursor,
-  Database,
-  Explainable,
-  ExplainableCursor,
-  Help,
-  HelpProperties,
-  Mongo,
-  Shard,
-  ReplicaSet,
-  ShellApi,
-  ShellBson,
-  ServerVersions,
-  Topologies,
-  InterruptFlag,
-};
-
-// TODO: do we really want all these?
-
-/*
-export { AggregateOrFindCursor } from './aggregate-or-find-cursor';
-export {
-  ServiceProviderFindCursor,
-  ServiceProviderAggregationCursor,
-  ReadPreferenceLike,
-  ReadConcernLevel,
-  TagSet,
-  CollationOptions,
-  HedgeOptions,
-  ExplainVerbosityLike,
-  FindOptions,
-  CountOptions,
-  DistinctOptions,
-  FindOneAndDeleteOptions,
-  FindOneAndReplaceOptions,
-  FindOneAndUpdateOptions,
-  UpdateOptions,
-  CommandOperationOptions,
-  AggregateOptions
-} from '@mongosh/service-provider-core';
-export { DatabaseImpl } from './database';
-export { CollectionImpl } from './collection';
-export { ShellResultSourceInformation, ShellCommandCompleter, Signatures, ShellApiWithMongoClass } from './decorators';
-export { MapReduceShellOptions } from './helpers';
-
-// and many more
-*/
+export { Mongo, Shard, ReplicaSet, ShellApi };
