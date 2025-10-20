85769e333cd4209795a43843f5816c3403319eff
Anna Henningsen <anna@addaleax.net>
2025-10-20 12:18:32 +0200

    fixup: revert build package changes due to circular dependency

--- packages/build/src/build-info.ts
+++ packages/build/src/build-info.ts
@@ -1,6 +1,6 @@
 import type { Config } from './config';
 import { promises as fs } from 'fs';
-import type { BuildInfo } from '../../cli-repl/src/build-info';
+import os from 'os';
 
 /**
  * Write data into a build config that is included in the executable but
@@ -18,22 +18,12 @@ export async function writeBuildInfo(
     throw new Error('Segment key is required');
   }
 
-  const info: Omit<
-    BuildInfo,
-    | 'deps'
-    | 'installationMethod'
-    | 'runtimeArch'
-    | 'runtimePlatform'
-    | 'nodeVersion'
-    | 'opensslVersion'
-    | 'sharedOpenssl'
-    | 'runtimeGlibcVersion'
-  > = {
+  const info = {
     segmentApiKey: config.segmentKey,
     version: config.version,
     distributionKind,
-    buildArch: process.arch,
-    buildPlatform: process.platform,
+    buildArch: os.arch(),
+    buildPlatform: os.platform(),
     buildTarget: config.executableOsId ?? 'unknown',
     buildTime: new Date().toISOString(),
     gitVersion: config.revision ?? null,
