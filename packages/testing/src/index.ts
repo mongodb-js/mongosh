4fcbdd285e769f703c376ed97bee856f1584d708
gagik <me@gagik.co>
2025-11-20 12:21:02 +0100

    chore: move certificates inside

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -7,7 +7,7 @@ export * from './fake-kms';
 /**
  * Path to the certificates directory containing test certificates
  */
-const TEST_CERTIFICATES_DIR = path.resolve(__dirname, '..', 'certificates');
+const TEST_CERTIFICATES_DIR = path.resolve(__dirname, 'certificates');
 
 /** Get the path to a test certificate */
 export function getTestCertificatePath(filename: string): string {
