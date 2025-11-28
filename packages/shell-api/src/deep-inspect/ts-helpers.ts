f783da404cb452751af36d4c1daade13408c56a1
Anna Henningsen <anna@addaleax.net>
2025-11-28 16:10:25 +0100

    fixup: prettier doesnt run on subdirs?

--- packages/shell-api/src/deep-inspect/ts-helpers.ts
+++ packages/shell-api/src/deep-inspect/ts-helpers.ts
@@ -1,6 +1,8 @@
-
-export type UnionToIntersection<T> =
-    (T extends any ? (k: T) => void : never) extends ((k: infer U) => void) ? U : never
+export type UnionToIntersection<T> = (
+  T extends any ? (k: T) => void : never
+) extends (k: infer U) => void
+  ? U
+  : never;
 
 export type PickMethodsByReturnType<T, R> = {
   [k in keyof T as NonNullable<T[k]> extends (...args: any[]) => R
