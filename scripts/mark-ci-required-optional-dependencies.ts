934618d699f430bd134784aae89587f2fa0409b2
gagik <me@gagik.co>
2026-01-07 13:41:08 +0100

    chore: add more java deps

--- scripts/mark-ci-required-optional-dependencies.ts
+++ scripts/mark-ci-required-optional-dependencies.ts
@@ -1,21 +1,19 @@
-import { promises as fs } from "fs";
-import assert from "assert";
-import path from "path";
-import { getPackagesInTopologicalOrder } from "@mongodb-js/monorepo-tools";
+import { promises as fs } from 'fs';
+import assert from 'assert';
+import path from 'path';
+import { getPackagesInTopologicalOrder } from '@mongodb-js/monorepo-tools';
 
 (async () => {
-  const monorepoRootPath = path.resolve(__dirname, "..");
+  const monorepoRootPath = path.resolve(__dirname, '..');
   // not really relevant that it is in topological order, but it is a convenient
   // way to get all the packages' paths
   const packages = await getPackagesInTopologicalOrder(monorepoRootPath);
 
   for (const { location } of packages) {
-    const packageJsonPath = path.join(location, "package.json");
-    const packageJson = JSON.parse(await fs.readFile(packageJsonPath, "utf8"));
+    const packageJsonPath = path.join(location, 'package.json');
+    const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'));
     const { ciRequiredOptionalDependencies } = packageJson.mongosh ?? {};
-    for (const [pkg, platforms] of Object.entries(
-      ciRequiredOptionalDependencies ?? {}
-    ) as any[]) {
+    for (const [pkg, platforms] of Object.entries(ciRequiredOptionalDependencies ?? {}) as any[]) {
       if (platforms.includes(process.platform)) {
         packageJson.dependencies ??= {};
 
@@ -24,13 +22,6 @@ import { getPackagesInTopologicalOrder } from "@mongodb-js/monorepo-tools";
         packageJson.dependencies[pkg] = packageJson.optionalDependencies[pkg];
       }
     }
-    await fs.writeFile(
-      packageJsonPath,
-      JSON.stringify(packageJson, null, 2) + "\n"
-    );
+    await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
   }
-})().catch((err) =>
-  process.nextTick(() => {
-    throw err;
-  })
-);
+})().catch(err => process.nextTick(() => { throw err; }));
