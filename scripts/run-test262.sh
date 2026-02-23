9c1f1d50b4eb10d8661d58c4ccf7e9627b717a61
Kevin Mas Ruiz <kevin.mas@hey.com>
2026-02-23 12:56:38 +0100

    chore: Run test folders one by one to avoid OOM errors
    GHA runners have a fairly low amount of memory, and the
    test-harness will accumulate inputs/outputs of tests until
    the whole suite is done. The async rewriter generates
    a pretty long output compared to their inputs, so it OOMs
    in GHA.

--- scripts/run-test262.sh
+++ scripts/run-test262.sh
@@ -5,15 +5,18 @@ set -x
 
 TEST_262_REPO=https://github.com/tc39/test262.git
 TMP_DIR=/tmp
-OUTPUT_DIR=tmp
+OUTPUT_DIR=tmp/
 
 TEST_DIR=$TMP_DIR/test262
-TMP_TEST262_RESULT=$TMP_DIR/test262-result.json
-PARSED_TEST262_RESULT=$OUTPUT_DIR/test262.test.json
-TEST_PATTERN="$TEST_DIR/test/language/**/*.js"
+TMP_TEST262_DIR=$OUTPUT_DIR/test262-in/
+OUT_TEST262_DIR=$OUTPUT_DIR/test262-out/
 
 rm -rf $TEST_DIR
+rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
+mkdir -p $TMP_TEST262_DIR
+mkdir -p $OUT_TEST262_DIR
+
 npm run compile --workspace='packages/async-rewriter2'
 
 git clone --depth=1 https://github.com/tc39/test262.git $TEST_DIR
@@ -27,11 +30,27 @@ npm install
 ./make.py
 cd $SELF
 
-npx test262-harness \
-    --timeout 1000 \
-    --preprocessor scripts/test262-preprocessor-module.js \
-    --reporter json \
-    --reporter-keys "file,result,duration" \
-    "$TEST_PATTERN" > $TMP_TEST262_RESULT
+for TEST_SUBDIR in $(find $TEST_DIR/test/ -type f -name "*.js" -printf '%h\n' | sort -u | grep -v '^\.$');
+do
+    TEST_FILE=$(echo "${TEST_SUBDIR}.json" | sed 's/\//_/g')
+    TEST_PATTERN="$TEST_SUBDIR/*.js"
 
-node scripts/test262-output-to-mocha.js $TMP_TEST262_RESULT $PARSED_TEST262_RESULT
+    echo "OUTPUT_TO=$TMP_TEST262_DIR/$TEST_FILE"
+    npx test262-harness \
+    	--timeout 1000 \
+    	--preprocessor scripts/test262-preprocessor-module.js \
+    	--reporter json \
+    	--reporter-keys "file,result,duration" \
+    	"$TEST_PATTERN" > "$TMP_TEST262_DIR/$TEST_FILE"
+done;
+
+for TESTIN in $(find $TMP_TEST262_DIR -type f -name "*.json");
+do
+    if $(jq empty $TESTIN >/dev/null 2>&1) ;
+    then
+	TESTOUT="$OUT_TEST262_DIR/$(basename $TESTIN)"
+	node scripts/test262-output-to-mocha.js $TESTIN $TESTOUT
+    else
+	echo "[ERROR] Could not parse $TESTIN, it seems to be an invalid JSON."
+    fi
+done;
