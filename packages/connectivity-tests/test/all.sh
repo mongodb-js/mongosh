ff379a34e1678c8d2edc236f59072c79bdc4a9e4
Alena Khineika <alena.khineika@mongodb.com>
2026-09-07 15:27:50 +0200

    test: bump test-envs to fix the kdc image build MONGOSH-3642

--- packages/connectivity-tests/test/all.sh
+++ packages/connectivity-tests/test/all.sh
@@ -20,7 +20,7 @@ fi
 git clone https://github.com/mongodb-js/devtools-docker-test-envs.git test-envs
 cd test-envs
 
-git checkout cbec3a65efe9def50638994ae6b36ffa3b4708ee
+git checkout b01126de0fb76ba41813d8ed9f761e3a02d735a6
 
 "$CONNECTIVITY_TEST_SOURCE_DIR/ldap.sh"
 "$CONNECTIVITY_TEST_SOURCE_DIR/localhost.sh"
